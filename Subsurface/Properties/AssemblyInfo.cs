b9bc6845006f946ac409393c0e2ffd1686a3f971
Regalis <regalis@undertowgames.com>
2016-02-29 20:01:28 +0200

    v0.3.4.0

--- Subsurface/Properties/AssemblyInfo.cs
+++ Subsurface/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © Undertow Games 2015")]
+[assembly: AssemblyCopyright("Copyright © Undertow Games 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.3.1")]
-[assembly: AssemblyFileVersion("0.3.3.1")]
+[assembly: AssemblyVersion("0.3.4.0")]
+[assembly: AssemblyFileVersion("0.3.4.0")]
