c6df095a8b8a759d47c0ef88cdad073e37b11814
Regalis <regalis@undertowgames.com>
2016-04-10 10:57:38 +0300

    Narrow caves in levels, more background sprites

--- Subsurface/Source/Characters/BackgroundSprite/BackgroundSpritePrefab.cs
+++ Subsurface/Source/Characters/BackgroundSprite/BackgroundSpritePrefab.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Xml.Linq;
 
 namespace Barotrauma
@@ -9,6 +10,12 @@ namespace Barotrauma
 
         public readonly Alignment Alignment;
 
+        public readonly Vector2 Scale;
+
+        public readonly bool AlignWithSurface;
+
+        public readonly Vector2 RandomRotation;
+
         public readonly int Commonness;
 
         public BackgroundSpritePrefab(XElement element)
@@ -18,6 +25,15 @@ namespace Barotrauma
             if (!Enum.TryParse(alignmentStr, out Alignment)) Alignment = Alignment.BottomCenter;
 
             Commonness = ToolBox.GetAttributeInt(element, "commonness", 1);
+
+            Scale.X = ToolBox.GetAttributeFloat(element, "minsize", 1.0f);
+            Scale.Y = ToolBox.GetAttributeFloat(element, "maxsize", 1.0f);
+
+            AlignWithSurface = ToolBox.GetAttributeBool(element, "alignwithsurface", false);
+
+            RandomRotation = ToolBox.GetAttributeVector2(element, "randomrotation", Vector2.Zero);
+            RandomRotation.X = MathHelper.ToRadians(RandomRotation.X);
+            RandomRotation.Y = MathHelper.ToRadians(RandomRotation.Y);
 
             foreach (XElement subElement in element.Elements())
             {
