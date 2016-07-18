2bdd6ff9166bf7d9011aed7ee9c581f9b6fd8036
Regalis <regalis@undertowgames.com>
2016-07-18 20:37:12 +0300

    Multi-sub transition cinematic, linkedsubs don't have to be docked to the main sub when leaving (being at the same exit is enough), resetting item/serverlog listbox scroll when filtering the list

--- Subsurface/Source/Map/TransitionCinematic.cs
+++ Subsurface/Source/Map/TransitionCinematic.cs
@@ -19,14 +19,24 @@ namespace Barotrauma
         private float duration;
         
         public TransitionCinematic(Submarine submarine, Camera cam, float duration)
+            : this(new List<Submarine>() { submarine }, cam, duration)
         {
-            Vector2 targetPos = submarine.Position;
 
-            if (submarine.AtEndPosition)
+        }
+
+        public TransitionCinematic(List<Submarine> submarines, Camera cam, float duration)
+        {
+            Vector2 targetPos = new Vector2(
+                submarines.Sum(s => s.Position.X),
+                submarines.Sum(s => s.Position.Y)) / submarines.Count;
+
+            if (!submarines.Any()) return;
+
+            if (submarines.First().AtEndPosition)
             {
                 targetPos = Level.Loaded.EndPosition + Vector2.UnitY * 500.0f;
             }
-            else if (submarine.AtStartPosition)
+            else if (submarines.First().AtStartPosition)
             {
                 targetPos = Level.Loaded.StartPosition + Vector2.UnitY * 500.0f;
             }
@@ -34,16 +44,18 @@ namespace Barotrauma
             this.duration = duration;
 
             Running = true;
-            CoroutineManager.StartCoroutine(UpdateTransitionCinematic(submarine, cam, targetPos));
+            CoroutineManager.StartCoroutine(UpdateTransitionCinematic(submarines, cam, targetPos));
         }
 
-        private IEnumerable<object> UpdateTransitionCinematic(Submarine sub, Camera cam, Vector2 targetPos)
+        private IEnumerable<object> UpdateTransitionCinematic(List<Submarine> subs, Camera cam, Vector2 targetPos)
         {
+            if (!subs.Any()) yield return CoroutineStatus.Success;
+
             Character.Controlled = null;
             cam.TargetPos = Vector2.Zero;
             GameMain.LightManager.LosEnabled = false;
 
-            Vector2 diff = targetPos - sub.Position;
+            //Vector2 diff = targetPos - sub.Position;
             float targetSpeed = 10.0f;
 
             Level.Loaded.ShaftBodies[0].Enabled = false;
@@ -66,17 +78,18 @@ namespace Barotrauma
 
                 cam.Zoom = Math.Max(0.2f, cam.Zoom - CoroutineManager.UnscaledDeltaTime * 0.1f);
 
-                Vector2 cameraPos = sub.Position + Submarine.MainSub.HiddenSubPosition;
+                Vector2 cameraPos = subs.First().Position + Submarine.MainSub.HiddenSubPosition;
                 cameraPos.Y = Math.Min(cameraPos.Y, ConvertUnits.ToDisplayUnits(Level.Loaded.ShaftBodies[0].Position.Y) - cam.WorldView.Height/2.0f);
 
                 GUI.ScreenOverlayColor = Color.Lerp(Color.TransparentBlack, Color.Black, timer/duration);
 
                 cam.Translate((cameraPos - cam.Position) * CoroutineManager.UnscaledDeltaTime*10.0f);
 
-                if (diff != Vector2.Zero)
+                foreach (Submarine sub in subs)
                 {
-                    sub.ApplyForce((Vector2.Normalize(diff) * targetSpeed - sub.Velocity) * 500.0f);
+                    sub.ApplyForce((Vector2.Normalize(sub.Position - targetPos) * targetSpeed - sub.Velocity) * 500.0f);
                 }
+                    
 
                 timer += CoroutineManager.UnscaledDeltaTime;
 
