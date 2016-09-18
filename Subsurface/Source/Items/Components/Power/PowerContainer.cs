bed0c4e22209fe8ec701ec03fdc8212073e23506
juanjp600 <juanjprot@gmail.com>
2016-09-18 11:41:17 -0300

    Added some missing UpdateGUI implementations

--- Subsurface/Source/Items/Components/Power/PowerContainer.cs
+++ Subsurface/Source/Items/Components/Power/PowerContainer.cs
@@ -246,6 +246,11 @@ namespace Barotrauma.Items.Components
             spriteBatch.DrawString(GUI.Font, "Recharge rate: " + (int)((rechargeSpeed / maxRechargeSpeed) * 100.0f) + " %", new Vector2(x + 30, y + 95), Color.White);
         }
 
+        public override void UpdateHUD(Character character)
+        {
+            GuiFrame.Update(1.0f / 60.0f);
+        }
+
         public override void ClientWrite(Lidgren.Network.NetOutgoingMessage msg)
         {
             float chargeSpeed = MathHelper.Clamp(rechargeSpeed / MaxRechargeSpeed, 0.0f, 1.0f);
