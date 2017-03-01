cd967538f623f579b9f8b2712ceb9a8d3a7bddcd
juanjp600 <juanjprot@gmail.com>
2017-03-01 17:04:51 -0300

    - fixed crash in UpdateVoteStatus - fixed clients reconnecting automatically after being kicked for desync

--- Subsurface/Source/Networking/GameClient.cs
+++ Subsurface/Source/Networking/GameClient.cs
@@ -557,6 +557,7 @@ namespace Barotrauma.Networking
                                 case "The server has been shut down":
                                 case "You have been banned from the server":
                                 case "You have been kicked from the server":
+                                case "You have been disconnected because of excessive desync":
                                     var msgBox = new GUIMessageBox("CONNECTION LOST", disconnectMsg);
                                     msgBox.Buttons[0].OnClicked += ReturnToServerList;
                                     break;
