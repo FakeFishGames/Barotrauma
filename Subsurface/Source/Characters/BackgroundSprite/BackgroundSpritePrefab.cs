ad22621e49fff5e72c8120c160ac7f72fe42b1aa
Regalis <regalis@undertowgames.com>
2017-03-06 18:38:38 +0200

    Added z position to background sprites

--- Subsurface/Source/Characters/BackgroundSprite/BackgroundSpritePrefab.cs
+++ Subsurface/Source/Characters/BackgroundSprite/BackgroundSpritePrefab.cs
@@ -13,9 +13,13 @@ namespace Barotrauma
 
         public readonly Vector2 Scale;
 
+        public bool SpawnOnWalls;
+
         public readonly bool AlignWithSurface;
 
         public readonly Vector2 RandomRotation;
+
+        public readonly Vector2 DepthRange;
 
         public readonly float SwingAmount;
 
@@ -34,8 +38,12 @@ namespace Barotrauma
 
             Commonness = ToolBox.GetAttributeInt(element, "commonness", 1);
 
+            SpawnOnWalls = ToolBox.GetAttributeBool(element, "spawnonwalls", true);
+
             Scale.X = ToolBox.GetAttributeFloat(element, "minsize", 1.0f);
             Scale.Y = ToolBox.GetAttributeFloat(element, "maxsize", 1.0f);
+
+            DepthRange = ToolBox.GetAttributeVector2(element, "depthrange", Vector2.Zero);
 
             AlignWithSurface = ToolBox.GetAttributeBool(element, "alignwithsurface", false);
 
