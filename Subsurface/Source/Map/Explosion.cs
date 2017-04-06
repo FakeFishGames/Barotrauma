a2e21a78f913ce87052087dc98ece50f2827bbb4
Regalis <regalis@undertowgames.com>
2017-04-06 21:58:35 +0300

    Nicer looking explosions

--- Subsurface/Source/Map/Explosion.cs
+++ Subsurface/Source/Map/Explosion.cs
@@ -15,7 +15,7 @@ namespace Barotrauma
         
         public float CameraShake;
 
-        private bool sparks, shockwave, flames;
+        private bool sparks, shockwave, flames, smoke;
 
         public Explosion(XElement element)
         {
@@ -25,7 +25,8 @@ namespace Barotrauma
 
             sparks      = ToolBox.GetAttributeBool(element, "sparks", true);
             shockwave   = ToolBox.GetAttributeBool(element, "shockwave", true);
-            flames      = ToolBox.GetAttributeBool(element, "flames", true); 
+            flames      = ToolBox.GetAttributeBool(element, "flames", true);
+            smoke       = ToolBox.GetAttributeBool(element, "smoke", true);
 
             CameraShake = ToolBox.GetAttributeFloat(element, "camerashake", attack.Range*0.1f);
         }
@@ -53,8 +54,13 @@ namespace Barotrauma
                 }
                 if (flames)
                 {
-                    GameMain.ParticleManager.CreateParticle("explosionfire", worldPosition + Rand.Vector(50f),
-                        Rand.Vector(Rand.Range(50f, 100.0f)), 0.0f, hull);
+                    GameMain.ParticleManager.CreateParticle("explosionfire", ClampParticlePos(worldPosition + Rand.Vector(50f), hull),
+                        Rand.Vector(Rand.Range(50.0f, 100.0f)), 0.0f, hull);
+                }
+                if (smoke)
+                {
+                    GameMain.ParticleManager.CreateParticle("smoke", ClampParticlePos(worldPosition + Rand.Vector(50f), hull),
+                        Rand.Vector(Rand.Range(1.0f, 10.0f)), 0.0f, hull);
                 }
             }
 
@@ -91,9 +97,19 @@ namespace Barotrauma
 
         }
 
+        private Vector2 ClampParticlePos(Vector2 particlePos, Hull hull)
+        {
+            if (hull == null) return particlePos;
+
+            return new Vector2(
+                MathHelper.Clamp(particlePos.X, hull.WorldRect.X, hull.WorldRect.Right),
+                MathHelper.Clamp(particlePos.Y, hull.WorldRect.Y - hull.WorldRect.Height, hull.WorldRect.Y));
+        }
+
+
         private IEnumerable<object> DimLight(LightSource light)
         {
-            float currBrightness= 1.0f;
+            float currBrightness = 1.0f;
             float startRange = light.Range;
 
             while (light.Color.A > 0.0f)
@@ -101,13 +117,13 @@ namespace Barotrauma
                 light.Color = new Color(light.Color.R, light.Color.G, light.Color.B, currBrightness);
                 light.Range = startRange * currBrightness;
 
-                currBrightness -= CoroutineManager.DeltaTime*10.0f;
+                currBrightness -= CoroutineManager.DeltaTime * 20.0f;
 
                 yield return CoroutineStatus.Running;
             }
 
             light.Remove();
-            
+
             yield return CoroutineStatus.Success;
         }
 
