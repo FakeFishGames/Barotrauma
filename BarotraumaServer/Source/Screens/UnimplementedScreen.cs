7ab01b82376c342d33614f127b2a33f26d868b4d
juanjp600 <juanjprot@gmail.com>
2017-06-21 14:51:36 -0300

    Fixed linked subs not spawning
    Turns out the game actually sets Screen.Selected to null when the game starts, so the server would break on EditMapScreen checks.

---
+++ BarotraumaServer/Source/Screens/UnimplementedScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Barotrauma.Networking;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace Barotrauma
+{
+    class UnimplementedScreen : Screen
+    {
+        public static readonly UnimplementedScreen Instance = new UnimplementedScreen();
+
+        public override void Select()
+        {
+            throw new Exception("Tried to select unimplemented screen");
+        }
+    }
+}
