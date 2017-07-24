8f0f51098b4da3293d5c405ee3cc67bd8c1f4c58
Joonas Rikkonen <poe.regalis@gmail.com>
2017-07-24 18:25:26 +0300

    v0.6.1.1

--- Barotrauma/Launcher/Properties/AssemblyInfo.cs
+++ Barotrauma/Launcher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Undertow Games 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
