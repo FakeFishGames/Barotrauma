b8c6fa600507deba3510d0c01eb810b29d782643
Joonas Rikkonen <poe.regalis@gmail.com>
2017-10-04 20:18:01 +0300

    Added oscillators (a signal component that sends out a periodic pulse, sine wave or a square wave)

---
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Signal/OscillatorComponent.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework;
+
+namespace Barotrauma.Items.Components
+{
+    class OscillatorComponent : ItemComponent
+    {
+        public enum WaveType
+        {
+            Pulse,
+            Sine,
+            Square,
+        }
+
+        private float frequency;
+
+        private float phase;
+
+        [InGameEditable, HasDefaultValue(WaveType.Pulse, true)]
+        public WaveType OutputType
+        {
+            get;
+            set;
+        }
+
+        [InGameEditable, HasDefaultValue(1.0f, true)]
+        public float Frequency
+        {
+            get { return frequency; }
+            set { frequency = Math.Max(0.0f, value); }
+        }
+
+        public OscillatorComponent(Item item, XElement element) : 
+            base(item, element)
+        {
+            IsActive = true;
+        }
+
+        public override void Update(float deltaTime, Camera cam)
+        {
+            switch (OutputType)
+            {
+                case WaveType.Pulse:
+                    if (frequency <= 0.0f) return;
+
+                    phase += deltaTime;
+                    float pulseInterval = 1.0f / frequency;
+                    while (phase >= pulseInterval)
+                    {
+                        item.SendSignal(0, "1", "signal_out", null);
+                        phase -= pulseInterval;
+                    }
+                    break;
+                case WaveType.Square:
+                    phase = (phase + deltaTime * frequency) % 1.0f;
+                    item.SendSignal(0, phase < 0.5f ? "0" : "1", "signal_out", null);
+                    break;
+                case WaveType.Sine:
+                    phase = (phase + deltaTime * frequency) % 1.0f;
+                    item.SendSignal(0, Math.Sin(phase * MathHelper.TwoPi).ToString(CultureInfo.InvariantCulture), "signal_out", null);
+                    break;
+            }
+        }
+
+        public override void ReceiveSignal(int stepsTaken, string signal, Connection connection, Item source, Character sender, float power = 0.0f)
+        {
+            switch (connection.Name)
+            {
+                case "set_frequency":
+                case "frequency_in":
+                    float newFrequency;
+                    if (float.TryParse(signal, out newFrequency))
+                    {
+                        Frequency = newFrequency;
+                    }
+                    IsActive = true;
+                    break;
+                case "set_outputtype":
+                case "set_wavetype":
+                    WaveType newOutputType;
+                    if (Enum.TryParse(signal, out newOutputType))
+                    {
+                        OutputType = newOutputType;
+                    }
+                    break;
+            }
+        }
+    }
+}
