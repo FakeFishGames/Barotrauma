c296b2cdcdfbe246681e3d78a210202f4e04ace5
Joonas Rikkonen <poe.regalis@gmail.com>
2017-12-19 22:27:07 +0200

    Added permission presets (or ranks). Current presets are none (no special permissions), moderator (round management & kicking) and admin (almost everything permitted).

---
+++ Barotrauma/BarotraumaShared/Source/Networking/ClientPermissions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Barotrauma.Networking
+{
+    [Flags]
+    enum ClientPermissions
+    {
+        None = 0,
+        [Description("End round")]
+        EndRound = 1,
+        [Description("Kick")]
+        Kick = 2,
+        [Description("Ban")]
+        Ban = 4,
+        [Description("Select submarine")]
+        SelectSub = 8,
+        [Description("Select game mode")]
+        SelectMode = 16,
+        [Description("Manage campaign")]
+        ManageCampaign = 32,
+        [Description("Console commands")]
+        ConsoleCommands = 64
+    }
+
+    class PermissionPreset
+    {
+        public static List<PermissionPreset> List = new List<PermissionPreset>();
+           
+        public readonly string Name;
+        public readonly string Description;
+        public readonly ClientPermissions Permissions;
+        public readonly List<DebugConsole.Command> PermittedCommands;
+        
+        public PermissionPreset(XElement element)
+        {
+            Name = element.GetAttributeString("name", "");
+            Description = element.GetAttributeString("description", "");
+
+            string permissionsStr = element.GetAttributeString("permissions", "");
+            if (!Enum.TryParse(permissionsStr, out Permissions))
+            {
+                DebugConsole.ThrowError("Error in permission preset \"" + Name + "\" - " + permissionsStr + " is not a valid permission!");
+            }
+
+            PermittedCommands = new List<DebugConsole.Command>();
+            if (Permissions.HasFlag(ClientPermissions.ConsoleCommands))
+            {
+                foreach (XElement subElement in element.Elements())
+                {
+                    if (subElement.Name.ToString().ToLowerInvariant() != "command") continue;
+                    string commandName = subElement.GetAttributeString("name", "");
+
+                    DebugConsole.Command command = DebugConsole.FindCommand(commandName);
+                    if (command == null)
+                    {
+                        DebugConsole.ThrowError("Error in permission preset \"" + Name + "\" - " + commandName + "\" is not a valid console command.");
+                        continue;
+                    }
+
+                    PermittedCommands.Add(command);
+                }
+            }
+        }
+
+        public static void LoadAll(string file)
+        {
+            if (!File.Exists(file)) return;
+
+            XDocument doc = XMLExtensions.TryLoadXml(file);
+            if (doc == null || doc.Root == null) return;
+
+            foreach (XElement element in doc.Root.Elements())
+            {
+                List.Add(new PermissionPreset(element));
+            }
+        }
+    }
+}
