a981ff49705b8d9255e38db048b0df9ea2f98f39
juanjp600 <juanjprot@gmail.com>
2018-03-07 10:57:54 -0300

    Implemented streaming
    Still gotta do a bunch of error checking and cleanup, but it works!

--- Barotrauma/BarotraumaClient/Source/Sounds/SoundChannel.cs
+++ Barotrauma/BarotraumaClient/Source/Sounds/SoundChannel.cs
@@ -5,6 +5,8 @@ namespace Barotrauma
 {
     class SoundChannel : IDisposable
     {
+        private const int STREAM_BUFFER_SIZE = 65536;
+
         public Sound Sound
         {
             get;
@@ -22,14 +24,16 @@ namespace Barotrauma
             get;
             private set;
         }
-
         private int streamSeekPos;
         private bool reachedEndSample;
+        private int[] streamBuffers;
+
         public bool IsPlaying
         {
             get
             {
                 if (ALSourceIndex < 0) return false;
+                if (IsStream && !reachedEndSample) return true;
                 bool playing = AL.GetSourceState(Sound.Owner.GetSourceFromIndex(ALSourceIndex)) == ALSourceState.Playing;
                 ALError alError = AL.GetError();
                 if (alError != ALError.NoError)
@@ -67,6 +71,20 @@ namespace Barotrauma
                         throw new Exception("Failed to play source: " + AL.GetErrorString(alError));
                     }
                 }
+                else
+                {
+                    streamBuffers = new int[4];
+                    for (int i=0;i<4;i++)
+                    {
+                        streamBuffers[i] = AL.GenBuffer();
+
+                        ALError alError = AL.GetError();
+                        if (alError != ALError.NoError)
+                        {
+                            throw new Exception("Failed to generate stream buffers: " + AL.GetErrorString(alError));
+                        }
+                    }
+                }
             }
         }
 
@@ -90,12 +108,72 @@ namespace Barotrauma
 
             if (!reachedEndSample)
             {
-                short[] buffer = new short[8192];
-                int readSamples = Sound.FillStreamBuffer(streamSeekPos, buffer);
-                streamSeekPos += readSamples;
-                if (readSamples < 8192)
+                uint alSource = Sound.Owner.GetSourceFromIndex(ALSourceIndex);
+
+                bool playing = AL.GetSourceState(alSource) == ALSourceState.Playing;
+                ALError alError = AL.GetError();
+                if (alError != ALError.NoError)
                 {
-                    reachedEndSample = true;
+                    throw new Exception("Failed to determine playing state from streamed source: "+AL.GetErrorString(alError));
+                }
+
+                int buffersToUnqueue = 0;
+                int[] unqueuedBuffers = null;
+                if (streamSeekPos > 0)
+                {
+                    buffersToUnqueue = 0;
+                    AL.GetSource(alSource, ALGetSourcei.BuffersProcessed, out buffersToUnqueue);
+                    alError = AL.GetError();
+                    if (alError != ALError.NoError)
+                    {
+                        throw new Exception("Failed to determine processed buffers from streamed source: " + AL.GetErrorString(alError));
+                    }
+
+                    unqueuedBuffers = new int[buffersToUnqueue];
+                    AL.SourceUnqueueBuffers((int)alSource, buffersToUnqueue, unqueuedBuffers);
+                    alError = AL.GetError();
+                    if (alError != ALError.NoError)
+                    {
+                        throw new Exception("Failed to unqueue buffers from streamed source: " + AL.GetErrorString(alError));
+                    }
+                }
+                else
+                {
+                    buffersToUnqueue = 4;
+                    unqueuedBuffers = (int[])streamBuffers.Clone();
+                }
+
+                for (int i = 0; i < buffersToUnqueue; i++)
+                {
+                    short[] buffer = new short[STREAM_BUFFER_SIZE];
+                    int readSamples = Sound.FillStreamBuffer(streamSeekPos, buffer);
+                    streamSeekPos += readSamples;
+                    if (readSamples < STREAM_BUFFER_SIZE)
+                    {
+                        reachedEndSample = true;
+                    }
+                    if (readSamples > 0)
+                    {
+                        AL.BufferData<short>(unqueuedBuffers[i], Sound.ALFormat, buffer, readSamples, Sound.SampleRate);
+
+                        alError = AL.GetError();
+                        if (alError != ALError.NoError)
+                        {
+                            throw new Exception("Failed to assign data to stream buffer: " + AL.GetErrorString(alError));
+                        }
+
+                        AL.SourceQueueBuffer((int)alSource, unqueuedBuffers[i]);
+                        alError = AL.GetError();
+                        if (alError != ALError.NoError)
+                        {
+                            throw new Exception("Failed to queue buffer["+i.ToString()+"] to stream: " + AL.GetErrorString(alError));
+                        }
+                    }
+                }
+
+                if (AL.GetSourceState(alSource) != ALSourceState.Playing)
+                {
+                    AL.SourcePlay(alSource);
                 }
             }
         }
