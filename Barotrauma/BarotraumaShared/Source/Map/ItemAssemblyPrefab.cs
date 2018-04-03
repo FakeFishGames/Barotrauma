b6331ac38bc7dcafe6dbdde09e8e4d24aa626d6e
Joonas Rikkonen <poe.regalis@gmail.com>
2018-04-03 12:31:29 +0300

    Fixed items being placed at a different position than where they appear when an item assembly is being placed, fixed wires disconnecting in item assemblies

--- Barotrauma/BarotraumaShared/Source/Map/ItemAssemblyPrefab.cs
+++ Barotrauma/BarotraumaShared/Source/Map/ItemAssemblyPrefab.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Text;
 using System.Xml.Linq;
 using Microsoft.Xna.Framework.Graphics;
+using Barotrauma.Items.Components;
 
 namespace Barotrauma
 {
@@ -36,32 +37,31 @@ namespace Barotrauma
                 if (mapEntity != null) entities.Add(new Pair<MapEntityPrefab,Rectangle>(mapEntity, entityElement.GetAttributeRect("rect", Rectangle.Empty)));
             }
             
-            float minX = entities[0].Second.X, maxX = entities[0].Second.Right;
-            float minY = entities[0].Second.Y - entities[0].Second.Height, maxY = entities[0].Second.Y;
-            foreach (Pair<MapEntityPrefab, Rectangle> entity in entities)
-            {
-                for (int i = 0; i < entities.Count; i++)
-                {
-                    minX = Math.Min(minX, entities[i].Second.Center.X);
-                    maxX = Math.Max(maxX, entities[i].Second.Center.X);
-                    minY = Math.Min(minY, entities[i].Second.Y - entities[i].Second.Height / 2);
-                    maxY = Math.Max(maxY, entities[i].Second.Y - entities[i].Second.Height / 2);
-                }
-            }
-
-            Vector2 center = new Vector2((minX + maxX) / 2.0f, (minY + maxY) / 2.0f);
-            for (int i = 0; i < entities.Count; i++)
-            {
-                entities[i].Second = new Rectangle(
-                    (int)(entities[i].Second.X - center.X),
-                    (int)(entities[i].Second.Y - center.Y),
-                    entities[i].Second.Width,
-                    entities[i].Second.Height);
-            }
-
             List.Add(this);
         }
+        
+        protected override void CreateInstance(Rectangle rect)
+        {
+            List<MapEntity> entities = MapEntity.LoadAll(Submarine.MainSub, configElement, configPath);
+            if (entities.Count == 0) return;
+            
+            foreach (MapEntity me in entities)
+            {
+                me.Move(rect.Location.ToVector2());
+                Item item = me as Item;
+                if (item == null) continue;
+                Wire wire = item.GetComponent<Wire>();
+                if (wire != null) wire.MoveNodes(rect.Location.ToVector2() - Submarine.MainSub.HiddenSubPosition);
+            }
 
+            MapEntity.MapLoaded(entities);
+#if CLIENT
+            MapEntity.SelectedList.Clear();
+            MapEntity.SelectedList.AddRange(entities);
+#endif         
+        }
+
+#if CLIENT
         public override void DrawPlacing(SpriteBatch spriteBatch, Camera cam, Rectangle? placeRect = null)
         {
             base.DrawPlacing(spriteBatch, cam);
@@ -73,35 +73,39 @@ namespace Barotrauma
             }
         }
 
-        protected override void CreateInstance(Rectangle rect)
+        public static XElement Save(List<MapEntity> entities, string name, string description)
         {
-            List<MapEntity> entities = MapEntity.LoadAll(Submarine.MainSub, configElement, configPath);
-            if (entities.Count == 0) return;
-            
-            //move the created entities to the center of the ItemAssembly
-            float minX = entities[0].WorldRect.X, maxX = entities[0].WorldRect.Right;
-            float minY = entities[0].WorldRect.Y - entities[0].WorldRect.Height, maxY = entities[0].WorldRect.Y;
-            for (int i = 0; i < entities.Count; i++)
+            XElement element = new XElement("ItemAssembly",
+                new XAttribute("name", name),
+                new XAttribute("description", description));
+
+            //move the entities so that their "center of mass" is at {0,0}
+            var assemblyEntities = MapEntity.CopyEntities(MapEntity.SelectedList);
+            float minX = assemblyEntities[0].WorldRect.X, maxX = assemblyEntities[0].WorldRect.Right;
+            float minY = assemblyEntities[0].WorldRect.Y - assemblyEntities[0].WorldRect.Height, maxY = assemblyEntities[0].WorldRect.Y;
+            for (int i = 1; i < assemblyEntities.Count; i++)
             {
-                minX = Math.Min(minX, entities[i].WorldRect.Center.X);
-                maxX = Math.Max(maxX, entities[i].WorldRect.Center.X);
-                minY = Math.Min(minY, entities[i].WorldRect.Y - entities[i].WorldRect.Height / 2);
-                maxY = Math.Max(maxY, entities[i].WorldRect.Y - entities[i].WorldRect.Height / 2);
+                minX = Math.Min(minX, assemblyEntities[i].WorldRect.X);
+                maxX = Math.Max(maxX, assemblyEntities[i].WorldRect.Right);
+                minY = Math.Min(minY, assemblyEntities[i].WorldRect.Y - assemblyEntities[i].WorldRect.Height);
+                maxY = Math.Max(maxY, assemblyEntities[i].WorldRect.Y);
             }
             Vector2 center = new Vector2((minX + maxX) / 2.0f, (minY + maxY) / 2.0f);
-            foreach (MapEntity me in entities)
+            if (Submarine.MainSub != null) center -= Submarine.MainSub.HiddenSubPosition;
+
+            MapEntity.SelectedList.Clear();
+            MapEntity.SelectedList.AddRange(assemblyEntities);
+
+            foreach (MapEntity mapEntity in assemblyEntities)
             {
-                me.Move(rect.Location.ToVector2() - center);
+                mapEntity.Move(-center);
+                mapEntity.Submarine = Submarine.MainSub;
+                mapEntity.Save(element);
             }
 
-            MapEntity.MapLoaded(entities);
-
-#if CLIENT
-            MapEntity.SelectedList.Clear();
-            MapEntity.SelectedList.AddRange(entities);
-#endif
+            return element;
         }
-
+#endif
         public void Delete()
         {
             List.Remove(this);
