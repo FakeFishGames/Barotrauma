ce8ea789428fcb0f64e60725f96b041dc6371bc4
Joonas Rikkonen <poe.regalis@gmail.com>
2018-04-03 09:38:54 +0300

    Item assemblies are visible when being placed, connected wires and contained items are included in item assemblies

--- Barotrauma/BarotraumaShared/Source/Map/ItemAssemblyPrefab.cs
+++ Barotrauma/BarotraumaShared/Source/Map/ItemAssemblyPrefab.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Xml.Linq;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Barotrauma
 {
@@ -11,6 +12,8 @@ namespace Barotrauma
     {
         private readonly XElement configElement;
         private readonly string configPath;
+
+        private List<Pair<MapEntityPrefab,Rectangle>> entities;
 
         public ItemAssemblyPrefab(string filePath)
         {
@@ -25,29 +28,73 @@ namespace Barotrauma
 
             SerializableProperty.DeserializeProperties(this, configElement);
 
+            entities = new List<Pair<MapEntityPrefab, Rectangle>>();
+            foreach (XElement entityElement in doc.Root.Elements())
+            {
+                string entityName = entityElement.GetAttributeString("name", "");
+                MapEntityPrefab mapEntity = List.Find(p => p.Name == entityName);
+                if (mapEntity != null) entities.Add(new Pair<MapEntityPrefab,Rectangle>(mapEntity, entityElement.GetAttributeRect("rect", Rectangle.Empty)));
+            }
+            
+            float minX = entities[0].Second.X, maxX = entities[0].Second.Right;
+            float minY = entities[0].Second.Y - entities[0].Second.Height, maxY = entities[0].Second.Y;
+            foreach (Pair<MapEntityPrefab, Rectangle> entity in entities)
+            {
+                for (int i = 0; i < entities.Count; i++)
+                {
+                    minX = Math.Min(minX, entities[i].Second.Center.X);
+                    maxX = Math.Max(maxX, entities[i].Second.Center.X);
+                    minY = Math.Min(minY, entities[i].Second.Y - entities[i].Second.Height / 2);
+                    maxY = Math.Max(maxY, entities[i].Second.Y - entities[i].Second.Height / 2);
+                }
+            }
+
+            Vector2 center = new Vector2((minX + maxX) / 2.0f, (minY + maxY) / 2.0f);
+            for (int i = 0; i < entities.Count; i++)
+            {
+                entities[i].Second = new Rectangle(
+                    (int)(entities[i].Second.X - center.X),
+                    (int)(entities[i].Second.Y - center.Y),
+                    entities[i].Second.Width,
+                    entities[i].Second.Height);
+            }
+
             List.Add(this);
+        }
+
+        public override void DrawPlacing(SpriteBatch spriteBatch, Camera cam, Rectangle? placeRect = null)
+        {
+            base.DrawPlacing(spriteBatch, cam);
+            foreach (Pair<MapEntityPrefab, Rectangle> entity in entities)
+            {
+                Rectangle drawRect = entity.Second;
+                drawRect.Location += Submarine.MouseToWorldGrid(cam, Submarine.MainSub).ToPoint();
+                entity.First.DrawPlacing(spriteBatch, cam, drawRect);
+            }
         }
 
         protected override void CreateInstance(Rectangle rect)
         {
             List<MapEntity> entities = MapEntity.LoadAll(Submarine.MainSub, configElement, configPath);
             if (entities.Count == 0) return;
-
+            
             //move the created entities to the center of the ItemAssembly
             float minX = entities[0].WorldRect.X, maxX = entities[0].WorldRect.Right;
             float minY = entities[0].WorldRect.Y - entities[0].WorldRect.Height, maxY = entities[0].WorldRect.Y;
             for (int i = 0; i < entities.Count; i++)
             {
-                minX = Math.Min(minX, entities[i].WorldRect.X);
-                maxX = Math.Max(maxX, entities[i].WorldRect.Right);
-                minY = Math.Min(minY, entities[i].WorldRect.Y - entities[i].WorldRect.Height);
-                maxY = Math.Max(maxY, entities[i].WorldRect.Y);
+                minX = Math.Min(minX, entities[i].WorldRect.Center.X);
+                maxX = Math.Max(maxX, entities[i].WorldRect.Center.X);
+                minY = Math.Min(minY, entities[i].WorldRect.Y - entities[i].WorldRect.Height / 2);
+                maxY = Math.Max(maxY, entities[i].WorldRect.Y - entities[i].WorldRect.Height / 2);
             }
             Vector2 center = new Vector2((minX + maxX) / 2.0f, (minY + maxY) / 2.0f);
             foreach (MapEntity me in entities)
             {
-                me.Move(new Vector2(rect.Center.X, rect.Y - rect.Height / 2) - center);
+                me.Move(rect.Location.ToVector2() - center);
             }
+
+            MapEntity.MapLoaded(entities);
 
 #if CLIENT
             MapEntity.SelectedList.Clear();
