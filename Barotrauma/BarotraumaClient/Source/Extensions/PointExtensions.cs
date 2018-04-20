b3397883a13cd5df4e5bffeb5878a4a12157bf99
itchyOwl <lauri.harkanen@gmail.com>
2018-04-20 10:54:03 +0300

    Add PointExtensions.

---
+++ Barotrauma/BarotraumaClient/Source/Extensions/PointExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Barotrauma.Extensions
+{
+    public static class PointExtensions
+    {
+        public static Point Multiply(this Point p, float f)
+        {
+            return new Point((int)(p.X * f), (int)(p.Y * f));
+        }
+
+        public static Point Multiply(this Point p, Vector2 v)
+        {
+            return new Point((int)(p.X * v.X), (int)(p.Y * v.Y));
+        }
+
+        public static Point Divide(this Point p, float f)
+        {
+            return new Point((int)(p.X / f), (int)(p.Y / f));
+        }
+
+        public static Point Divide(this Point p, Vector2 v)
+        {
+            return new Point((int)(p.X / v.X), (int)(p.Y / v.Y));
+        }
+
+        public static Point Inverse(this Point p)
+        {
+            return new Point(-p.X, -p.Y);
+        }
+    }
+}
