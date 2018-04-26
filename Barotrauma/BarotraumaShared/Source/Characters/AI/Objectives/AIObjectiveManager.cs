9d54e719a5af54a8414e1d710b04ba96a256db12
Joonas Rikkonen <poe.regalis@gmail.com>
2018-04-26 10:28:27 +0300

    AI characters ordered to maintain the position of the sub use the position when the order was given, not the position when the character reaches and starts operating the nav terminal.

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveManager.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Barotrauma.Items.Components;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Barotrauma
@@ -114,6 +115,12 @@ namespace Barotrauma
                     break;
                 case "extinguishfires":
                     currentOrder = new AIObjectiveExtinguishFires(character);
+                    break;
+                case "steer":
+                    var steering = (order?.TargetEntity as Item)?.GetComponent<Steering>();
+                    if (steering != null) steering.PosToMaintain = steering.Item.Submarine?.WorldPosition;
+                    if (order.TargetItemComponent == null) return;
+                    currentOrder = new AIObjectiveOperateItem(order.TargetItemComponent, character, option, false, null, order.UseController);
                     break;
                 default:
                     if (order.TargetItemComponent == null) return;
