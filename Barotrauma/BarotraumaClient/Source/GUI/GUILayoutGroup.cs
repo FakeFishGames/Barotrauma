ae08710636b72be8ea27a2f1210779a674b02025
itchyOwl <lauri.harkanen@gmail.com>
2018-05-18 11:07:26 +0300

    Fix the GUILayoutGroup.

--- Barotrauma/BarotraumaClient/Source/GUI/GUILayoutGroup.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUILayoutGroup.cs
@@ -18,51 +18,28 @@ namespace Barotrauma
             }
         }
 
-        private int pos;
-
         public GUILayoutGroup(RectTransform rectT, bool isHorizontal = false, Anchor childAnchor = Anchor.TopLeft, int spacing = 0) : base(null, rectT)
         {
             this.isHorizontal = isHorizontal;
             this.childAnchor = childAnchor;
             this.spacing = spacing;
+            rectT.ChildrenChanged += (child) => Recalculate();
         }
 
-        public override void AddChild(GUIComponent child)
-        {
-            child.IgnoreLayoutGroups = false;
-            child.RectTransform.Parent = RectTransform;
-            child.RectTransform.SetPosition(childAnchor);
-            if (isHorizontal)
-            {
-                child.RectTransform.AbsoluteOffset = new Point(pos, 0);
-                pos += child.Rect.Width + spacing;
-            }
-            else
-            {
-                child.RectTransform.AbsoluteOffset = new Point(0, pos);
-                pos += child.Rect.Height + spacing;
-            }
-        }
-
-        public override void RemoveChild(GUIComponent child)
-        {
-            base.RemoveChild(child);
-            child.RectTransform.Parent = null;
-            if (isHorizontal)
-            {
-                pos -= child.Rect.Width + spacing;
-            }
-            else
-            {
-                pos -= child.Rect.Height + spacing;
-            }
-        }
-
+        private bool needsToRecalculate;
         public void Recalculate()
         {
-            pos = 0;
+            //TODO: option to stretch the children to fit the layout group?
+            int pos = 0;
             foreach (var child in RectTransform.Children)
             {
+                if (child.GUIComponent == null)
+                {
+                    // GUIComponent not yet set -> evaluate on the next frame
+                    // This happens when the event is launched in the RectTransform constructor, before the GUIComponent constructor is ready.
+                    needsToRecalculate = true;
+                    break;
+                }
                 if (child.GUIComponent.IgnoreLayoutGroups) { continue; }
                 child.SetPosition(childAnchor);
                 if (isHorizontal)
@@ -75,6 +52,16 @@ namespace Barotrauma
                     child.AbsoluteOffset = new Point(0, pos);
                     pos += child.Rect.Height + spacing;
                 }
+            }
+        }
+
+        protected override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+            if (needsToRecalculate)
+            {
+                Recalculate();
+                needsToRecalculate = false;
             }
         }
     }
