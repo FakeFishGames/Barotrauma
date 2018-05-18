d0c4ebd31e56f8a21a4f99a804d2ffe62fda8c3e
itchyOwl <lauri.harkanen@gmail.com>
2018-05-18 09:45:19 +0300

    Refactor GUILayoutGroup.

--- Barotrauma/BarotraumaClient/Source/GUI/GUILayoutGroup.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUILayoutGroup.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Barotrauma
 {
     class GUILayoutGroup : GUIComponent
     {
         private bool isHorizontal;
-
         private int spacing;
 
         private Anchor childAnchor;
+        public Anchor ChildAnchor
+        {
+            get { return childAnchor; }
+            set
+            {
+                childAnchor = value;
+                Recalculate();
+            }
+        }
+
+        private int pos;
 
         public GUILayoutGroup(RectTransform rectT, bool isHorizontal = false, Anchor childAnchor = Anchor.TopLeft, int spacing = 0) : base(null, rectT)
         {
@@ -22,16 +27,44 @@ namespace Barotrauma
             this.spacing = spacing;
         }
 
-        protected override void Update(float deltaTime)
+        public override void AddChild(GUIComponent child)
         {
-            //TODO: only update layout when children are added/removed
-            //TODO: option to stretch the children to fit the layout group?
-            int pos = 0;
-            foreach (RectTransform child in RectTransform.Children)
+            child.IgnoreLayoutGroups = false;
+            child.RectTransform.Parent = RectTransform;
+            child.RectTransform.SetPosition(childAnchor);
+            if (isHorizontal)
             {
-                if (child.GUIComponent.IgnoreLayoutGroups) continue;
-                child.Anchor = childAnchor;
+                child.RectTransform.AbsoluteOffset = new Point(pos, 0);
+                pos += child.Rect.Width + spacing;
+            }
+            else
+            {
+                child.RectTransform.AbsoluteOffset = new Point(0, pos);
+                pos += child.Rect.Height + spacing;
+            }
+        }
 
+        public override void RemoveChild(GUIComponent child)
+        {
+            base.RemoveChild(child);
+            child.RectTransform.Parent = null;
+            if (isHorizontal)
+            {
+                pos -= child.Rect.Width + spacing;
+            }
+            else
+            {
+                pos -= child.Rect.Height + spacing;
+            }
+        }
+
+        public void Recalculate()
+        {
+            pos = 0;
+            foreach (var child in RectTransform.Children)
+            {
+                if (child.GUIComponent.IgnoreLayoutGroups) { continue; }
+                child.SetPosition(childAnchor);
                 if (isHorizontal)
                 {
                     child.AbsoluteOffset = new Point(pos, 0);
@@ -41,7 +74,7 @@ namespace Barotrauma
                 {
                     child.AbsoluteOffset = new Point(0, pos);
                     pos += child.Rect.Height + spacing;
-                }                
+                }
             }
         }
     }
