e254d4d9a43210957db5b1482addc517f18cef7d
Joonas Rikkonen <poe.regalis@gmail.com>
2018-05-21 15:35:25 +0300

    Fixed GUIProgressBar rendering (forgot to add to previous commit)

--- Barotrauma/BarotraumaClient/Source/GUI/GUIProgressBar.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUIProgressBar.cs
@@ -25,7 +25,7 @@ namespace Barotrauma
             set
             {
                 barSize = MathHelper.Clamp(value, 0.0f, 1.0f);
-                UpdateRect();
+                //UpdateRect();
             }
         }
 
@@ -62,7 +62,7 @@ namespace Barotrauma
             GUI.Style.Apply(slider, "Slider", this);
 
             this.barSize = barSize;
-            UpdateRect();
+            //UpdateRect();
         }
 
         /// <summary>
@@ -80,7 +80,6 @@ namespace Barotrauma
             slider = new GUIFrame(new RectTransform(Vector2.One, rectT));
             GUI.Style.Apply(slider, "Slider", this);
             this.barSize = barSize;
-            UpdateRect();
         }
 
         /*public override void ApplyStyle(GUIComponentStyle style)
@@ -98,7 +97,7 @@ namespace Barotrauma
             this.style = style;
         }*/
 
-        private void UpdateRect()
+        /*private void UpdateRect()
         {
             if (RectTransform != null)
             {
@@ -113,68 +112,41 @@ namespace Barotrauma
                     isHorizontal ? (int)((frame.Rect.Width - padding.X - padding.Z) * barSize) : frame.Rect.Width,
                     isHorizontal ? (int)(frame.Rect.Height - padding.Y - padding.W) : (int)(frame.Rect.Height * barSize));
             }
-        }
-
+        }*/
+        
         protected override void Draw(SpriteBatch spriteBatch)
         {
             if (!Visible) return;
 
             if (ProgressGetter != null) BarSize = ProgressGetter();   
 
+            Rectangle sliderRect = new Rectangle(
+                    (int)(frame.Rect.X + padding.X),
+                    (int)(frame.Rect.Y + padding.Y + (isHorizontal ? 0 : frame.Rect.Height * (1.0f - barSize))),
+                    isHorizontal ? (int)((frame.Rect.Width - padding.X - padding.Z) * barSize) : frame.Rect.Width,
+                    isHorizontal ? (int)(frame.Rect.Height - padding.Y - padding.W) : (int)(frame.Rect.Height * barSize));
+
+            Rectangle prevScissorRect = spriteBatch.GraphicsDevice.ScissorRectangle;
+            spriteBatch.GraphicsDevice.ScissorRectangle = sliderRect;
+
             Color currColor = color;
             if (state == ComponentState.Selected) currColor = selectedColor;
             if (state == ComponentState.Hover) currColor = hoverColor;
 
-            if (slider.sprites != null && slider.sprites[state].Count > 0)
+            slider.Visible = true;
+            slider.Color = currColor;
+            if (slider.RectTransform == null) slider.Rect = Rect;
+            if (slider.AutoDraw)
             {
-                foreach (UISprite uiSprite in slider.sprites[state])
-                {
-                    if (uiSprite.Tile)
-                    {
-                        uiSprite.Sprite.DrawTiled(spriteBatch, slider.Rect.Location.ToVector2(), slider.Rect.Size.ToVector2(), color: currColor);
-                    }
-                    else if (uiSprite.Slice)
-                    {
-                        Vector2 pos = new Vector2(slider.Rect.X, slider.Rect.Y);
-
-                        int centerWidth = System.Math.Max(slider.Rect.Width - uiSprite.Slices[0].Width - uiSprite.Slices[2].Width, 0);
-                        int centerHeight = System.Math.Max(slider.Rect.Height - uiSprite.Slices[0].Height - uiSprite.Slices[8].Height, 0);
-
-                        Vector2 scale = new Vector2(
-                            MathHelper.Clamp((float)slider.Rect.Width / (uiSprite.Slices[0].Width + uiSprite.Slices[2].Width), 0, 1),
-                            MathHelper.Clamp((float)slider.Rect.Height / (uiSprite.Slices[0].Height + uiSprite.Slices[6].Height), 0, 1));
-
-                        for (int x = 0; x < 3; x++)
-                        {
-                            float width = (x == 1 ? centerWidth : uiSprite.Slices[x].Width) * scale.X;
-                            for (int y = 0; y < 3; y++)
-                            {
-                                float height = (y == 1 ? centerHeight : uiSprite.Slices[x + y * 3].Height) * scale.Y;
-
-                                spriteBatch.Draw(uiSprite.Sprite.Texture,
-                                    new Rectangle((int)pos.X, (int)pos.Y, (int)width, (int)height),
-                                    uiSprite.Slices[x + y * 3],
-                                    currColor * (currColor.A / 255.0f));
-                                
-                                pos.Y += height;
-                            }
-                            pos.X += width;
-                            pos.Y = slider.Rect.Y;
-                        }                        
-                    }
-                    else
-                    {
-                        spriteBatch.Draw(uiSprite.Sprite.Texture,
-                            slider.Rect, new Rectangle(
-                                uiSprite.Sprite.SourceRect.X, 
-                                uiSprite.Sprite.SourceRect.Y, 
-                                (int)(uiSprite.Sprite.SourceRect.Width * (isHorizontal ? barSize : 1.0f)),
-                                (int)(uiSprite.Sprite.SourceRect.Height * (isHorizontal ? 1.0f : barSize))), 
-                            currColor);
-                    }
-                }
+                slider.DrawAuto(spriteBatch);
             }
+            else
+            {
+                slider.DrawManually(spriteBatch);
+            }
+            //hide the slider, we've already drawn it manually
+            slider.Visible = false;
+            spriteBatch.GraphicsDevice.ScissorRectangle = prevScissorRect;
         }
-
     }
 }
