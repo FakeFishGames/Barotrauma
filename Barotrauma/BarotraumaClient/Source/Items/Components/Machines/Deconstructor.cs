792d246a08ff3f775e9b4d9f52fa569b795f2307
Joonas Rikkonen <poe.regalis@gmail.com>
2018-05-22 15:47:38 +0300

    Deconstructor, pump & minimap interfaces

--- Barotrauma/BarotraumaClient/Source/Items/Components/Machines/Deconstructor.cs
+++ Barotrauma/BarotraumaClient/Source/Items/Components/Machines/Deconstructor.cs
@@ -1,6 +1,8 @@
 ﻿using Barotrauma.Networking;
 using Lidgren.Network;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Xml.Linq;
 
 namespace Barotrauma.Items.Components
 {
@@ -14,6 +16,20 @@ namespace Barotrauma.Items.Components
             GuiFrame.AddToGUIUpdateList();
         }
         
+        partial void InitProjSpecific(XElement element)
+        {
+            var paddedFrame = new GUIFrame(new RectTransform(new Vector2(0.9f, 0.8f), GuiFrame.RectTransform, Anchor.Center), style: null);
+
+            progressBar = new GUIProgressBar(new RectTransform(new Vector2(1.0f, 0.15f), paddedFrame.RectTransform, Anchor.BottomCenter),
+                barSize: 0.0f, color: Color.LightGreen);
+
+            activateButton = new GUIButton(new RectTransform(new Point(200, 30), paddedFrame.RectTransform, Anchor.Center),
+                TextManager.Get("DeconstuctorDeconstruct"))
+            {
+                OnClicked = ToggleActive
+            };
+        }
+
         private bool ToggleActive(GUIButton button, object obj)
         {
             SetActive(!IsActive, Character.Controlled);
