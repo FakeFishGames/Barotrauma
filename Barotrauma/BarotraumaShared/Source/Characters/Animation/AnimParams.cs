282d9e75edfbb370e297cf8d4cf901d02ba83fe0
itchyOwl <lauri.harkanen@gmail.com>
2018-05-25 11:40:41 +0300

    Create an abstract base class for AnimParams for FishAnimParams and HumanoidAnimParams and move the common code base there. Setup an abstract FishAnimParams class and the derivative (singleton) instance classes. Change the logic how the editor is created and handled. It's not automatic anymore. Add MantisAnimParams.xml and CarrierAnimParams.xml for testing. Add WaveAmplitudeMultiplier. Refactor handling of the AnimParams instances in the editor screen.

---
+++ Barotrauma/BarotraumaShared/Source/Characters/Animation/AnimParams.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Barotrauma
+{
+    abstract class AnimParams : ISerializableEntity
+    {
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public Dictionary<string, SerializableProperty> SerializableProperties
+        {
+            get;
+            private set;
+        }
+
+        protected string filePath;
+
+        protected AnimParams(string file)
+        {
+            filePath = file;
+            XDocument doc = XMLExtensions.TryLoadXml(file);
+            if (doc == null || doc.Root == null) return;
+            Name = doc.Root.Name.ToString();
+            SerializableProperties = SerializableProperty.DeserializeProperties(this, doc.Root);
+        }
+
+#if CLIENT
+        private static GUIListBox editor;
+        public static GUIListBox Editor
+        {
+            get
+            {
+                if (editor == null)
+                {
+                    CreateEditor();
+                }
+                return editor;
+            }
+        }
+
+        public static void CreateEditor()
+        {
+            editor = new GUIListBox(new RectTransform(new Vector2(0.25f, 1), GUI.Canvas) { MinSize = new Point(200, GameMain.GraphicsHeight) });
+        }
+
+        public void AddToEditor()
+        {
+            Editor.AddChild(new SerializableEntityEditor(Editor.RectTransform, this, false, true, elementHeight: 0.04f));
+        }
+
+        public bool Save()
+        {
+            XDocument doc = XMLExtensions.TryLoadXml(filePath);
+            if (doc == null || doc.Root == null) return false;       
+            SerializableProperty.SerializeProperties(this, doc.Root, true);
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Indent = true,
+                OmitXmlDeclaration = true,
+                NewLineOnAttributes = true
+            };
+            using (var writer = XmlWriter.Create(filePath, settings))
+            {
+                doc.WriteTo(writer);
+                writer.Flush();
+            }
+            return true;
+        }
+
+        public bool Reset()
+        {
+            XDocument doc = XMLExtensions.TryLoadXml(filePath);
+            if (doc == null || doc.Root == null) return false;
+            SerializableProperties = SerializableProperty.DeserializeProperties(this, doc.Root);
+            return true;
+        }
+#endif
+    }
+}
---
+++ Barotrauma/BarotraumaShared/Source/Characters/Animation/FishAnimParams.cs
@@ -0,0 +1,39 @@
+﻿namespace Barotrauma
+{
+    abstract class FishAnimParams : AnimParams
+    {
+        protected FishAnimParams(string file) : base(file) { }
+
+        public static FishAnimParams GetInstance(string speciesName)
+        {
+            switch (speciesName)
+            {
+                case "mantis":
+                    return MantisAnimParams.Instance;
+                case "carrier":
+                    return CarrierAnimParams.Instance;
+                default:
+                    return null;
+            }
+        }
+
+        [Serialize(1.0f, true), Editable]
+        public float WaveAmplitudeMultiplier
+        {
+            get;
+            set;
+        }
+    }
+
+    class CarrierAnimParams : FishAnimParams
+    {
+        public static CarrierAnimParams Instance = new CarrierAnimParams("Content/Characters/Carrier/CarrierAnimParams.xml");
+        protected CarrierAnimParams(string file) : base(file) { }
+    }
+
+    class MantisAnimParams : FishAnimParams
+    {
+        public static MantisAnimParams Instance = new MantisAnimParams("Content/Characters/Mantis/MantisAnimParams.xml");
+        protected MantisAnimParams(string file) : base(file) { }
+    }
+}
