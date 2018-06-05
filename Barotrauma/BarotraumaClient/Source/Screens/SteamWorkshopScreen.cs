98890af7f73cfa16208697bdb2b645a708dc2bb2
Joonas Rikkonen <poe.regalis@gmail.com>
2018-06-05 14:53:03 +0300

    Steam workshop items include a metadata file that tells where each file should go in the game folder. Items can be enabled in the Workshop screen, which moves the files from Steam's download folder to the appropriate place.

--- Barotrauma/BarotraumaClient/Source/Screens/SteamWorkshopScreen.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/SteamWorkshopScreen.cs
@@ -9,8 +9,9 @@ namespace Barotrauma
     class SteamWorkshopScreen : Screen
     {
         private GUIFrame menu;
-        private GUIListBox itemList;
-        
+        private GUIListBox installedItemList;
+        private GUIListBox availableItemList;
+
         public SteamWorkshopScreen()
         {
             int width = Math.Min(GameMain.GraphicsWidth - 160, 1000);
@@ -21,14 +22,24 @@ namespace Barotrauma
             menu = new GUIFrame(new RectTransform(new Vector2(0.8f, 0.9f), GUI.Canvas, Anchor.Center));
             var paddedFrame = new GUIFrame(new RectTransform(new Vector2(0.9f, 0.9f), menu.RectTransform, Anchor.Center), style: null);
 
-            itemList = new GUIListBox(new RectTransform(new Vector2(0.4f, 0.8f), paddedFrame.RectTransform, Anchor.BottomLeft));
-            
-            //--------------------------------------------------------
+            var listContainer = new GUILayoutGroup(new RectTransform(new Vector2(0.4f, 1.0f), paddedFrame.RectTransform))
+            {
+                Stretch = true,
+                RelativeSpacing = 0.02f
+            };
 
-            GUIButton button = new GUIButton(new RectTransform(new Vector2(0.2f, 0.1f), paddedFrame.RectTransform),
+            GUIButton button = new GUIButton(new RectTransform(new Vector2(0.5f, 0.05f), listContainer.RectTransform),
                 TextManager.Get("Back"));
             button.OnClicked = GameMain.MainMenuScreen.SelectTab;
             button.SelectedColor = button.Color;
+
+            new GUITextBlock(new RectTransform(new Vector2(1.0f, 0.05f), listContainer.RectTransform), "Installed items");
+            installedItemList = new GUIListBox(new RectTransform(new Vector2(1.0f, 0.4f), listContainer.RectTransform));
+            new GUITextBlock(new RectTransform(new Vector2(1.0f, 0.05f), listContainer.RectTransform), "Available items");
+            availableItemList = new GUIListBox(new RectTransform(new Vector2(1.0f, 0.4f), listContainer.RectTransform));
+
+            //--------------------------------------------------------
+
         }
 
         public override void Select()
@@ -44,25 +55,32 @@ namespace Barotrauma
         
         private void OnItemsReceived(IList<Facepunch.Steamworks.Workshop.Item> itemDetails)
         {
-            itemList.ClearChildren();
+            installedItemList.ClearChildren();
+            availableItemList.ClearChildren();
             foreach (var item in itemDetails)
             {
-                var itemFrame = new GUIFrame(new RectTransform(new Vector2(1.0f, 0.1f), itemList.Content.RectTransform, minSize: new Point(0, 80)),
+                GUIListBox listBox = item.Installed ? installedItemList : availableItemList;
+                var itemFrame = new GUIFrame(new RectTransform(new Vector2(1.0f, 0.1f), listBox.Content.RectTransform, minSize: new Point(0, 80)),
                     style: "ListBoxElement")
                 {
                     UserData = item
                 };
-                new GUITextBlock(new RectTransform(new Vector2(0.8f, 0.25f), itemFrame.RectTransform), item.Title);
-                new GUITextBlock(new RectTransform(new Vector2(0.8f, 0.75f), itemFrame.RectTransform, Anchor.BottomLeft), item.Description,
+                new GUITextBlock(new RectTransform(new Vector2(0.75f, 0.25f), itemFrame.RectTransform), item.Title);
+                new GUITextBlock(new RectTransform(new Vector2(0.75f, 0.75f), itemFrame.RectTransform, Anchor.BottomLeft), item.Description,
                     wrap: true, font: GUI.SmallFont);
-
+                
                 if (item.Installed)
                 {
-                    new GUITextBlock(new RectTransform(new Vector2(0.3f, 0.25f), itemFrame.RectTransform, Anchor.CenterRight), "Installed", textAlignment: Alignment.CenterRight);
+                    new GUITickBox(new RectTransform(new Vector2(0.25f, 0.5f), itemFrame.RectTransform, Anchor.CenterRight), "Enabled")
+                    {
+                        Selected = SteamManager.CheckWorkshopItemEnabled(item),
+                        UserData = item,
+                        OnSelected = ToggleItemEnabled
+                    };
                 }
                 else
                 {
-                    var downloadBtn = new GUIButton(new RectTransform(new Vector2(0.2f, 0.2f), itemFrame.RectTransform, Anchor.CenterRight),
+                    var downloadBtn = new GUIButton(new RectTransform(new Vector2(0.2f, 0.5f), itemFrame.RectTransform, Anchor.CenterRight),
                         TextManager.Get("DownloadButton"))
                     {
                         UserData = item,
@@ -78,7 +96,21 @@ namespace Barotrauma
             item.Download();
             return true;
         }
-        
+
+        private bool ToggleItemEnabled(GUITickBox tickBox)
+        {
+            Facepunch.Steamworks.Workshop.Item item = tickBox.UserData as Facepunch.Steamworks.Workshop.Item;
+            if (tickBox.Selected)
+            {
+                tickBox.Selected = SteamManager.EnableWorkShopItem(item, false);
+            }
+            else
+            {
+                tickBox.Selected = !SteamManager.DisableWorkShopItem(item);
+            }
+            return true;
+        }
+
         public override void Draw(double deltaTime, GraphicsDevice graphics, SpriteBatch spriteBatch)
         {
             graphics.Clear(Color.CornflowerBlue);
