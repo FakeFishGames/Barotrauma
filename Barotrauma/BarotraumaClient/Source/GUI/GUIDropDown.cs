654ec343e9ab6347ddf3a443f9c565ca9f77bca5
Joonas Rikkonen <poe.regalis@gmail.com>
2018-06-07 00:12:05 +0300

    Fixed GUIDropDowns not working correctly when they're the child of a listbox or drawn manually. Listboxes clip children outside the rect, which caused the dropdown listboxes to not be fully visible. Drawing the dropdown manually caused its listbox to be drawn manually as well, ignoring the draw order and whether the listbox was open or not.

--- Barotrauma/BarotraumaClient/Source/GUI/GUIDropDown.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUIDropDown.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,7 +14,7 @@ namespace Barotrauma
         private GUIButton button;
         private GUIListBox listBox;
 
-        private RectTransform prevParent;
+        private RectTransform currentListBoxParent;
 
         public bool Dropped { get; set; }
 
@@ -90,17 +91,40 @@ namespace Barotrauma
                 OnSelected = SelectItem
             };
 
-            prevParent = rectT.Parent;
-            rectT.Parent.GUIComponent.OnAddedToGUIUpdateList += AddListBoxToGUIUpdateList;
+            currentListBoxParent = FindListBoxParent();
+            currentListBoxParent.GUIComponent.OnAddedToGUIUpdateList += AddListBoxToGUIUpdateList;
             rectT.ParentChanged += (RectTransform newParent) =>
             {
-                prevParent.GUIComponent.OnAddedToGUIUpdateList -= AddListBoxToGUIUpdateList;
+                currentListBoxParent.GUIComponent.OnAddedToGUIUpdateList -= AddListBoxToGUIUpdateList;
                 if (newParent != null)
                 {
-                    newParent.GUIComponent.OnAddedToGUIUpdateList += AddListBoxToGUIUpdateList;
+                    currentListBoxParent = FindListBoxParent();
+                    currentListBoxParent.GUIComponent.OnAddedToGUIUpdateList += AddListBoxToGUIUpdateList;
                 }
             };
         }
+
+
+        /// <summary>
+        /// Finds the component after which the listbox should be drawn. Usually the parent of the dropdown, but if the dropdown
+        /// is the child of another GUIListBox, we need to draw our listbox after that because listboxes clip everything outside their rect.
+        /// </summary>
+        private RectTransform FindListBoxParent()
+        {
+            List<RectTransform> parents = new List<RectTransform>() { RectTransform.Parent };
+            while (parents.Last().Parent != null)
+            {
+                parents.Add(parents.Last().Parent);
+            }
+            //find the parent GUIListBox highest in the hierarchy
+            for (int i = parents.Count - 1; i >= 0; i--)
+            {
+                if (parents[i].GUIComponent is GUIListBox) return parents[i];
+            }
+            //or just go with the direct parent if there are no listboxes in the hierarchy
+            return RectTransform.Parent;
+        }
+
         
         public void AddItem(string text, object userData = null, string toolTip = "")
         {
@@ -167,15 +191,30 @@ namespace Barotrauma
         }
 
         private void AddListBoxToGUIUpdateList(GUIComponent parent)
-        {
-            if (parent.RectTransform != RectTransform.Parent)
+        {            
+            //the parent is not our parent anymore :(
+            //can happen when subscribed to a parent higher in the hierarchy (instead of the direct parent),
+            //and somewhere between this component and the higher parent a component was removed
+            if (!parent.IsParentOf(this))
             {
-                DebugConsole.ThrowError("GUIDropDown received a AddListBoxToGUIUpdateList callback from a component that is not its parent.");
+                parent.OnAddedToGUIUpdateList -= AddListBoxToGUIUpdateList;
                 return;
             }
             if (Dropped)
             {
                 listBox.AddToGUIUpdateList(false, UpdateOrder);
+            }
+        }
+
+        public override void DrawManually(SpriteBatch spriteBatch, bool alsoChildren = false, bool recursive = true)
+        {
+            if (!Visible) return;
+
+            AutoDraw = false;
+            Draw(spriteBatch);
+            if (alsoChildren)
+            {
+                button.DrawManually(spriteBatch, alsoChildren, recursive);
             }
         }
 
