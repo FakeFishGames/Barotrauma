d56809e6df2f4f40da618a3be018b04096d03e93
itchyOwl <lauri.harkanen@gmail.com>
2018-06-07 17:17:23 +0300

    Add some math functions.

---
+++ Barotrauma/BarotraumaClient/Source/Extensions/VectorExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Barotrauma
+{
+    public static class VectorExtensions
+    {
+        /// <summary>
+        /// Unity's Angle implementation.
+        /// Returns the angle in degrees.
+        /// Min -180, max 180.
+        /// </summary>
+        public static float Angle(this Vector2 from, Vector2 to)
+        {
+            return (float)Math.Acos(MathHelper.Clamp(Vector2.Dot(Vector2.Normalize(from), Vector2.Normalize(to)), -1f, 1f)) * 57.29578f;
+        }
+
+        /// <summary>
+        /// Creates a forward pointing vector based on the rotation (in radians).
+        /// </summary>
+        public static Vector2 Forward(float radians, float radius = 1)
+        {
+            return new Vector2((float)Math.Sin(radians), (float)Math.Cos(radians)) * radius;
+        }
+    }
+}
