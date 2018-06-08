2eff4d85f62007d2c49267dd18983f3331c0f95c
itchyOwl <lauri.harkanen@gmail.com>
2018-06-08 15:03:25 +0300

    Don't disable frame visible parameter, because then the child elements are not updated and drawn.

--- Barotrauma/BarotraumaClient/Source/Screens/Screen.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/Screen.cs
@@ -15,7 +15,6 @@ namespace Barotrauma
                 if (frame == null)
                 {
                     frame = new GUIFrame(new RectTransform(Vector2.One, GUICanvas.Instance), style: null);
-                    frame.Visible = false;
                     frame.CanBeFocused = false;
                 }
                 return frame;
