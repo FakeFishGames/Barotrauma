ea86d785d15c31cde564a86734d02c8cebd24946
Joonas Rikkonen <poe.regalis@gmail.com>
2018-06-14 10:00:01 +0300

    - Deformable sprites with the same texture and the same number of subdivisions share the same vertex and index buffers. - Moved deform effect from LevelObjectManager to DeformableSprite. - Animation system that can be used to apply different types of deformations to sprites (atm the only types are inflate and custom which deforms the sprite using an arbitrary grid of vectors). The deformations can be added or multiplied together, for example the sonar flora have an inflate animation and a custom animation that multiplies the animations at the base with zero.

---
+++ Barotrauma/BarotraumaClient/Source/Sprite/DeformAnimations/CustomDeformation.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework;
+
+namespace Barotrauma.SpriteDeformations
+{
+    class CustomDeformation : SpriteDeformation
+    {
+        private float frequency;
+        private float amplitude;
+
+        private float phase;
+
+        private Vector2[,] deformation;
+
+        public CustomDeformation(XElement element) : base(element)
+        {
+            frequency = element.GetAttributeFloat("frequency", 0.0f);
+            amplitude = element.GetAttributeFloat("amplitude", 1.0f);
+            
+            List<Vector2[]> deformRows = new List<Vector2[]>();
+            for (int i = 0; ; i++)
+            {
+                string row = element.GetAttributeString("row" + i, "");
+                if (string.IsNullOrWhiteSpace(row)) break;
+
+                string[] splitRow = row.Split(' ');
+                Vector2[] rowVectors = new Vector2[splitRow.Length];
+                for (int j = 0; j < splitRow.Length; j++)
+                {
+                    rowVectors[j] = XMLExtensions.ParseVector2(splitRow[j]);
+                }
+                deformRows.Add(rowVectors);
+            }
+
+            if (deformRows.Count() == 0 || deformRows.First() == null || deformRows.First().Length == 0)
+            {
+                return;
+            }
+
+            var configDeformation = new Vector2[deformRows.First().Length, deformRows.Count];
+            for (int x = 0; x < configDeformation.GetLength(0); x++)
+            {
+                for (int y = 0; y < configDeformation.GetLength(1); y++)
+                {
+                    configDeformation[x, y] = deformRows[y][x];
+                }
+            }
+
+            //construct an array for the desired resolution, 
+            //interpolating values if the resolution configured in the xml is smaller
+            deformation = new Vector2[resolution.X, resolution.Y];
+            float divX = 1.0f / resolution.X, divY = 1.0f / resolution.Y;            
+            for (int x = 0; x < resolution.X; x++)
+            {
+                float normalizedX = x / (float)(resolution.X - 1);
+                for (int y = 0; y < resolution.Y; y++)
+                {
+                    float normalizedY = y / (float)(resolution.Y - 1);
+
+                    Point indexTopLeft = new Point(
+                        Math.Min((int)Math.Floor(normalizedX * (configDeformation.GetLength(0) - 1)), configDeformation.GetLength(0) - 1),
+                        Math.Min((int)Math.Floor(normalizedY * (configDeformation.GetLength(1) - 1)), configDeformation.GetLength(1) - 1));
+                    Point indexBottomRight = new Point(
+                        Math.Min(indexTopLeft.X + 1, configDeformation.GetLength(0) - 1),
+                        Math.Min(indexTopLeft.Y + 1, configDeformation.GetLength(1) - 1));
+
+                    Vector2 deformTopLeft = configDeformation[indexTopLeft.X, indexTopLeft.Y];
+                    Vector2 deformTopRight = configDeformation[indexBottomRight.X, indexTopLeft.Y];
+                    Vector2 deformBottomLeft = configDeformation[indexTopLeft.X, indexBottomRight.Y];
+                    Vector2 deformBottomRight = configDeformation[indexBottomRight.X, indexBottomRight.Y];
+
+                    deformation[x, y] = Vector2.Lerp(
+                        Vector2.Lerp(deformTopLeft, deformTopRight, (normalizedX % divX) / divX),
+                        Vector2.Lerp(deformBottomLeft, deformBottomRight, (normalizedX % divX) / divX),
+                        (normalizedY % divY) / divY);
+                }
+            }
+        }
+
+        protected override void GetDeformation(out Vector2[,] deformation, out float multiplier)
+        {
+            deformation = this.deformation;
+            multiplier = frequency <= 0.0f ? amplitude : (float)Math.Sin(phase) * amplitude;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            phase += deltaTime * frequency;
+            phase %= MathHelper.TwoPi;
+        }
+    }
+}
---
+++ Barotrauma/BarotraumaClient/Source/Sprite/DeformAnimations/Inflate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework;
+
+namespace Barotrauma.SpriteDeformations
+{
+    class Inflate : SpriteDeformation
+    {
+        private float frequency;
+        private float scale;
+
+        private float phase;
+
+        private Vector2[,] deformation;
+
+        public Inflate(XElement element) : base(element)
+        {
+            frequency = element.GetAttributeFloat("frequency", 0.0f);
+            scale = element.GetAttributeFloat("scale", 1.0f);
+
+            deformation = new Vector2[resolution.X, resolution.Y];
+            for (int x = 0; x < resolution.X; x++)
+            {
+                float normalizedX = x / (float)(resolution.X - 1);
+                for (int y = 0; y < resolution.Y; y++)
+                {
+                    float normalizedY = y / (float)(resolution.X - 1);
+
+                    Vector2 centerDiff = new Vector2(normalizedX - 0.5f, normalizedY - 0.5f);
+                    float centerDist = centerDiff.Length() * 2.0f;
+                    if (centerDist == 0.0f) continue;
+
+                    deformation[x, y] = (centerDiff / centerDist) * Math.Min(1.0f, centerDist);
+                }
+            }
+        }
+
+        protected override void GetDeformation(out Vector2[,] deformation, out float multiplier)
+        {
+            deformation = this.deformation;
+            multiplier = frequency <= 0.0f ? scale : (float)(Math.Sin(phase)+1.0f) / 2.0f * scale;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            phase += deltaTime * frequency;
+            phase %= MathHelper.TwoPi;
+        }
+    }
+}
