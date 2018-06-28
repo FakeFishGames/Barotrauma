4c908da60f4970f5b170b2e10cd9689c1c2c9508
Joonas Rikkonen <poe.regalis@gmail.com>
2018-06-28 13:32:14 +0300

    Removed the requirement to return back to the starting location from combat missions. Forcing the losing team to sit and watch while the other team tries to make it to the starting location is no fun.

--- Barotrauma/BarotraumaShared/Source/Events/Missions/CombatMission.cs
+++ Barotrauma/BarotraumaShared/Source/Events/Missions/CombatMission.cs
@@ -196,10 +196,10 @@ namespace Barotrauma
             {
                 for (int i = 0; i < teamDead.Length; i++)
                 {
-                    if (!teamDead[i] && teamDead[1-i])
+                    if (!teamDead[i] && teamDead[1 - i])
                     {
                         //make sure nobody in the other team can be revived because that would be pretty weird
-                        crews[1-i].ForEach(c => { if (!c.IsDead) c.Kill(CauseOfDeathType.Unknown, null); });
+                        crews[1 - i].ForEach(c => { if (!c.IsDead) c.Kill(CauseOfDeathType.Unknown, null); });
 
                         winner = i;
 
@@ -213,12 +213,10 @@ namespace Barotrauma
             }
             else
             {
-                if (winner>=0 && subs[winner] != null && 
-                    (winner == 0 && subs[winner].AtStartPosition) || (winner == 1 && subs[winner].AtEndPosition) &&
-                    crews[winner].Any(c => !c.IsDead && c.Submarine == subs[winner]))
+                if (winner >= 0)
                 {
 #if CLIENT
-                    GameMain.GameSession.CrewManager.WinningTeam = winner+1;
+                    GameMain.GameSession.CrewManager.WinningTeam = winner + 1;
 #endif
                     if (GameMain.Server != null) GameMain.Server.EndGame();
                 }
