e1e761a4ddb5f5ae2f3354fbd0b4b13a8ea918cf
itchyOwl <lauri.harkanen@gmail.com>
2018-07-04 12:23:04 +0300

    Remove an old debug msg.

--- Barotrauma/BarotraumaClient/Source/GUI/GUI.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUI.cs
@@ -266,8 +266,6 @@ namespace Barotrauma
             {
                 DrawString(spriteBatch, new Vector2(500, 0), "gui components: " + updateList.Count, Color.White, Color.Black * 0.5f, 0, SmallFont);
                 DrawString(spriteBatch, new Vector2(500, 20), "mouse on: " + (MouseOn == null ? "null" : MouseOn.ToString()), Color.White, Color.Black * 0.5f, 0, SmallFont);
-                DrawString(spriteBatch, new Vector2(500, 40), "scroll bar value: " + (GUIScrollBar.draggingBar == null ? "null" : GUIScrollBar.draggingBar.BarScroll.ToString()), Color.White, Color.Black * 0.5f, 0, SmallFont);
-
                 GameMain.GameSession?.EventManager?.DebugDrawHUD(spriteBatch);
             }
             
