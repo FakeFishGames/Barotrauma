4041633eec67cef78e581fe0a634ad6e50b36823
Joonas Rikkonen <poe.regalis@gmail.com>
2018-07-23 18:33:32 +0300

    Clamping velocity when correcting submarine position

--- Barotrauma/BarotraumaShared/Source/Map/SubmarineBody.cs
+++ Barotrauma/BarotraumaShared/Source/Map/SubmarineBody.cs
@@ -190,6 +190,11 @@ namespace Barotrauma
 
                 Body.CorrectPosition(memPos, deltaTime, out newVelocity, out newPosition);
                 Vector2 moveAmount = ConvertUnits.ToDisplayUnits(newPosition - Body.SimPosition);
+                newVelocity = newVelocity.ClampLength(100.0f);
+                if (!MathUtils.IsValid(newVelocity))
+                {
+                    return;
+                }
 
                 List<Submarine> subsToMove = submarine.GetConnectedSubs();
                 foreach (Submarine dockedSub in subsToMove)
