cb0c4d044d86aa3a9dcf07ae7d2ee4e60aabd330
Joonas Rikkonen <poe.regalis@gmail.com>
2018-07-23 17:40:40 +0300

    Fixed clients not syncing the position of their controlled character when dead/unconscious

--- Barotrauma/BarotraumaClient/Source/Characters/CharacterNetworking.cs
+++ Barotrauma/BarotraumaClient/Source/Characters/CharacterNetworking.cs
@@ -150,7 +150,7 @@ namespace Barotrauma
                     msg.ReadPadBits();
 
                     int index = 0;
-                    if (GameMain.NetworkMember.Character == this)
+                    if (GameMain.NetworkMember.Character == this && AllowInput)
                     {
                         var posInfo = new CharacterStateInfo(pos, rotation, networkUpdateID, facingRight ? Direction.Right : Direction.Left, selectedEntity, animation);
                         while (index < memState.Count && NetIdUtils.IdMoreRecent(posInfo.ID, memState[index].ID))
