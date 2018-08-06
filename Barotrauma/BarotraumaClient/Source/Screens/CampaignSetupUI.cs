7c93730fc3d481f95c273651a9183397e5181609
Joonas Rikkonen <poe.regalis@gmail.com>
2018-08-06 14:28:54 +0300

    Fixed errors in netlobbyscreen when attempting to select a sub that fails to load (for example due to a missing/corrupt file), campaign cannot be started if the selected submarine cannot be loaded

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -57,6 +57,14 @@ namespace Barotrauma
                 
                 Submarine selectedSub = subList.SelectedData as Submarine;
                 if (selectedSub == null) return false;
+                
+                if (string.IsNullOrEmpty(selectedSub.MD5Hash.Hash))
+                {
+                    ((GUITextBlock)subList.Selected).TextColor = Color.DarkRed * 0.8f;
+                    subList.Selected.CanBeFocused = false;
+                    subList.Deselect();
+                    return false;
+                }
 
                 string savePath = SaveUtil.CreateSavePath(isMultiplayer ? SaveUtil.SaveType.Multiplayer : SaveUtil.SaveType.Singleplayer, saveNameBox.Text);
                 if (selectedSub.HasTag(SubmarineTag.Shuttle) || !selectedSub.CompatibleContentPackages.Contains(GameMain.SelectedPackage.Name))
