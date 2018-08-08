fc733cd23ae474bf5970bb8e2717caba5db2fff5
Juan Pablo Arce <juanjprot@gmail.com>
2018-08-08 18:06:19 -0300

    Added voip serialization classes

---
+++ Barotrauma/BarotraumaShared/Source/Networking/Voip/VoipConfig.cs
@@ -0,0 +1,61 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barotrauma.Networking
+{
+    static partial class VoipConfig
+    {
+        public const int MAX_COMPRESSED_SIZE = 120; //amount of bytes we expect each 60ms of audio to fit in
+
+        public const int SEND_INTERVAL_MS = 200;
+
+        public class VoipQueue
+        {
+            private const int BUFFER_COUNT = 5;
+            private int[] bufferLengths;
+            private byte[][] buffers;
+            private int newestBuffer;
+
+            public byte[] BufferToQueue
+            {
+                get;
+                private set;
+            }
+
+            public VoipQueue()
+            {
+                BufferToQueue = new byte[MAX_COMPRESSED_SIZE];
+                newestBuffer = BUFFER_COUNT - 1;
+                bufferLengths = new int[BUFFER_COUNT];
+                buffers = new byte[BUFFER_COUNT][];
+                for (int i = 0; i < BUFFER_COUNT; i++)
+                {
+                    buffers[i] = new byte[MAX_COMPRESSED_SIZE];
+                }
+            }
+
+            public void EnqueueBuffer(int length)
+            {
+                if (length > byte.MaxValue) return;
+
+                newestBuffer = (newestBuffer + 1) % BUFFER_COUNT;
+
+                bufferLengths[newestBuffer] = length;
+                BufferToQueue.CopyTo(buffers[newestBuffer], 0);
+            }
+
+            public void Write(NetBuffer msg)
+            {
+                for (int i=0;i<BUFFER_COUNT;i++)
+                {
+                    int index = (newestBuffer+i+1) % BUFFER_COUNT;
+
+                    msg.Write((byte)bufferLengths[index]);
+                    msg.Write(buffers[index], 0, bufferLengths[index]);
+                }
+            }
+        }
+    }
+}
---
+++ Barotrauma/BarotraumaShared/Source/Networking/Voip/VoipServer.cs
@@ -0,0 +1,58 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barotrauma.Networking
+{
+    class VoipServer
+    {
+        private NetServer server;
+        private TimeSpan sendIntervalTimeSpan;
+        private Dictionary<Client,DateTime> lastSendTime;
+
+        public VoipServer(NetServer server)
+        {
+            this.server = server;
+            sendIntervalTimeSpan = new TimeSpan(0,0,0,0,VoipConfig.SEND_INTERVAL_MS);
+            lastSendTime = new Dictionary<Client, DateTime>();
+        }
+        
+        public void SendToClients(List<Client> clients)
+        {
+            foreach (Client client in clients)
+            {
+                if (lastSendTime.ContainsKey(client))
+                {
+                    if ((lastSendTime[client] + sendIntervalTimeSpan) > DateTime.Now) continue;
+                    lastSendTime[client] = DateTime.Now;
+                }
+                else
+                {
+                    lastSendTime.Add(client, DateTime.Now);
+                }
+
+                NetOutgoingMessage msg = server.CreateMessage();
+
+                msg.Write((byte)ServerPacketHeader.VOICE);
+
+                msg.Write((byte)(clients.Count - 1));
+                foreach (Client c in clients)
+                {
+                    if (c == client) continue;
+                    msg.Write(c.ID);
+                    c.voipQueue.Write(msg);
+                }
+            }
+        }
+    }
+
+    partial class Client
+    {
+        public VoipConfig.VoipQueue voipQueue
+        {
+            get;
+            private set;
+        }
+    }
+}
