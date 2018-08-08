db690fd2003843c615758d119b36395c3a0d6644
Juan Pablo Arce <juanjprot@gmail.com>
2018-08-08 12:12:08 -0300

    Increased frequency to 44000 + put a hard limit on compressed audio size

--- Barotrauma/BarotraumaClient/Source/Sounds/InputPlaybackSound.cs
+++ Barotrauma/BarotraumaClient/Source/Sounds/InputPlaybackSound.cs
@@ -18,18 +18,20 @@ namespace Barotrauma.Sounds
 
         OpusEncoder encoder;
         OpusDecoder decoder;
+        byte[] compressedBuffer;
 
-        private const int FREQUENCY = 24000; //not amazing, but not bad audio quality
-        private const int BUFFER_SIZE = 1440; //60ms window, the max Opus seems to support
-        private const int REPEAT_MARGIN = 720; //30ms window of audio to repeat in case not enough audio is available
+        private const int FREQUENCY = 48000; //not amazing, but not bad audio quality
+        private const int BUFFER_SIZE = 2880; //60ms window, the max Opus seems to support
+        private const int MAX_COMPRESSED_SIZE = 150; //amount of bytes we expect each 60ms of audio to fit in
+        private const int REPEAT_MARGIN = 1440; //30ms window of audio to repeat in case not enough audio is available
         private short[] repeatBuffer;
-        
+
         SoundChannel soundChannel;
-        
+
         public InputPlaybackSound(SoundManager owner) : base(owner, "input", true)
         {
             //set up capture device
-            captureDevice = Alc.CaptureOpenDevice(null, FREQUENCY, ALFormat.Mono16, BUFFER_SIZE*5);
+            captureDevice = Alc.CaptureOpenDevice(null, FREQUENCY, ALFormat.Mono16, BUFFER_SIZE * 5);
             repeatBuffer = new short[REPEAT_MARGIN];
             ALError alError = AL.GetError();
             AlcError alcError = Alc.GetError(captureDevice);
@@ -48,12 +50,15 @@ namespace Barotrauma.Sounds
             soundChannel = null;
 
             //set up Opus
-            encoder = new OpusEncoder(24000, 1, OpusApplication.OPUS_APPLICATION_VOIP);
-            encoder.Bitrate = 12000;
+            encoder = new OpusEncoder(FREQUENCY, 1, OpusApplication.OPUS_APPLICATION_VOIP);
+            encoder.Bandwidth = OpusBandwidth.OPUS_BANDWIDTH_AUTO;
+            encoder.Bitrate = 8 * MAX_COMPRESSED_SIZE * FREQUENCY / BUFFER_SIZE;
             encoder.SignalType = OpusSignal.OPUS_SIGNAL_VOICE;
 
-            decoder = new OpusDecoder(24000, 1);
-            
+            decoder = new OpusDecoder(FREQUENCY, 1);
+
+            compressedBuffer = new byte[MAX_COMPRESSED_SIZE];
+
             Alc.CaptureStart(captureDevice);
             alcError = Alc.GetError(captureDevice);
             if (alcError != AlcError.NoError)
@@ -64,7 +69,7 @@ namespace Barotrauma.Sounds
             chn.FilledByNetwork = true;
             soundChannel = chn;
         }
-        
+
         public override SoundChannel Play(float gain, float range, Vector2 position, bool muffle = false)
         {
             throw new InvalidOperationException();
@@ -100,7 +105,7 @@ namespace Barotrauma.Sounds
             {
                 //Console.WriteLine("USING REPEAT MARGIN");
                 repeatBuffer.CopyTo(buffer, 0);
-                return REPEAT_MARGIN*2;
+                return REPEAT_MARGIN * 2;
             }
 
             GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
@@ -112,24 +117,22 @@ namespace Barotrauma.Sounds
             {
                 handle.Free();
             }
-            
+
             alcError = Alc.GetError(captureDevice);
             if (alcError != AlcError.NoError)
             {
                 throw new Exception("Failed to capture samples: " + alcError.ToString());
             }
 
-            byte[] compressed = new byte[BUFFER_SIZE];
-
             //encode and decode audio to test compression ratio and quality
-            int compressedCount = encoder.Encode(buffer, 0, BUFFER_SIZE, compressed, 0, BUFFER_SIZE);
-            //Console.WriteLine("UNCOMPRESSED: " + BUFFER_SIZE.ToString() + "; COMPRESSED: " + compressedCount.ToString());
-            decoder.Decode(compressed, 0, compressedCount, buffer, 0, BUFFER_SIZE);
+            int compressedCount = encoder.Encode(buffer, 0, BUFFER_SIZE, compressedBuffer, 0, MAX_COMPRESSED_SIZE);
+            Console.WriteLine("UNCOMPRESSED: " + BUFFER_SIZE.ToString() + "; COMPRESSED: " + compressedCount.ToString());
+            decoder.Decode(compressedBuffer, 0, compressedCount, buffer, 0, BUFFER_SIZE);
 
             //copy audio into repeat buffer because this should always be up to date
             Array.Copy(buffer, BUFFER_SIZE - REPEAT_MARGIN, repeatBuffer, 0, REPEAT_MARGIN);
-            
-            return BUFFER_SIZE*2;
+
+            return BUFFER_SIZE * 2;
         }
 
         public override void Dispose()
