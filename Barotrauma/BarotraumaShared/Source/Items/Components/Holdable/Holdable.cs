5b2b7be3cc2cc95acb109a24a50e17abebe87ac1
Joonas Rikkonen <poe.regalis@gmail.com>
2018-08-13 17:14:49 +0300

    Fixed characters being able to aim with items that have no aimPos configured (causing them to hold two-handed items like shells an crates in one hand when holding the aim button). Closes #613

--- Barotrauma/BarotraumaShared/Source/Items/Components/Holdable/Holdable.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Holdable/Holdable.cs
@@ -358,7 +358,7 @@ namespace Barotrauma.Items.Components
 
             if (picker.HasSelectedItem(item))
             {
-                picker.AnimController.HoldItem(deltaTime, item, handlePos, holdPos, aimPos, picker.IsKeyDown(InputType.Aim), holdAngle);
+                picker.AnimController.HoldItem(deltaTime, item, handlePos, holdPos, aimPos, picker.IsKeyDown(InputType.Aim) && aimPos != Vector2.Zero, holdAngle);
             }
             else
             {
