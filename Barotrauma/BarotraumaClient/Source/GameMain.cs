2f6cb84b5e238c034801513128f216568c828ceb
juanjp600 <juanjprot@gmail.com>
2018-08-17 19:26:24 -0300

    Added workaround to tabbing out breaking fullscreen

--- Barotrauma/BarotraumaClient/Source/GameMain.cs
+++ Barotrauma/BarotraumaClient/Source/GameMain.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using GameAnalyticsSDK.Net;
+using System.Threading;
 
 namespace Barotrauma
 {
@@ -74,7 +75,8 @@ namespace Barotrauma
             get;
             private set;
         }
-        
+        private static bool FullscreenOnTabIn;
+
         public static WindowMode WindowMode
         {
             get;
@@ -231,7 +233,36 @@ namespace Barotrauma
             TitleScreen = new LoadingScreen(GraphicsDevice);
 
             loadingCoroutine = CoroutineManager.StartCoroutine(Load());
+
+            var myForm = (System.Windows.Forms.Form)System.Windows.Forms.Form.FromHandle(Window.Handle);
+            myForm.Deactivate += new EventHandler(HandleDefocus);
+            myForm.Activated += new EventHandler(HandleFocus);
         }
+
+        private void HandleDefocus(object sender, EventArgs e)
+        {
+            if (GraphicsDeviceManager.IsFullScreen && GraphicsDeviceManager.HardwareModeSwitch)
+            {
+                GraphicsDeviceManager.IsFullScreen = false;
+                GraphicsDeviceManager.ApplyChanges();
+                FullscreenOnTabIn = true;
+                Thread.Sleep(500);
+            }
+        }
+
+        private void HandleFocus(object sender, EventArgs e)
+        {
+            if (FullscreenOnTabIn)
+            {
+                GraphicsDeviceManager.HardwareModeSwitch = true;
+                GraphicsDeviceManager.IsFullScreen = true;
+                GraphicsDeviceManager.ApplyChanges();
+                FullscreenOnTabIn = false;
+                Thread.Sleep(500);
+            }
+        }
+
+
 
         private void InitUserStats()
         {
