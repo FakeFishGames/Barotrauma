c6cafd4887668134e2999d03ab079c18a92d8a34
Joonas Rikkonen <poe.regalis@gmail.com>
2018-08-23 12:37:20 +0300

    Whoops

--- Barotrauma/BarotraumaShared/Source/Items/Components/ItemComponent.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Components/ItemComponent.cs
@@ -586,13 +586,11 @@ namespace Barotrauma.Items.Components
             string msg = TextManager.Get(Msg, true);
             if (msg != null)
             {
-
                 foreach (InputType inputType in Enum.GetValues(typeof(InputType)))
                 {
                     msg = msg.Replace("[" + inputType.ToString().ToLowerInvariant() + "]", GameMain.Config.KeyBind(inputType).ToString());
                 }
-                ri.Msg = msg;
-
+                Msg = msg;
             }
 #endif
             List<RelatedItem> prevRequiredItems = new List<RelatedItem>(requiredItems);
