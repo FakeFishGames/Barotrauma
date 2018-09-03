087fc5ea04c0250f9a5f763a86606eba82feac0e
itchyOwl <lauri.harkanen@gmail.com>
2018-09-03 16:01:14 +0300

    Add a function for checking the floating point equality.

--- Barotrauma/BarotraumaShared/Source/Utils/MathUtils.cs
+++ Barotrauma/BarotraumaShared/Source/Utils/MathUtils.cs
@@ -576,6 +576,33 @@ namespace Barotrauma
                 }
             }*/
         }
+
+        /// <summary>
+        /// Float comparison. Note that may still fail in some cases.
+        /// </summary>
+        // References: 
+        // http://floating-point-gui.de/errors/comparison/
+        // https://stackoverflow.com/questions/3874627/floating-point-comparison-functions-for-c-sharp
+        public static bool NearlyEqual(float a, float b, float epsilon = 0.0001f)
+        {
+            float diff = Math.Abs(a - b);
+            if (a == b)
+            {
+                // shortcut, handles infinities
+                return true;
+            }
+            else if (a == 0 || b == 0 || diff < float.Epsilon)
+            {
+                // a or b is zero or both are extremely close to it
+                // relative error is less meaningful here
+                return diff < epsilon;
+            }
+            else
+            {
+                // use relative error
+                return diff / (Math.Abs(a) + Math.Abs(b)) < epsilon;
+            }
+        }
     }
 
     class CompareCCW : IComparer<Vector2>
