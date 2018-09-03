f636383207264e3ba2ebc5875d01f5324d692e75
itchyOwl <lauri.harkanen@gmail.com>
2018-09-03 16:07:30 +0300

    Move the extension methods from the Client project into the Shared project. Add methods that were missing in the dev branch.

---
+++ Barotrauma/BarotraumaShared/Source/Extensions/StringFormatter.cs
@@ -0,0 +1,62 @@
+﻿using System.Globalization;
+using Microsoft.Xna.Framework;
+
+namespace Barotrauma
+{
+    public static class StringFormatter
+    {
+        /// <summary>
+        /// Formats the value with one decimal.
+        /// </summary>
+        public static string FormatAsSingleDecimal(this float value)
+        {
+            return value.ToString("0.#", CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Formats the value with two decimals.
+        /// </summary>
+        public static string FormatAsDoubleDecimal(this float value)
+        {
+            return value.ToString("0.##", CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Converts first to int, then to string.
+        /// </summary>
+        public static string FormatAsInt(this float value)
+        {
+            int v = (int)value;
+            return v.ToString();
+        }
+
+        /// <summary>
+        /// Formats the value with one decimal.
+        /// </summary>
+        public static string FormatAsSingleDecimal(this Vector2 value)
+        {
+            return $"({value.X.FormatAsSingleDecimal()}, {value.Y.FormatAsSingleDecimal()})";
+        }
+
+        /// <summary>
+        /// Formats the value with two decimals.
+        /// </summary>
+        public static string FormatAsDoubleDecimal(this Vector2 value)
+        {
+            return $"({value.X.FormatAsDoubleDecimal()}, {value.Y.FormatAsDoubleDecimal()})";
+        }
+
+        /// <summary>
+        /// Formats the value with no decimals.
+        /// </summary>
+        public static string FormatAsZeroDecimal(this Vector2 value)
+        {
+            return $"({value.X.FormatAsInt()}, {value.Y.FormatAsInt()})";
+        }
+
+        /// <summary>
+        /// Capitalises the first letter (invariant) and forces the rest to lower case (invariant).
+        /// </summary>
+        public static string CapitaliseFirstInvariant(this string s) => s.Substring(0, 1).ToUpperInvariant() + s.Substring(1, s.Length - 1).ToLowerInvariant();
+    }
+}
