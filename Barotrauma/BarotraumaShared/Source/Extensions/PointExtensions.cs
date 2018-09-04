dd1fab10b9820d36b2ff9a04c7b58a938aa4d7fa
itchyOwl <lauri.harkanen@gmail.com>
2018-09-04 14:01:23 +0300

    Add some Point calculations. Fix potential division by zero exceptions.

--- Barotrauma/BarotraumaShared/Source/Extensions/PointExtensions.cs
+++ Barotrauma/BarotraumaShared/Source/Extensions/PointExtensions.cs
@@ -9,18 +9,31 @@ namespace Barotrauma.Extensions
             return new Point((int)(p.X * f), (int)(p.Y * f));
         }
 
+        public static Point Multiply(this Point p, int i)
+        {
+            return new Point(p.X * i, p.Y * i);
+        }
+
         public static Point Multiply(this Point p, Vector2 v)
         {
             return new Point((int)(p.X * v.X), (int)(p.Y * v.Y));
         }
 
+        public static Point Divide(this Point p, int i)
+        {
+            if (i == 0) { return Point.Zero; }
+            return new Point(p.X / i, p.Y / i);
+        }
+
         public static Point Divide(this Point p, float f)
         {
+            if (f == 0) { return Point.Zero; }
             return new Point((int)(p.X / f), (int)(p.Y / f));
         }
 
         public static Point Divide(this Point p, Vector2 v)
         {
+            if (v.X == 0 || v.Y == 0) { return Point.Zero; }
             return new Point((int)(p.X / v.X), (int)(p.Y / v.Y));
         }
 
