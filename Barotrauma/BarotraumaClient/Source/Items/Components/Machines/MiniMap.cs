912565e46a0f30474a865e26c6ba06b0aad7cc48
Joonas Rikkonen <poe.regalis@gmail.com>
2018-09-05 19:12:21 +0300

    Status monitors use the Submarine.CreateMiniMap method to create the minimap.

--- Barotrauma/BarotraumaClient/Source/Items/Components/Machines/MiniMap.cs
+++ Barotrauma/BarotraumaClient/Source/Items/Components/Machines/MiniMap.cs
@@ -8,10 +8,13 @@ namespace Barotrauma.Items.Components
 {
     partial class MiniMap : Powered
     {
+        private GUIFrame submarineContainer;
+
         partial void InitProjSpecific(XElement element)
         {
             new GUICustomComponent(new RectTransform(new Vector2(0.9f, 0.85f), GuiFrame.RectTransform, Anchor.Center),
                 DrawHUD, null);
+            submarineContainer = new GUIFrame(new RectTransform(new Vector2(0.9f, 0.85f), GuiFrame.RectTransform, Anchor.Center), style: null);
         }
 
         public override void AddToGUIUpdateList()
@@ -19,17 +22,72 @@ namespace Barotrauma.Items.Components
             GuiFrame.AddToGUIUpdateList();
         }
 
+        public override void OnMapLoaded()
+        {
+            base.OnMapLoaded();
+            CreateHUD();
+        }
+
+        private void CreateHUD()
+        {
+            submarineContainer.ClearChildren();
+            item.Submarine.CreateMiniMap(submarineContainer);
+        }
+
         private void DrawHUD(SpriteBatch spriteBatch, GUICustomComponent container)
         {
             if (item.Submarine == null) return;
-
-            int width = container.Rect.Width, height = container.Rect.Height;
-            int x = container.Rect.X;
-            int y = container.Rect.Y;
-
+            
             if (!hasPower) return;
 
-            Rectangle miniMap = new Rectangle(x + 20, y + 40, width - 40, height - 60);
+            foreach (Hull hull in Hull.hullList)
+            {
+                var hullFrame = submarineContainer.Children.First().FindChild(hull);
+                if (hullFrame == null) continue;
+
+                hullDatas.TryGetValue(hull, out HullData hullData);
+
+                Color borderColor = Color.DarkCyan;
+
+                float? gapOpenSum = 0.0f;
+                if (ShowHullIntegrity)
+                {
+                    gapOpenSum = hull.ConnectedGaps.Where(g => !g.IsRoomToRoom).Sum(g => g.Open);
+                    borderColor = Color.Lerp(Color.DarkCyan, Color.Red, Math.Min((float)gapOpenSum, 1.0f));
+                }
+
+                float? oxygenAmount = null;
+                if (!RequireOxygenDetectors || hullData?.Oxygen != null)
+                {
+                    oxygenAmount = RequireOxygenDetectors ? hullData.Oxygen : hull.OxygenPercentage;
+                    GUI.DrawRectangle(spriteBatch, hullFrame.Rect, Color.Lerp(Color.Red * 0.5f, Color.Green * 0.3f, (float)oxygenAmount / 100.0f), true);
+                }
+
+                float? waterAmount = null;
+                if (!RequireWaterDetectors || hullData.Water != null)
+                {
+                    waterAmount = RequireWaterDetectors ? hullData.Water : Math.Min(hull.WaterVolume / hull.Volume, 1.0f);
+                    if (hullFrame.Rect.Height * waterAmount > 3.0f)
+                    {
+                        Rectangle waterRect = new Rectangle(
+                            hullFrame.Rect.X, (int)(hullFrame.Rect.Y + hullFrame.Rect.Height * (1.0f - waterAmount)),
+                            hullFrame.Rect.Width, (int)(hullFrame.Rect.Height * waterAmount));
+
+                        waterRect.Inflate(-3, -3);
+
+                        GUI.DrawRectangle(spriteBatch, waterRect, new Color(85, 136, 147), true);
+                        GUI.DrawLine(spriteBatch, new Vector2(waterRect.X, waterRect.Y), new Vector2(waterRect.Right, waterRect.Y), Color.LightBlue);
+                    }
+                }
+
+                if (hullFrame.Rect.Contains(PlayerInput.MousePosition))
+                {
+                    borderColor = Color.Lerp(borderColor, Color.White, 0.5f);
+                }
+                hullFrame.Color = borderColor;
+            }
+
+            /*Rectangle miniMap = new Rectangle(x + 20, y + 40, width - 40, height - 60);
             float size = Math.Min(miniMap.Width / (float)item.Submarine.Borders.Width, miniMap.Height / (float)item.Submarine.Borders.Height);
             foreach (Hull hull in Hull.hullList)
             {
@@ -128,7 +186,7 @@ namespace Barotrauma.Items.Components
                 }
 
                 GUI.DrawRectangle(spriteBatch, hullRect, borderColor, false, 0.0f, 2);
-            }
+            }*/
         }
     }
 }
