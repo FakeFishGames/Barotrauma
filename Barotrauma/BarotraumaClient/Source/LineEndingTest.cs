c0e2e4ac12efc91d19e44776eb1cf441036e7aaa
itchyOwl <lauri.harkanen@gmail.com>
2018-09-06 12:14:51 +0300

    LineEndingTest

---
+++ Barotrauma/BarotraumaClient/Source/LineEndingTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barotrauma
+{
+    class LineEndingTest
+    {
+        public int testField = 1;
+
+        public void TestFunction()
+        {
+
+        }
+    }
+}
