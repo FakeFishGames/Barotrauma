78f716555bc800e68c5ca545c85a1901e008d1df
juanjp600 <juanjprot@gmail.com>
2018-09-26 23:57:47 -0300

    GUIScrollBar.BarScrollValue
    Doing the conversion to and from the 0-1 range in the ServerSettings class was going to be a problem so it's handled by GUIScrollBar instead

--- Barotrauma/BarotraumaShared/Source/Networking/ServerSettings.cs
+++ Barotrauma/BarotraumaShared/Source/Networking/ServerSettings.cs
@@ -76,7 +76,69 @@ namespace Barotrauma.Networking
                 this.property = property;
                 this.typeString = typeString;
             }
+            
+            public void Read(NetBuffer msg)
+            {
+                long oldPos = msg.Position;
+                UInt32 size = msg.ReadVariableUInt32();
 
+                float x; float y; float z; float w;
+                byte r; byte g; byte b; byte a;
+                int ix; int iy; int width; int height;
+
+                switch (typeString)
+                {
+                    case "float":
+                        if (size != 4) break;
+                        property.SetValue(msg.ReadFloat());
+                        return;
+                    case "vector2":
+                        if (size != 8) break;
+                        x = msg.ReadFloat();
+                        y = msg.ReadFloat();
+                        property.SetValue(new Vector2(x, y));
+                        return;
+                    case "vector3":
+                        if (size != 12) break;
+                        x = msg.ReadFloat();
+                        y = msg.ReadFloat();
+                        z = msg.ReadFloat();
+                        property.SetValue(new Vector3(x, y, z));
+                        return;
+                    case "vector4":
+                        if (size != 16) break;
+                        x = msg.ReadFloat();
+                        y = msg.ReadFloat();
+                        z = msg.ReadFloat();
+                        w = msg.ReadFloat();
+                        property.SetValue(new Vector4(x, y, z, w));
+                        return;
+                    case "color":
+                        if (size != 4) break;
+                        r = msg.ReadByte();
+                        g = msg.ReadByte();
+                        b = msg.ReadByte();
+                        a = msg.ReadByte();
+                        property.SetValue(new Color(r, g, b, a));
+                        return;
+                    case "rectangle":
+                        if (size != 16) break;
+                        ix = msg.ReadInt32();
+                        iy = msg.ReadInt32();
+                        width = msg.ReadInt32();
+                        height = msg.ReadInt32();
+                        property.SetValue(new Rectangle(ix, iy, width, height));
+                        return;
+                    default:
+                        msg.Position = oldPos; //reset position to properly read the string
+                        string incVal = msg.ReadString();
+                        property.TrySetValue(incVal);
+                        return;
+                }
+
+                //size didn't match: skip this
+                msg.Position += 8 * size;
+            }
 
             public void Write(NetBuffer msg)
             {
