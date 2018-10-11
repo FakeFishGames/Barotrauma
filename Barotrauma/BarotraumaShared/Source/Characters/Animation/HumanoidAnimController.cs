04109bcf360c8a7ef93a4ba1da98084fec3344c3
itchyOwl <lauri.harkanen@gmail.com>
2018-10-11 12:10:32 +0300

    Use the cycle speed as a total cycle multiplier for humanoid swim animations. Sync movement speed and cycle speed by default. Manipulating the movement speed widget also manipulates the cycle speed. The values can be adjusted separately either from the params editor or by using the cycle widget (alt).

--- Barotrauma/BarotraumaShared/Source/Characters/Animation/HumanoidAnimController.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/Animation/HumanoidAnimController.cs
@@ -898,11 +898,11 @@ namespace Barotrauma
                 }
                 else
                 {
-                    thigh.body.SmoothRotate(torso.Rotation + (float)Math.Sin(legCyclePos / CurrentSwimParams.LegCycleLength) * i * 0.3f, 2.0f);
+                    thigh.body.SmoothRotate(torso.Rotation + (float)Math.Sin(legCyclePos / CurrentSwimParams.LegCycleLength) * i * 0.3f * CurrentAnimationParams.CycleSpeed, 2.0f);
                 }
             }
 
-            Vector2 transformedFootPos = new Vector2((float)Math.Sin(legCyclePos / CurrentSwimParams.LegCycleLength) * CurrentSwimParams.LegMoveAmount, 0.0f);
+            Vector2 transformedFootPos = new Vector2((float)Math.Sin(legCyclePos / CurrentSwimParams.LegCycleLength) * CurrentSwimParams.LegMoveAmount * CurrentAnimationParams.CycleSpeed, 0.0f);
             transformedFootPos = Vector2.Transform(transformedFootPos, Matrix.CreateRotationZ(Collider.Rotation));
 
             MoveLimb(rightFoot, footPos - transformedFootPos, 1.0f);
@@ -939,8 +939,8 @@ namespace Barotrauma
             // Not sure why the params has to be flipped, but it works.
             var handMoveAmount = CurrentSwimParams.HandMoveAmount.Flip();
             var handMoveOffset = CurrentSwimParams.HandMoveOffset.Flip();
-            float handPosX = (float)Math.Cos(handCyclePos) * handMoveAmount.X;
-            float handPosY = (float)Math.Sin(handCyclePos) * handMoveAmount.Y;
+            float handPosX = (float)Math.Cos(handCyclePos) * handMoveAmount.X * CurrentAnimationParams.CycleSpeed;
+            float handPosY = (float)Math.Sin(handCyclePos) * handMoveAmount.Y * CurrentAnimationParams.CycleSpeed;
             //handPosY = MathHelper.Clamp(handPosY, -0.8f, 0.8f);
 
             Matrix rotationMatrix = Matrix.CreateRotationZ(torso.Rotation);
