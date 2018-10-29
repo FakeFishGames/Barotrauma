fe8319331a063a19232ab7910a002f759566f493
Joonas Rikkonen <poe.regalis@gmail.com>
2018-10-29 13:02:18 +0200

    Fixed particles not appearing in the particle editor if the gamescreen camera has been moved

--- Barotrauma/BarotraumaClient/Source/Items/Components/Repairable.cs
+++ Barotrauma/BarotraumaClient/Source/Items/Components/Repairable.cs
@@ -1,13 +1,7 @@
-﻿using Barotrauma.Networking;
-using Barotrauma.Particles;
-using Lidgren.Network;
+﻿using Barotrauma.Particles;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace Barotrauma.Items.Components
