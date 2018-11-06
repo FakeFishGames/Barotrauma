ae8e10dbc707806edee63e8e12591b6feba51d63
Joonas Rikkonen <poe.regalis@gmail.com>
2018-11-06 14:42:59 +0200

    Aim assist strength can be adjusted in the settings

--- Barotrauma/BarotraumaShared/Source/Characters/Character.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/Character.cs
@@ -1567,8 +1567,8 @@ namespace Barotrauma
                 (isLocalPlayer && (findFocusedTimer <= 0.0f || Screen.Selected == GameMain.SubEditorScreen)))
             {
                 focusedCharacter = FindCharacterAtPosition(mouseSimPos);
-                //TODO: allow players to change the amount of aim assist?
-                focusedItem = CanInteract ? FindItemAtPosition(mouseSimPos, AnimController.InWater ? 0.6f : 0.3f) : null;
+                focusedItem = CanInteract ? 
+                    FindItemAtPosition(mouseSimPos, GameMain.Config.AimAssistAmount * (AnimController.InWater ? 1.5f : 1.0f)) : null;
                 
                 findFocusedTimer = 0.05f;
             }
