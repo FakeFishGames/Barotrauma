faa233511d7a35d9b38b97214f9e655a8b764b63
Joonas Rikkonen <poe.regalis@gmail.com>
2018-11-19 14:46:40 +0200

    Fixed equipped items occasionally messing up the order of the inventory after a reload. Closes #918

--- Barotrauma/BarotraumaShared/Source/Characters/Character.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/Character.cs
@@ -725,18 +725,24 @@ namespace Barotrauma
                         
             if (file == humanConfigFile && Info.PickedItemIDs.Any())
             {
-                for (ushort i = 0; i < Info.PickedItemIDs.Count; i++ )
+                for (int j = 0; j < 2; j++)
                 {
-                    if (Info.PickedItemIDs[i] == 0) continue;
+                    for (ushort i = 0; i < Info.PickedItemIDs.Count; i++)
+                    {
+                        //put items in the "Any" slots first, 
+                        //otherwise equipped items may end up taking up "Any" slots they shouldn't be in, 
+                        //and prevent other items from fitting in the inventory
+                        if ((Inventory.SlotTypes[i] == InvSlotType.Any) != (j == 0)) continue;
+                        if (Info.PickedItemIDs[i] == 0) continue;
 
-                    Item item = FindEntityByID(Info.PickedItemIDs[i]) as Item;
+                        Item item = FindEntityByID(Info.PickedItemIDs[i]) as Item;
+                        System.Diagnostics.Debug.Assert(item != null);
+                        if (item == null) continue;
 
-                    System.Diagnostics.Debug.Assert(item != null);
-                    if (item == null) continue;
-
-                    item.TryInteract(this, true, true, true);
-                    Inventory.TryPutItem(item, i, false, false, null, false);
-                }                
+                        item.TryInteract(this, true, true, true);
+                        Inventory.TryPutItem(item, i, false, false, null, false);
+                    }
+                }
             }
 
             AnimController.FindHull(null);
