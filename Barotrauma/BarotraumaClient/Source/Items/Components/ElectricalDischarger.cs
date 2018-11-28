01bcf469bbd58f82928579c4235f8ae4cde11d7c
Joonas Rikkonen <poe.regalis@gmail.com>
2018-11-28 10:01:58 +0200

    Added knockback to discharger

--- Barotrauma/BarotraumaClient/Source/Items/Components/ElectricalDischarger.cs
+++ Barotrauma/BarotraumaClient/Source/Items/Components/ElectricalDischarger.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace Barotrauma.Items.Components
 {
