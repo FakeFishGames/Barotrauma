40d19de0f009ea2566db59f5c4040ff870a72d7a
Joonas Rikkonen <poe.regalis@gmail.com>
2018-12-06 21:50:46 +0200

    Some additional features to item DecorativeSprites: - Optional offset & rotation, can be animated by multiplying with a since curve or a noise value. - Option to activate specific sprites using PropertyConditionals (e.g. change the sprite when some value is below some threshold or when some component is active).

--- Barotrauma/BarotraumaClient/Source/Items/ItemPrefab.cs
+++ Barotrauma/BarotraumaClient/Source/Items/ItemPrefab.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace Barotrauma
 {
@@ -22,8 +23,120 @@ namespace Barotrauma
 
     partial class ItemPrefab : MapEntityPrefab
     {
+        public class DecorativeSprite
+        {
+            public Sprite Sprite { get; private set; }
+
+            public enum AnimationType
+            {
+                None,
+                Sine,
+                Noise
+            }
+
+            [Serialize("0,0", false)]
+            public Vector2 Offset { get; private set; }
+
+            [Serialize(AnimationType.None, false)]
+            public AnimationType OffsetAnim { get; private set; }
+
+            [Serialize(0.0f, false)]
+            public float OffsetAnimSpeed { get; private set; }
+
+            private float rotationSpeedRadians;
+            [Serialize(0.0f, false)]
+            public float RotationSpeed
+            {
+                get
+                {
+                    return MathHelper.ToDegrees(rotationSpeedRadians);
+                }
+                private set
+                {
+                    rotationSpeedRadians = MathHelper.ToRadians(value);
+                }
+            }
+
+            [Serialize(0.0f, false)]
+            public float Rotation { get; private set; }
+
+            [Serialize(AnimationType.None, false)]
+            public AnimationType RotationAnim { get; private set; }
+
+            /// <summary>
+            /// The sprite is only drawn if these conditions are fulfilled
+            /// </summary>
+            public List<PropertyConditional> Conditionals { get; private set; } = new List<PropertyConditional>();
+
+            public DecorativeSprite(XElement element, string path = "")
+            {
+                Sprite = new Sprite(element, path);
+                SerializableProperty.DeserializeProperties(this, element);
+
+                foreach (XElement subElement in element.Elements())
+                {
+                    if (subElement.Name.ToString().ToLowerInvariant() == "conditional")
+                    {
+                        foreach (XAttribute attribute in subElement.Attributes())
+                        {
+                            if (attribute.Name.ToString().ToLowerInvariant() == "targetitemcomponent") { continue; }
+                            Conditionals.Add(new PropertyConditional(attribute));
+                        }
+                    }
+                }
+            }
+
+            public Vector2 GetOffset(ref float offsetState)
+            {
+                if (OffsetAnimSpeed <= 0.0f)
+                {
+                    return Offset;
+                }
+                switch (OffsetAnim)
+                {                    
+                    case AnimationType.Sine:
+                        offsetState = offsetState % (MathHelper.TwoPi / OffsetAnimSpeed);
+                        return Offset * (float)Math.Sin(offsetState * OffsetAnimSpeed);
+                    case AnimationType.Noise:
+                        offsetState = offsetState % (1.0f / (OffsetAnimSpeed * 0.1f));
+
+                        float t = offsetState * 0.1f * OffsetAnimSpeed;
+                        return new Vector2(
+                            Offset.X * (PerlinNoise.GetPerlin(t, t) - 0.5f),
+                            Offset.Y * (PerlinNoise.GetPerlin(t + 0.5f, t + 0.5f) - 0.5f));
+                    default:
+                        return Offset;
+                }
+            }
+            
+            public float GetRotation(ref float rotationState)
+            {
+                if (rotationSpeedRadians <= 0.0f)
+                {
+                    return Rotation;
+                }
+                switch (OffsetAnim)
+                {
+                    case AnimationType.Sine:
+                        rotationState = rotationState % (MathHelper.TwoPi / rotationSpeedRadians);
+                        return Rotation * (float)Math.Sin(rotationState * rotationSpeedRadians);
+                    case AnimationType.Noise:
+                        rotationState = rotationState % (1.0f / rotationSpeedRadians);
+                        return Rotation * PerlinNoise.GetPerlin(rotationState * rotationSpeedRadians, rotationState * rotationSpeedRadians);
+                    default:
+                        return rotationState * rotationSpeedRadians;
+                }
+            }
+
+            public void Remove()
+            {
+                Sprite?.Remove();
+                Sprite = null;
+            }
+        }
+
         public List<BrokenItemSprite> BrokenSprites = new List<BrokenItemSprite>();
-        public List<Sprite> DecorativeSprites = new List<Sprite>();
+        public List<DecorativeSprite> DecorativeSprites = new List<DecorativeSprite>();
         public Sprite InventoryIcon;
 
         //only used to display correct color in the sub editor, item instances have their own property that can be edited on a per-item basis
