FakeFishGames/Barotrauma

commit fd7938c06cfbf178de953bc3bbd4dfff26871b9b

Fixed room names not being saved

Barotrauma/BarotraumaShared/Source/Map/Hull.cs
@@ -926,11 +926,15 @@ namespace Barotrauma
                     int.Parse(element.Attribute("height").Value));
             }
 
-            return new Hull(MapEntityPrefab.Find(null, "hull"), rect, submarine)
+            var hull = new Hull(MapEntityPrefab.Find(null, "hull"), rect, submarine)
             {
                 waterVolume = element.GetAttributeFloat("pressure", 0.0f),
                 ID = (ushort)int.Parse(element.Attribute("ID").Value)
             };
+            
+            SerializableProperty.DeserializeProperties(hull, element);
+
+            return hull;
         }
 
         public override XElement Save(XElement parentElement)
@@ -953,9 +957,8 @@ namespace Barotrauma
                     rect.Width + "," + rect.Height),
                 new XAttribute("water", waterVolume)
             );
-
+            SerializableProperty.SerializeProperties(this, element);
             parentElement.Add(element);
-
             return element;
         }
 

commit 0315d6b964ad4665ee848155b91c0a07188827ff

Fixed hulls being almost completely out of oxygen when starting the game. Caused by fd7938c - the oxygen level wasn't saved before, so the game would set it to the (very low) default value.

Barotrauma/BarotraumaShared/Source/Map/Hull.cs
@@ -152,7 +152,7 @@ namespace Barotrauma
             }
         }
 
-        [Serialize(90.0f, true)]
+        [Serialize(100000.0f, true)]
         public float Oxygen
         {
             get { return oxygen; }
@@ -933,6 +933,7 @@ namespace Barotrauma
             };
             
             SerializableProperty.DeserializeProperties(hull, element);
+            if (element.Attribute("oxygen") == null) { hull.Oxygen = hull.Volume; }
 
             return hull;
         }