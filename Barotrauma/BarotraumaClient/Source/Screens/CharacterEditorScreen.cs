038052233b73d0c9b7e90d437a31e1f33a439354
itchyOwl <lauri.harkanen@gmail.com>
2018-12-21 15:41:20 +0200

    Define a character info also for ragdolls when the job is not defined -> causes the head attachments to be chosen.

--- Barotrauma/BarotraumaClient/Source/Screens/CharacterEditorScreen.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CharacterEditorScreen.cs
@@ -1030,7 +1030,7 @@ namespace Barotrauma
             }
             else
             {
-                character = Character.Create(configFile, spawnPosition, ToolBox.RandomSeed(8), hasAi: false, ragdoll: ragdoll);
+                character = Character.Create(configFile, spawnPosition, ToolBox.RandomSeed(8), new CharacterInfo(configFile), hasAi: false, ragdoll: ragdoll);
                 selectedJob = null;
             }
             character.dontFollowCursor = dontFollowCursor;
