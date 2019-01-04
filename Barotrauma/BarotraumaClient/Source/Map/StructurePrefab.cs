49cefda59e3e0be3a721776d0fa851114b6a4003
itchyOwl <lauri.harkanen@gmail.com>
2019-01-04 15:44:24 +0200

    Fix scaling not taken into account when drawing structures. Use DrawTiled method only.

--- Barotrauma/BarotraumaClient/Source/Map/StructurePrefab.cs
+++ Barotrauma/BarotraumaClient/Source/Map/StructurePrefab.cs
@@ -20,21 +20,14 @@ namespace Barotrauma
             }
             else
             {
-                Vector2 placeSize = size;
+                Vector2 placeSize = ScaledSize;
                 if (ResizeHorizontal) placeSize.X = position.X - placePosition.X;
                 if (ResizeVertical) placeSize.Y = placePosition.Y - position.Y;
 
                 newRect = Submarine.AbsRect(placePosition, placeSize);
             }
 
-            if (ResizeHorizontal || ResizeVertical)
-            {
-                sprite.DrawTiled(spriteBatch, new Vector2(newRect.X, -newRect.Y), new Vector2(newRect.Width, newRect.Height), textureScale: TextureScale * Scale);
-            }
-            else
-            {
-                sprite.Draw(spriteBatch, new Vector2(newRect.X, -newRect.Y), Color.White, Vector2.Zero, 0, Scale);
-            }
+            sprite.DrawTiled(spriteBatch, new Vector2(newRect.X, -newRect.Y), new Vector2(newRect.Width, newRect.Height), textureScale: TextureScale * Scale);
             GUI.DrawRectangle(spriteBatch, new Rectangle(newRect.X - GameMain.GraphicsWidth, -newRect.Y, newRect.Width + GameMain.GraphicsWidth * 2, newRect.Height), Color.White);
             GUI.DrawRectangle(spriteBatch, new Rectangle(newRect.X, -newRect.Y - GameMain.GraphicsHeight, newRect.Width, newRect.Height + GameMain.GraphicsHeight * 2), Color.White);
         }
