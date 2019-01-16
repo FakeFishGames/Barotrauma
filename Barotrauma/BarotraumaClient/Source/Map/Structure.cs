77e1075694a8b22d2b5220cd7a4c2d105ec3a5bd
itchyOwl <lauri.harkanen@gmail.com>
2019-01-16 11:43:14 +0200

    Fix the highlight color still affecting tiled structures when they are selected.

--- Barotrauma/BarotraumaClient/Source/Map/Structure.cs
+++ Barotrauma/BarotraumaClient/Source/Map/Structure.cs
@@ -184,7 +184,7 @@ namespace Barotrauma
                 if (HasBody && !ShowWalls) return;
             }
 
-            Color color = (isHighlighted) ? Color.Orange : spriteColor;
+            Color color = (isHighlighted && (!IsSelected || !editing)) ? Color.Orange : spriteColor;
             if (IsSelected && editing)
             {
                 //color = Color.Lerp(color, Color.Gold, 0.5f);
