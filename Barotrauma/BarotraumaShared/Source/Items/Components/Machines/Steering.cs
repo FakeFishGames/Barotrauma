f46e70485af2576747a432487529cc54eef42572
Joonas Rikkonen <poe.regalis@gmail.com>
2019-01-18 12:43:39 +0200

    Fixed sonar creating excessive amounts of network events (every frame when cursor was on the display), reduced the interval at which steering events are created

--- Barotrauma/BarotraumaShared/Source/Items/Components/Machines/Steering.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Machines/Steering.cs
@@ -174,9 +174,9 @@ namespace Barotrauma.Items.Components
 
         public override void Update(float deltaTime, Camera cam)
         {
+            networkUpdateTimer -= deltaTime;
             if (unsentChanges)
             {
-                networkUpdateTimer -= deltaTime;
                 if (networkUpdateTimer <= 0.0f)
                 {
 #if CLIENT
@@ -192,7 +192,7 @@ namespace Barotrauma.Items.Components
                         item.CreateServerEvent(this);
                     }
 
-                    networkUpdateTimer = 0.5f;
+                    networkUpdateTimer = 0.1f;
                     unsentChanges = false;
                 }
             }
