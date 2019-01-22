242e2429fd2c3ed199ac26b55e2cbdc8636e73f9
Darkwolf <Darkwolf0101@gmail.com>
2019-01-21 21:26:57 -0600

    Expansion of Barotrauma's logic system.
    Changed:
    - AdderComponent and children can clamp their output
    - Powercontainer signals for charge,charge% and charge rate
    
    Added:
    - ColorComponent: Dynamic signals for light set_color inputs
    - MemoryComponent: Stores and sends a signal that is edge latched
    - DivideComponent: Standard division
    - MultiplyComponent: Standard multiplication
    - SubtractComponent: Standard subtraction
    - XorComponent: Exclusive or
    - EqualsComponent: Equals comparison
    - GreaterComponent: Greater than comparison

---
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Signal/XorComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Linq;
+
+namespace Barotrauma.Items.Components
+{
+    class XorComponent : AndComponent
+    {
+        public XorComponent(Item item, XElement element)
+            : base(item, element)
+        {
+            IsActive = true;
+        }
+
+        public override void Update(float deltaTime, Camera cam)
+        {
+            int sendOutput = 0;
+            for (int i = 0; i < timeSinceReceived.Length; i++)
+            {
+                if (timeSinceReceived[i] <= timeFrame) sendOutput += 1;
+                timeSinceReceived[i] += deltaTime;
+            }
+
+            string signalOut = sendOutput == 1 ? output : falseOutput;
+            if (string.IsNullOrEmpty(signalOut)) return;
+
+            item.SendSignal(0, signalOut, "signal_out", null);
+        }
+    }
+}
