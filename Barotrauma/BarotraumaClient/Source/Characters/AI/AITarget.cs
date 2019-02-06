6d3a92c6f0ac570dc4f69687c1f58bf418646c79
itchyOwl <lauri.harkanen@gmail.com>
2019-02-06 12:42:57 +0200

    Adjust the ai debug info.

--- Barotrauma/BarotraumaClient/Source/Characters/AI/AITarget.cs
+++ Barotrauma/BarotraumaClient/Source/Characters/AI/AITarget.cs
@@ -13,11 +13,13 @@ namespace Barotrauma
             var pos = new Vector2(WorldPosition.X, -WorldPosition.Y);
             if (soundRange > 0.0f)
             {
-                ShapeExtensions.DrawCircle(spriteBatch, pos, SoundRange, 100, Color.CornflowerBlue, thickness: 1 / Screen.Selected.Cam.Zoom);
+                Color color = Entity is Character ? Color.Yellow : Color.Orange;
+                ShapeExtensions.DrawCircle(spriteBatch, pos, SoundRange, 100, color, thickness: 1 / Screen.Selected.Cam.Zoom);
             }
             if (sightRange > 0.0f)
             {
-                ShapeExtensions.DrawCircle(spriteBatch, pos, SightRange, 100, Color.Orange, thickness: 1 / Screen.Selected.Cam.Zoom);
+                Color color = Entity is Character ? Color.CornflowerBlue : Color.CadetBlue;
+                ShapeExtensions.DrawCircle(spriteBatch, pos, SightRange, 100, color, thickness: 1 / Screen.Selected.Cam.Zoom);
             }
         }
     }
