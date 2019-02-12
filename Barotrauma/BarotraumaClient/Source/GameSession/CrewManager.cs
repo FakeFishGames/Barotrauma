a575cce7f5bc58f87b2d57a2a7e59c3b1b3e71ef
CommanderMark <markjarjour@gmail.com>
2019-02-12 15:03:41 -0500

    Chat input is wiped on deselect.

--- Barotrauma/BarotraumaClient/Source/GameSession/CrewManager.cs
+++ Barotrauma/BarotraumaClient/Source/GameSession/CrewManager.cs
@@ -4,6 +4,7 @@ using Barotrauma.Networking;
 using FarseerPhysics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -154,6 +155,10 @@ namespace Barotrauma
                         textbox.Text = "";
                         return true;
                     }
+                };
+                chatBox.InputBox.OnDeselected += (gui, Keys) =>
+                {
+                    this.chatBox.InputBox.Text = "";
                 };
                 chatBox.InputBox.OnTextChanged += chatBox.TypingChatMessage;
             }
