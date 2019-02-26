142fbbdbac3d9a2792aacb6ebec7f93bdc65f366
itchyOwl <lauri.harkanen@gmail.com>
2019-02-26 14:46:25 +0200

    Only seek the items of the current and connected subs.

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveRepairItems.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveRepairItems.cs
@@ -11,22 +11,26 @@ namespace Barotrauma
         public override string DebugTag => "repair items";
 
         private Dictionary<Item, AIObjectiveRepairItem> repairObjectives = new Dictionary<Item, AIObjectiveRepairItem>();
+        private readonly List<Item> itemList = new List<Item>();
 
         /// <summary>
         /// Should the character only attempt to fix items they have the skills to fix, or any damaged item
         /// </summary>
         public bool RequireAdequateSkills;
 
-        public AIObjectiveRepairItems(Character character) : base(character, "") { }
+        public AIObjectiveRepairItems(Character character) : base(character, "")
+        {
+            itemList = character.Submarine.GetItems(true);
+        }
 
         public override float GetPriority(AIObjectiveManager objectiveManager)
         {
             GetBrokenItems();
-            if (subObjectives.Any() && objectiveManager.CurrentOrder == this)
+            if (repairObjectives.None()) { return 0; }
+            else if (objectiveManager.CurrentOrder == this)
             {
                 return AIObjectiveManager.OrderPriority;
             }
-            if (repairObjectives.None()) { return 0; }
             // Don't use the itemlist, because it can be huge.
             float avg = repairObjectives.Average(ro => 100 - ro.Key.ConditionPercentage);
             return MathHelper.Lerp(0, 50, avg / 100);
@@ -40,8 +44,8 @@ namespace Barotrauma
 
         private void GetBrokenItems()
         {
-            SyncRemovedObjectives(repairObjectives, Item.ItemList);
-            foreach (Item item in Item.ItemList)
+            SyncRemovedObjectives(repairObjectives, itemList);
+            foreach (Item item in itemList)
             {
                 if (!item.IsFullCondition)
                 {
