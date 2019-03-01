f299f37d47ec60b0e23b2a78319a7348f9ce57a0
itchyOwl <lauri.harkanen@gmail.com>
2019-03-01 13:07:19 +0200

    Fix the charge batteries objective.

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveChargeBatteries.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveChargeBatteries.cs
@@ -10,7 +10,7 @@ namespace Barotrauma
     {
         public override string DebugTag => "charge batteries";
 
-        private List<PowerContainer> targets = new List<PowerContainer>();
+        private List<PowerContainer> batteries = new List<PowerContainer>();
         private Dictionary<PowerContainer, AIObjectiveOperateItem> chargeObjectives = new Dictionary<PowerContainer, AIObjectiveOperateItem>();
         private string orderOption;
         
@@ -19,15 +19,27 @@ namespace Barotrauma
             orderOption = option;
         }
 
+        public override void UpdatePriority(AIObjectiveManager objectiveManager, float deltaTime)
+        {
+            SyncRemovedObjectives(chargeObjectives, batteries);
+            if (batteries.None())
+            {
+                FindBatteries();
+            }
+            if (chargeObjectives.None())
+            {
+                CreateObjectives();
+            }
+        }
+
         public override float GetPriority(AIObjectiveManager objectiveManager)
         {
             if (character.Submarine == null) { return 0; }
-            if (targets.None()) { GetTargets(); }
-            if (targets.None()) { return 0; }
-            float avgNeedOfCharge = targets.Average(b => 100 - b.ChargePercentage);
-            if (objectiveManager.CurrentOrder == this && avgNeedOfCharge > 10)
+            if (batteries.None() || chargeObjectives.None()) { return 0; }
+            float avgNeedOfCharge = batteries.Average(b => 100 - b.ChargePercentage);
+            if (objectiveManager.CurrentOrder == this)
             {
-                return AIObjectiveManager.OrderPriority;
+                return AIObjectiveManager.OrderPriority - MathHelper.Max(0, AIObjectiveManager.OrderPriority - avgNeedOfCharge);
             }
             return MathHelper.Lerp(0, 50, avgNeedOfCharge / 100);
         }
@@ -35,19 +47,17 @@ namespace Barotrauma
         public override bool IsCompleted() => false;
         public override bool CanBeCompleted => true;
 
+        // TODO: currently there can be multiple objectives with different order options. We don't want that
         public override bool IsDuplicate(AIObjective otherObjective)
         {
             return otherObjective is AIObjectiveChargeBatteries other && other.orderOption == orderOption;
         }
 
-        protected override void Act(float deltaTime)
-        {
-            SyncRemovedObjectives(chargeObjectives, targets);
-        }
+        protected override void Act(float deltaTime) { }
 
-        private void GetTargets()
+        private void FindBatteries()
         {
-            targets.Clear();
+            batteries.Clear();
             foreach (Item item in Item.ItemList)
             {
                 if (item.Prefab.Identifier != "battery" && !item.HasTag("battery")) { continue; }
@@ -57,28 +67,32 @@ namespace Barotrauma
                     // If the character is inside, only take items in connected hulls into account.
                     if (character.Submarine != null && !character.Submarine.IsEntityFoundOnThisSub(item, true)) { continue; }
                     var battery = item.GetComponent<PowerContainer>();
-                    if (!targets.Contains(battery))
+                    if (!batteries.Contains(battery))
                     {
-                        targets.Add(battery);
-                        if (battery.ChargePercentage < 90)
-                        {
-                            if (!chargeObjectives.TryGetValue(battery, out AIObjectiveOperateItem objective))
-                            {
-                                objective = new AIObjectiveOperateItem(battery, character, orderOption, false);
-                                chargeObjectives.Add(battery, objective);
-                                AddSubObjective(objective);
-                            }
-                        }
+                        batteries.Add(battery);
                     }
                 }
             }
-            if (targets.None())
+            if (batteries.None())
             {
                 character.Speak(TextManager.Get("DialogNoBatteries"), null, 4.0f, "nobatteries", 10.0f);
             }
             else
             {
-                targets.Sort((x, y) => x.ChargePercentage.CompareTo(y.ChargePercentage));
+                batteries.Sort((x, y) => x.ChargePercentage.CompareTo(y.ChargePercentage));
+            }
+        }
+
+        private void CreateObjectives()
+        {
+            foreach (var battery in batteries)
+            {
+                if (!chargeObjectives.TryGetValue(battery, out AIObjectiveOperateItem objective))
+                {
+                    objective = new AIObjectiveOperateItem(battery, character, orderOption, false);
+                    chargeObjectives.Add(battery, objective);
+                    AddSubObjective(objective);
+                }
             }
         }
     }
