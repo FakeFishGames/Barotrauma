cb8e67c75ba969a1e879fb1c0f94239c5a408a1d
itchyOwl <lauri.harkanen@gmail.com>
2019-03-04 13:50:35 +0200

    Create a common abstract base class for charge batteries, fix leaks and repair items objectives to reduce the need for duplicate/triple logic.

---
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIMultiObjective.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Barotrauma.Extensions;
+using Microsoft.Xna.Framework;
+
+namespace Barotrauma
+{
+    abstract class AIMultiObjective<T> : AIObjective
+    {
+        protected List<T> targets = new List<T>();
+        protected Dictionary<T, AIObjective> objectives = new Dictionary<T, AIObjective>();
+        protected HashSet<T> ignoreList = new HashSet<T>();
+        protected readonly float ignoreListClearInterval = 30;
+        private float ignoreListTimer;
+        protected readonly float targetUpdateInterval = 2;
+        private float targetUpdateTimer;
+
+        public AIMultiObjective(Character character, string option) : base(character, option)
+        {
+            FindTargets();
+            CreateObjectives();
+        }
+
+        public override void UpdatePriority(AIObjectiveManager objectiveManager, float deltaTime)
+        {
+            base.UpdatePriority(objectiveManager, deltaTime);
+            if (ignoreListTimer > ignoreListClearInterval)
+            {
+                ignoreList.Clear();
+                ignoreListTimer = 0;
+                FindTargets();
+                CreateObjectives();
+            }
+            else
+            {
+                ignoreListTimer += deltaTime;
+            }
+            if (targetUpdateTimer > targetUpdateInterval)
+            {
+                targetUpdateTimer = 0;
+                FindTargets();
+            }
+            else
+            {
+                targetUpdateTimer += deltaTime;
+            }
+        }
+
+        public override float GetPriority(AIObjectiveManager objectiveManager)
+        {
+            if (character.Submarine == null) { return 0; }
+            if (targets.None()) { return 0; }
+            float avg = targets.Average(t => AverageFunction(t));
+            if (objectiveManager.CurrentOrder == this)
+            {
+                return AIObjectiveManager.OrderPriority - MathHelper.Max(0, AIObjectiveManager.OrderPriority - avg);
+            }
+            return MathHelper.Lerp(0, AIObjectiveManager.OrderPriority, avg / 100);
+        }
+
+        protected override void Act(float deltaTime)
+        {
+            foreach (var objective in objectives)
+            {
+                if (!objective.Value.CanBeCompleted)
+                {
+                    ignoreList.Add(objective.Key);
+                }
+            }
+            SyncRemovedObjectives(objectives, targets);
+            if (targets.None())
+            {
+                FindTargets();
+            }
+            if (objectives.None())
+            {
+                CreateObjectives();
+            }
+        }
+
+        public override bool IsCompleted() => false;
+        public override bool CanBeCompleted => true;
+
+        protected abstract void FindTargets();
+        protected abstract void CreateObjectives();
+        protected abstract float AverageFunction(T target);
+    }
+}
--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveRepairItems.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveRepairItems.cs
@@ -1,113 +1,62 @@
 ﻿using Barotrauma.Items.Components;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Barotrauma.Extensions;
 
 namespace Barotrauma
 {
-    class AIObjectiveRepairItems : AIObjective
+    class AIObjectiveRepairItems : AIMultiObjective<Item>
     {
         public override string DebugTag => "repair items";
-
-        private Dictionary<Item, AIObjectiveRepairItem> repairObjectives = new Dictionary<Item, AIObjectiveRepairItem>();
-        private HashSet<Item> ignoreList = new HashSet<Item>();
-        private readonly float ignoreListClearIntervalBase = 30;
-        private readonly float ignoreListIntervalAddition = 10;
-        private float ignoreListClearInterval;
-        private float ignoreListTimer;
 
         /// <summary>
         /// Should the character only attempt to fix items they have the skills to fix, or any damaged item
         /// </summary>
         public bool RequireAdequateSkills;
 
-        public AIObjectiveRepairItems(Character character) : base(character, "")
-        {
-            ignoreListClearInterval = ignoreListClearIntervalBase;
-        }
-
-        public override void UpdatePriority(AIObjectiveManager objectiveManager, float deltaTime)
-        {
-            base.UpdatePriority(objectiveManager, deltaTime);
-            if (ignoreListTimer > ignoreListClearInterval)
-            {
-                if (ignoreList.Any())
-                {
-                    // Increase the clear interval if there are items in the list -> reduces spam if items are added on the list over and over again.
-                    ignoreListClearInterval += ignoreListIntervalAddition;
-                }
-                else
-                {
-                    // Else reset the interval
-                    ignoreListClearInterval = ignoreListClearIntervalBase;
-                }
-                ignoreList.Clear();
-                ignoreListTimer = 0;
-                GetBrokenItems();
-            }
-            else
-            {
-                ignoreListTimer += deltaTime;
-            }
-            foreach (var repairObjective in repairObjectives)
-            {
-                if (!repairObjective.Value.CanBeCompleted)
-                {
-                    ignoreList.Add(repairObjective.Key);
-                }
-            }
-            SyncRemovedObjectives(repairObjectives, Item.ItemList);
-            if (repairObjectives.None())
-            {
-                GetBrokenItems();
-            }
-        }
-
-        public override float GetPriority(AIObjectiveManager objectiveManager)
-        {
-            if (character.Submarine == null) { return 0; }
-            if (repairObjectives.None()) { return 0; }
-            float avg = repairObjectives.Average(ro => 100 - ro.Key.ConditionPercentage);
-            if (objectiveManager.CurrentOrder == this)
-            {
-                return AIObjectiveManager.OrderPriority - MathHelper.Max(0, AIObjectiveManager.OrderPriority - avg);
-            }
-            return MathHelper.Lerp(0, 50, avg / 100);
-        }
-
-        public override bool IsCompleted() => false;
-        public override bool CanBeCompleted => true;
+        public AIObjectiveRepairItems(Character character) : base(character, "") { }
 
         // TODO: This can allow two active repair items objectives, if RequireAdequateSkills is not at the same value. We don't want that.
         public override bool IsDuplicate(AIObjective otherObjective) => otherObjective is AIObjectiveRepairItems repairItems && repairItems.RequireAdequateSkills == RequireAdequateSkills;
 
-        protected override void Act(float deltaTime) { }
-
-        private void GetBrokenItems()
+        protected override void FindTargets()
         {
+            targets.Clear();
             foreach (Item item in Item.ItemList)
             {
-                if (item.IsFullCondition || ignoreList.Contains(item)) { continue; }
+                if (ignoreList.Contains(item)) { continue; }
+                if (item.IsFullCondition) { continue; }
                 foreach (Submarine sub in Submarine.Loaded)
                 {
                     if (sub.TeamID != character.TeamID) { continue; }
                     // If the character is inside, only take connected hulls into account.
                     if (character.Submarine != null && !character.Submarine.IsEntityFoundOnThisSub(item, true)) { continue; }
-                    foreach (Repairable repairable in item.Repairables)
+                    if (!targets.Contains(item))
                     {
-                        if (item.Condition > repairable.ShowRepairUIThreshold) { continue; }
-                        if (RequireAdequateSkills && !repairable.HasRequiredSkills(character)) { continue; }
-                        if (!repairObjectives.TryGetValue(item, out AIObjectiveRepairItem objective))
-                        {
-                            objective = new AIObjectiveRepairItem(character, item);
-                            repairObjectives.Add(item, objective);
-                            AddSubObjective(objective);
-                        }
-                        break;
+                        targets.Add(item);
                     }
                 }
             }
+            // TODO: the order dosen't matter, because the objectives are sorted by the priority?
+            targets.Sort((x, y) => x.ConditionPercentage.CompareTo(y.ConditionPercentage));
         }
+
+        protected override void CreateObjectives()
+        {
+            foreach (var item in targets)
+            {
+                foreach (Repairable repairable in item.Repairables)
+                {
+                    if (item.Condition > repairable.ShowRepairUIThreshold) { continue; }
+                    if (RequireAdequateSkills && !repairable.HasRequiredSkills(character)) { continue; }
+                    if (!objectives.TryGetValue(item, out AIObjective objective))
+                    {
+                        objective = new AIObjectiveRepairItem(character, item);
+                        objectives.Add(item, objective);
+                        AddSubObjective(objective);
+                    }
+                    break;
+                }
+            }
+        }
+
+        protected override float AverageFunction(Item item) => 100 - item.ConditionPercentage;
     }
 }
