367c4f5b3784c65c3c26807dd6c10e837960c6bb
itchyOwl <lauri.harkanen@gmail.com>
2019-03-06 18:52:15 +0200

    Add console commands for changing the gender and race of the character.

--- Barotrauma/BarotraumaShared/Source/Characters/Character.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/Character.cs
@@ -793,15 +793,12 @@ namespace Barotrauma
         }
         partial void InitProjSpecific(XDocument doc);
 
-        public void ReloadHead(int? headId = null, int? hairIndex = null, int? beardIndex = null, int? moustacheIndex = null, int? faceAttachmentIndex = null)
+        public void ReloadHead(int? headId = null, int hairIndex = -1, int beardIndex = -1, int moustacheIndex = -1, int faceAttachmentIndex = -1)
         {
             if (Info == null) { return; }
             var head = AnimController.GetLimb(LimbType.Head);
             if (head == null) { return; }
-            if (headId.HasValue)
-            {
-                Info.RecreateHead(headId.Value, Info.Race, Info.Gender, hairIndex ?? -1, beardIndex ?? -1, moustacheIndex ?? -1, faceAttachmentIndex ?? -1);
-            }
+            Info.RecreateHead(headId ?? Info.HeadSpriteId, Info.Race, Info.Gender, hairIndex, beardIndex, moustacheIndex, faceAttachmentIndex);
 #if CLIENT
             head.RecreateSprite();
 #endif
