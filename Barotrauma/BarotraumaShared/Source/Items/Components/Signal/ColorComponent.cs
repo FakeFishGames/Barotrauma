94597091872e86bb8fa02dc23e90a8ed4d53dc7d
Joonas Rikkonen <poe.regalis@gmail.com>
2019-03-11 14:41:21 +0200

    Use invariant culture when parsing floats or converting them to strings in signal components

--- Barotrauma/BarotraumaShared/Source/Items/Components/Signal/ColorComponent.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Signal/ColorComponent.cs
@@ -1,53 +1,33 @@
 ﻿using System;
+using System.Globalization;
 using System.Xml.Linq;
 
 namespace Barotrauma.Items.Components
 {
     class ColorComponent : ItemComponent
     {
-        //an array to keep track of how long ago a signal was received on both inputs
-        protected float[] timeSinceReceived;
-
         protected float[] receivedSignal;
 
-        //the output is sent if both inputs have received a signal within the timeframe
-        protected float timeFrame;
-
-        [InGameEditable, Serialize(0.0f, true)]
-        public float TimeFrame
-        {
-            get { return timeFrame; }
-            set
-            {
-                timeFrame = Math.Max(0.0f, value);
-            }
-        }
+        private string output = "0,0,0,0";
 
         public ColorComponent(Item item, XElement element)
             : base(item, element)
         {
-            timeSinceReceived = new float[] { Math.Max(timeFrame * 2.0f, 0.1f), Math.Max(timeFrame * 2.0f, 0.1f), Math.Max(timeFrame * 2.0f, 0.1f), Math.Max(timeFrame * 2.0f, 0.1f) };
             receivedSignal = new float[4];
             IsActive = true;
         }
 
         public override void Update(float deltaTime, Camera cam)
         {
-            bool sendOutput = true;
-            for (int i = 0; i < timeSinceReceived.Length; i++)
-            {
-                if (timeSinceReceived[i] > timeFrame) sendOutput = false;
-                timeSinceReceived[i] += deltaTime;
-            }
-            if (sendOutput)
-            {
-                string output = receivedSignal[0].ToString();
-                output += "," + receivedSignal[1].ToString();
-                output += "," + receivedSignal[2].ToString();
-                output += "," + receivedSignal[3].ToString();
+            item.SendSignal(0, output, "signal_out", null);
+        }
 
-                item.SendSignal(0, output, "signal_out", null);
-            }
+        private void UpdateOutput()
+        {
+            output = receivedSignal[0].ToString("G", CultureInfo.InvariantCulture);
+            output += "," + receivedSignal[1].ToString("G", CultureInfo.InvariantCulture);
+            output += "," + receivedSignal[2].ToString("G", CultureInfo.InvariantCulture);
+            output += "," + receivedSignal[3].ToString("G", CultureInfo.InvariantCulture);
         }
 
         public override void ReceiveSignal(int stepsTaken, string signal, Connection connection, Item source, Character sender, float power = 0.0f, float signalStrength = 1.0f)
@@ -55,20 +35,20 @@ namespace Barotrauma.Items.Components
             switch (connection.Name)
             {
                 case "signal_r":
-                    float.TryParse(signal, out receivedSignal[0]);
-                    timeSinceReceived[0] = 0.0f;
+                    float.TryParse(signal, NumberStyles.Float, CultureInfo.InvariantCulture, out receivedSignal[0]);
+                    UpdateOutput();
                     break;
                 case "signal_g":
-                    float.TryParse(signal, out receivedSignal[1]);
-                    timeSinceReceived[1] = 0.0f;
+                    float.TryParse(signal, NumberStyles.Float, CultureInfo.InvariantCulture, out receivedSignal[1]);
+                    UpdateOutput();
                     break;
                 case "signal_b":
-                    float.TryParse(signal, out receivedSignal[2]);
-                    timeSinceReceived[2] = 0.0f;
+                    float.TryParse(signal, NumberStyles.Float, CultureInfo.InvariantCulture, out receivedSignal[2]);
+                    UpdateOutput();
                     break;
                 case "signal_a":
-                    float.TryParse(signal, out receivedSignal[3]);
-                    timeSinceReceived[3] = 0.0f;
+                    float.TryParse(signal, NumberStyles.Float, CultureInfo.InvariantCulture, out receivedSignal[3]);
+                    UpdateOutput();
                     break;
             }
         }
