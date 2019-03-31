FakeFishGames/Barotrauma

commit 866621cd8da9ee1b2ee85bb7a0be626f4a87d2d8

Display a warning on the status monitor when docked to an outpost ("Docked to X, undock before attempting to maneuver the submarine").

Barotrauma/BarotraumaShared/Source/Map/Levels/Level.cs
@@ -1519,6 +1519,14 @@ namespace Barotrauma
                 string outpostFile = outpostFiles.GetRandom(Rand.RandSync.Server);
                 var outpost = new Submarine(outpostFile, tryLoad: false);
                 outpost.Load(unloadPrevious: false);
+                if (i == 0)
+                {
+                    if (GameMain.GameSession?.StartLocation != null) { outpost.Name = GameMain.GameSession.StartLocation.Name; }
+                }
+                else
+                {
+                    if (GameMain.GameSession?.EndLocation != null) { outpost.Name = GameMain.GameSession.EndLocation.Name; }
+                }
                 outpost.MakeOutpost();
 
                 Point? minSize = null;

Barotrauma/BarotraumaClient/Source/Items/Components/Machines/MiniMap.cs
@@ -25,6 +25,12 @@ namespace Barotrauma.Items.Components
                 DrawHUDBack, null);
             submarineContainer = new GUIFrame(new RectTransform(new Vector2(0.9f, 0.85f), GuiFrame.RectTransform, Anchor.Center), style: null);
 
+            new GUICustomComponent(new RectTransform(new Vector2(0.9f, 0.85f), GuiFrame.RectTransform, Anchor.Center),
+                DrawHUDFront, null)
+            {
+                CanBeFocused = false
+            };
+
             hullInfoFrame = new GUIFrame(new RectTransform(new Vector2(0.13f, 0.13f), GUI.Canvas, minSize: new Point(250, 150)),
                 style: "InnerFrame")
             {
@@ -47,7 +53,7 @@ namespace Barotrauma.Items.Components
         public override void AddToGUIUpdateList()
         {
             base.AddToGUIUpdateList();
-            if (hasPower) hullInfoFrame.AddToGUIUpdateList();
+            hullInfoFrame.AddToGUIUpdateList();
         }
 
         public override void OnMapLoaded()
@@ -94,6 +100,27 @@ namespace Barotrauma.Items.Components
                     hullData.DistortionTimer = Rand.Range(1.0f, 10.0f);
                 }
             }
+        }
+
+        private void DrawHUDFront(SpriteBatch spriteBatch, GUICustomComponent container)
+        {
+            foreach (GUIComponent child in submarineContainer.Children.First().Children)
+            {
+                if (child.UserData is Hull hull)
+                {
+                    if (hull.Submarine == null || !hull.Submarine.IsOutpost) { continue; }
+                    string text = TextManager.Get("MiniMapOutpostDockingInfo").Replace("[outpost]", hull.Submarine.Name);
+                    Vector2 textSize = GUI.Font.MeasureString(text);
+                    Vector2 textPos = child.Center;
+                    if (textPos.X + textSize.X / 2 > submarineContainer.Rect.Right)
+                        textPos.X -= ((textPos.X + textSize.X / 2) - submarineContainer.Rect.Right) + 10;
+                    if (textPos.X - textSize.X / 2 < submarineContainer.Rect.X)
+                        textPos.X += (submarineContainer.Rect.X - (textPos.X - textSize.X / 2)) + 10;
+                    GUI.DrawString(spriteBatch, textPos - textSize / 2, text,
+                       Color.Orange * (float)Math.Abs(Math.Sin(Timing.TotalTime)), Color.Black * 0.8f);
+                    break;
+                }
+            }            
         }
 
         private void DrawHUDBack(SpriteBatch spriteBatch, GUICustomComponent container)
@@ -221,7 +248,7 @@ namespace Barotrauma.Items.Components
             foreach (Submarine sub in subs)
             {
                 if (sub.HullVertices == null) { continue; }
-
+                
                 Rectangle worldBorders = sub.GetDockedBorders();
                 worldBorders.Location += sub.WorldPosition.ToPoint();
                 
@@ -242,6 +269,8 @@ namespace Barotrauma.Items.Components
                     GUI.DrawLine(spriteBatch, center + start, center + end, Color.DarkCyan * Rand.Range(0.3f, 0.35f), width: 10);
                 }
             }
+
+
         }
 
         private void GetLinkedHulls(Hull hull, List<Hull> linkedHulls)

Barotrauma/BarotraumaShared/Content/Texts/EnglishVanilla.xml
@@ -2751,4 +2751,6 @@
   <EntityDescription.thoriumfuelrod>A thorium bar, for use within a reactor. Generates slightly more heat and lasts longer than uranium fuel rods.</EntityDescription.thoriumfuelrod>
 
   <HuskDormant>You feel feverish</HuskDormant>
+
+  <MiniMapOutpostDockingInfo>Docked to [outpost].\nUndock before attempting to maneuver to submarine.</MiniMapOutpostDockingInfo>
 </infotexts>

commit b8f75ee96e6eea8263ac4bcf7f814319b0fbfeec

Fixed level inequality errors caused by 866621c. Accessing the level start/end location properties during level generation caused the seed of RandSync.Server to be changed. Now generating a new random location uses a new MTRandom instance and doesn't touch RandSync.Server.

Barotrauma/BarotraumaClient/Source/Screens/NetLobbyScreen.cs
@@ -233,7 +233,8 @@ namespace Barotrauma
 
                 levelSeed = value;
 
-                backgroundSprite = LocationType.Random(levelSeed)?.GetPortrait(ToolBox.StringToInt(levelSeed));
+                int intSeed = ToolBox.StringToInt(levelSeed);
+                backgroundSprite = LocationType.Random(new MTRandom(intSeed))?.GetPortrait(intSeed);
                 seedBox.Text = levelSeed;
 
                 //lastUpdateID++;

Barotrauma/BarotraumaServer/Source/Screens/NetLobbyScreen.cs
@@ -148,7 +148,7 @@ namespace Barotrauma
 
                 lastUpdateID++;
                 levelSeed = value;
-                LocationType.Random(levelSeed); //call to sync up with clients
+                LocationType.Random(new MTRandom(ToolBox.StringToInt(levelSeed))); //call to sync up with clients
             }
         }
         

Barotrauma/BarotraumaShared/Source/GameSession/GameSession.cs
@@ -139,7 +139,7 @@ namespace Barotrauma
             MTRandom rand = new MTRandom(ToolBox.StringToInt(seed));
             for (int i = 0; i < 2; i++)
             {
-                dummyLocations[i] = Location.CreateRandom(new Vector2((float)rand.NextDouble() * 10000.0f, (float)rand.NextDouble() * 10000.0f), null);
+                dummyLocations[i] = Location.CreateRandom(new Vector2((float)rand.NextDouble() * 10000.0f, (float)rand.NextDouble() * 10000.0f), null, rand);
             }
         }
 

Barotrauma/BarotraumaShared/Source/Map/Levels/Level.cs
@@ -1519,14 +1519,6 @@ namespace Barotrauma
                 string outpostFile = outpostFiles.GetRandom(Rand.RandSync.Server);
                 var outpost = new Submarine(outpostFile, tryLoad: false);
                 outpost.Load(unloadPrevious: false);
-                if (i == 0)
-                {
-                    if (GameMain.GameSession?.StartLocation != null) { outpost.Name = GameMain.GameSession.StartLocation.Name; }
-                }
-                else
-                {
-                    if (GameMain.GameSession?.EndLocation != null) { outpost.Name = GameMain.GameSession.EndLocation.Name; }
-                }
                 outpost.MakeOutpost();
 
                 Point? minSize = null;
@@ -1567,10 +1559,12 @@ namespace Barotrauma
                 if ((i == 0) == !Mirrored)
                 {
                     StartOutpost = outpost;
+                    if (GameMain.GameSession?.StartLocation != null) { outpost.Name = GameMain.GameSession.StartLocation.Name; }
                 }
                 else
                 {
                     EndOutpost = outpost;
+                    if (GameMain.GameSession?.EndLocation != null) { outpost.Name = GameMain.GameSession.EndLocation.Name; }
                 }
             }            
         }

Barotrauma/BarotraumaShared/Source/Map/Map/Location.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -76,10 +77,10 @@ namespace Barotrauma
             }
         }
 
-        public Location(Vector2 mapPosition, int? zone)
+        public Location(Vector2 mapPosition, int? zone, Random rand)
         {
-            this.Type = LocationType.Random("", zone);
-            this.Name = RandomName(Type);
+            this.Type = LocationType.Random(rand, zone);
+            this.Name = RandomName(Type, rand);
             this.MapPosition = mapPosition;
 
             PortraitId = ToolBox.StringToInt(Name);
@@ -87,9 +88,9 @@ namespace Barotrauma
             Connections = new List<LocationConnection>();
         }
 
-        public static Location CreateRandom(Vector2 position, int? zone)
+        public static Location CreateRandom(Vector2 position, int? zone , Random rand)
         {
-            return new Location(position, zone);        
+            return new Location(position, zone, rand);        
         }
 
         public IEnumerable<Mission> GetMissionsInConnection(LocationConnection connection)
@@ -129,10 +130,10 @@ namespace Barotrauma
             availableMissions.RemoveAll(m => m.Completed);
         }
 
-        private string RandomName(LocationType type)
+        private string RandomName(LocationType type, Random rand)
         {
-            baseName = type.GetRandomName();
-            nameFormatIndex = Rand.Int(type.NameFormats.Count, Rand.RandSync.Server);
+            baseName = type.GetRandomName(rand);
+            nameFormatIndex = rand.Next() % type.NameFormats.Count;
             return type.NameFormats[nameFormatIndex].Replace("[name]", baseName);
         }
 

Barotrauma/BarotraumaShared/Source/Map/Map/LocationType.cs
@@ -155,19 +155,14 @@ namespace Barotrauma
             return portraits[Math.Abs(portraitId) % portraits.Count];
         }
 
-        public string GetRandomName()
+        public string GetRandomName(Random rand)
         {
-            return names[Rand.Int(names.Count, Rand.RandSync.Server)];
+            return names[rand.Next() % names.Count];
         }
 
-        public static LocationType Random(string seed = "", int? zone = null)
+        public static LocationType Random(Random rand, int? zone = null)
         {
             Debug.Assert(List.Count > 0, "LocationType.list.Count == 0, you probably need to initialize LocationTypes");
-
-            if (!string.IsNullOrWhiteSpace(seed))
-            {
-                Rand.SetSyncedSeed(ToolBox.StringToInt(seed));
-            }
 
             List<LocationType> allowedLocationTypes = zone.HasValue ? List.FindAll(lt => lt.CommonnessPerZone.ContainsKey(zone.Value)) : List;
 
@@ -180,12 +175,12 @@ namespace Barotrauma
             {
                 return ToolBox.SelectWeightedRandom(
                     allowedLocationTypes, 
-                    allowedLocationTypes.Select(a => a.CommonnessPerZone[zone.Value]).ToList(), 
-                    Rand.RandSync.Server);
+                    allowedLocationTypes.Select(a => a.CommonnessPerZone[zone.Value]).ToList(),
+                    rand);
             }
             else
             {
-                return allowedLocationTypes[Rand.Int(allowedLocationTypes.Count, Rand.RandSync.Server)];
+                return allowedLocationTypes[rand.Next() % allowedLocationTypes.Count];
             }
         }
 

Barotrauma/BarotraumaShared/Source/Map/Map/Map.cs
@@ -196,7 +196,7 @@ namespace Barotrauma
                     Vector2 position = points[positionIndex];
                     if (newLocations[1 - i] != null && newLocations[1 - i].MapPosition == position) position = points[1 - positionIndex];
                     int zone = MathHelper.Clamp(generationParams.DifficultyZones - (int)Math.Floor(Vector2.Distance(position, mapCenter) / zoneRadius), 1, generationParams.DifficultyZones);
-                    newLocations[i] = Location.CreateRandom(position, zone);
+                    newLocations[i] = Location.CreateRandom(position, zone, Rand.GetRNG(Rand.RandSync.Server));
                     Locations.Add(newLocations[i]);
                 }
 