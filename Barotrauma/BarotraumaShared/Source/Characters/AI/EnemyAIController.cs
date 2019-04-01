9fffb056257b4261b2d6ce95f7181e4fbfe17724
Joonas Rikkonen <poe.regalis@gmail.com>
2019-04-01 22:49:28 +0300

    (9db186429) Double the wall priorioties for crawlers and mudraptors, so that they remain effectively the same after the code changes.

--- Barotrauma/BarotraumaShared/Source/Characters/AI/EnemyAIController.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/EnemyAIController.cs
@@ -1089,50 +1089,7 @@ namespace Barotrauma
                 string targetingTag = null;
                 if (targetCharacter != null)
                 {
-                    if (targetCharacter.Submarine != null && Character.Submarine == null)
-                    {
-                        targetingTag = "dead";
-                        if (targetCharacter.Submarine != Character.Submarine)
-                        {
-                            // In a different sub or the target is outside when we are inside or vice versa -> Ignore the target
-                            continue;
-                        }
-                        else if (targetCharacter.CurrentHull != Character.CurrentHull)
-                        {
-                            // In the same sub, halve the priority, if not in the same hull.
-                            valueModifier = 0.5f;
-                        }
-                    }
-                    else if (targetCharacter.AIController is EnemyAIController enemy)
-                    {
-                        if (enemy.combatStrength > combatStrength)
-                        {
-                            targetingTag = "stronger";
-                        }
-                        else if (enemy.combatStrength < combatStrength)
-                        {
-                            targetingTag = "weaker";
-                        }
-                        if (State == AIState.Escape && targetingTag == "stronger")
-                        {
-                            // Frightened
-                            valueModifier = 2;
-                        }
-                        else
-                        {
-                            if (targetCharacter.Submarine != Character.Submarine)
-                            {
-                                // In a different sub or the target is outside when we are inside or vice versa -> Ignore the target
-                                continue;
-                            }
-                            else if (targetCharacter.CurrentHull != Character.CurrentHull)
-                            {
-                                // In the same sub, halve the priority, if not in the same hull.
-                                valueModifier = 0.5f;
-                            }
-                        }
-                    }
-                    else if (targetCharacter.Submarine != null && Character.Submarine == null)
+                    if (targetCharacter.IsDead)
                     {
                         targetingTag = "dead";
                         if (targetCharacter.Submarine != Character.Submarine)
@@ -1276,10 +1233,48 @@ namespace Barotrauma
                                 valueModifier = isOutdoor ? 1 : 0;
                                 valueModifier *= isOpen ? 5 : 1;
                             }
-                            for (int i = 0; i < s.Sections.Length; i++)
+                        }
+                    }
+                    else if (targetCharacter.Submarine != null && Character.Submarine == null)
+                    {
+                        targetingTag = "dead";
+                        if (targetCharacter.Submarine != Character.Submarine)
+                        {
+                            // In a different sub or the target is outside when we are inside or vice versa -> Ignore the target
+                            continue;
+                        }
+                        else if (targetCharacter.CurrentHull != Character.CurrentHull)
+                        {
+                            // In the same sub, halve the priority, if not in the same hull.
+                            valueModifier = 0.5f;
+                        }
+                    }
+                    else if (targetCharacter.AIController is EnemyAIController enemy)
+                    {
+                        if (enemy.combatStrength > combatStrength)
+                        {
+                            targetingTag = "stronger";
+                        }
+                        else if (enemy.combatStrength < combatStrength)
+                        {
+                            targetingTag = "weaker";
+                        }
+                        if (State == AIState.Escape && targetingTag == "stronger")
+                        {
+                            // Frightened
+                            valueModifier = 2;
+                        }
+                        else
+                        {
+                            if (targetCharacter.Submarine != Character.Submarine)
                             {
                                 valueModifier = isOutdoor ? 0 : 1;
                                 valueModifier *= isOpen ? 0 : 1;
+                            }
+                            else if (targetCharacter.CurrentHull != Character.CurrentHull)
+                            {
+                                // In the same sub, halve the priority, if not in the same hull.
+                                valueModifier = 0.5f;
                             }
                         }
                         else if (isOpen) //ignore broken and open doors
@@ -1292,6 +1287,11 @@ namespace Barotrauma
                          continue;
                     }
                 }
+
+                if (targetingTag == null) continue;
+                if (!targetingPriorities.ContainsKey(targetingTag)) continue;
+
+                valueModifier *= targetingPriorities[targetingTag].Priority;
 
                 if (targetingTag == null) continue;
                 if (!targetingPriorities.ContainsKey(targetingTag)) continue;
