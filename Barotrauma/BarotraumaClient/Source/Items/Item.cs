caa04a08a761b669882113228a6eb12cab5a56a3
Joonas Rikkonen <poe.regalis@gmail.com>
2019-04-03 16:17:45 +0300

    Added a search bar to fabricators, made the interface larger and item previews smaller to fit more items in the view, take chatbox into account when preventing item interface overlap. Closes #1305

--- Barotrauma/BarotraumaClient/Source/Items/Item.cs
+++ Barotrauma/BarotraumaClient/Source/Items/Item.cs
@@ -607,6 +607,9 @@ namespace Barotrauma
             if (GameMain.GameSession?.CrewManager != null)
             {
                 disallowedAreas.Add(GameMain.GameSession.CrewManager.GetCharacterListArea());
+                disallowedAreas.Add(new Rectangle(
+                    HUDLayoutSettings.ChatBoxArea.X - 150, HUDLayoutSettings.ChatBoxArea.Y, 
+                    HUDLayoutSettings.ChatBoxArea.Width + 150, HUDLayoutSettings.ChatBoxArea.Height));                
             }
 
             GUI.PreventElementOverlap(elementsToMove, disallowedAreas,
