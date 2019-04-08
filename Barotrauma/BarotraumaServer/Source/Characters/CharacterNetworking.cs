b651e55f44a4e818bb8333545aee098ed70d4e1e
Joonas Rikkonen <poe.regalis@gmail.com>
2019-04-08 11:40:37 +0300

    Don't quantize physicsbody velocities server-side. Caused subs to occasionally not move horizontally because the velocities got rounded down to zero if the sub wasn't accelerating fast enough.

--- Barotrauma/BarotraumaServer/Source/Characters/CharacterNetworking.cs
+++ Barotrauma/BarotraumaServer/Source/Characters/CharacterNetworking.cs
@@ -383,7 +383,9 @@ namespace Barotrauma
                 tempBuffer.Write(SimPosition.X);
                 tempBuffer.Write(SimPosition.Y);
                 float MaxVel = NetConfig.MaxPhysicsBodyVelocity;
-                AnimController.Collider.LinearVelocity = NetConfig.Quantize(AnimController.Collider.LinearVelocity, -MaxVel, MaxVel, 12);
+                AnimController.Collider.LinearVelocity = new Vector2(
+                    MathHelper.Clamp(AnimController.Collider.LinearVelocity.X, -MaxVel, MaxVel),
+                    MathHelper.Clamp(AnimController.Collider.LinearVelocity.Y, -MaxVel, MaxVel));
                 tempBuffer.WriteRangedSingle(AnimController.Collider.LinearVelocity.X, -MaxVel, MaxVel, 12);
                 tempBuffer.WriteRangedSingle(AnimController.Collider.LinearVelocity.Y, -MaxVel, MaxVel, 12);
 
--- Barotrauma/BarotraumaServer/Source/Physics/PhysicsBody.cs
+++ Barotrauma/BarotraumaServer/Source/Physics/PhysicsBody.cs
@@ -33,13 +33,15 @@ namespace Barotrauma
             if (FarseerBody.Awake)
             {
                 body.Enabled = true;
-                body.LinearVelocity = NetConfig.Quantize(body.LinearVelocity, -MaxVel, MaxVel, 12);
+                body.LinearVelocity = new Vector2(
+                    MathHelper.Clamp(body.LinearVelocity.X, -MaxVel, MaxVel),
+                    MathHelper.Clamp(body.LinearVelocity.Y, -MaxVel, MaxVel));
                 msg.WriteRangedSingle(body.LinearVelocity.X, -MaxVel, MaxVel, 12);
                 msg.WriteRangedSingle(body.LinearVelocity.Y, -MaxVel, MaxVel, 12);
                 if (!FarseerBody.FixedRotation)
                 {
-                    body.AngularVelocity = NetConfig.Quantize(body.AngularVelocity, -MaxAngularVel, MaxAngularVel, 8);
-                    msg.WriteRangedSingle(MathHelper.Clamp(body.AngularVelocity, -MaxAngularVel, MaxAngularVel), -MaxAngularVel, MaxAngularVel, 8);
+                    body.AngularVelocity = MathHelper.Clamp(body.AngularVelocity, -MaxAngularVel, MaxAngularVel);
+                    msg.WriteRangedSingle(body.AngularVelocity, -MaxAngularVel, MaxAngularVel, 8);
                 }
             }
 
