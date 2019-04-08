fb3b4d3610aac6a6d5184538d8effeabed9b534f
EdusFF <pitkanen.eetu@gmail.com>
2019-04-08 13:41:08 +0300

    Added: First iteration of the mechanic tutorial, needs outpost updates to work

--- Barotrauma/BarotraumaClient/Source/GameSession/GameModes/Tutorials/MechanicTutorial.cs
+++ Barotrauma/BarotraumaClient/Source/GameSession/GameModes/Tutorials/MechanicTutorial.cs
@@ -8,23 +8,42 @@ namespace Barotrauma.Tutorials
     class MechanicTutorial : ScenarioTutorial
     {
         // Room 1
-        private Item firstButton;
+        private Item mechanic_firstButton;
         
         // Room 2
-        private MotionSensor equipmentVideoSensor;
-        private Item doorButtonBeforeEquipmentOn;
+        private MotionSensor mechanic_equipmentObjectiveSensor;
+        private Item mechanic_doorButtonBeforeEquipmentOn;
 
         // Room 3
-        private MotionSensor weldingVideoSensor;
-        private Item doorButtonBeforeWaterDrained;
-        private Structure mechanic_brokenwall_1;
+        private MotionSensor mechanic_weldingObjectiveSensor;
+        private Item mechanic_doorButtonBeforeWaterDrained;
+        private Structure mechanic_brokenWall_1;
         private Hull mechanic_brokenhull_1;
-        private Structure mechanic_brokenwall_2;
-        private Hull mechanic_brokenhull_2;
 
         // Room 4
-        private MotionSensor deconstructorVideoSensor;
-        private MotionSensor fabricatorVideoSensor;
+        private Deconstructor mechanic_deconstructor;
+        private Fabricator mechanic_fabricator;
+        private Item mechanic_doorButtonBeforeFire;
+
+        private bool fabricatorInteractedWith = false;
+        private bool deconstructorInteractedWith = false;
+
+        // Room 5
+        private Door mechanic_doorToFire;
+        private DummyFireSource mechanic_fire;
+        private MotionSensor mechanic_divingSuitObjectiveSensor;
+        private Item mechanic_doorButtonBeforePressure;
+
+        // Room 6
+        private Pump mechanic_brokenPump;
+        private Structure mechanic_brokenWall_2;
+        private Hull mechanic_brokenhull_2;
+
+        // Submarine
+        private MotionSensor mechanic_enteredSubmarineSensor;
+        private Engine mechanic_submarineEngine;
+        private Pump mechanic_ballastPump_1;
+        private Pump mechanic_ballastPump_2;
 
         // Colors
         private Color inaccessibleButtonColor = Color.Red;
@@ -39,32 +58,46 @@ namespace Barotrauma.Tutorials
         {
             base.Start();
 
-            firstButton.SpriteColor = accessibleButtonColor;
+            // Room 1
+            mechanic_firstButton = Item.ItemList.Find(i => i.HasTag("mechanic_firstButton"));
+            mechanic_firstButton.SpriteColor = accessibleButtonColor;
 
-            equipmentVideoSensor = Item.ItemList.Find(i => i.HasTag("mechanic_equipment_video"))?.GetComponent<MotionSensor>();
-            doorButtonBeforeEquipmentOn = Item.ItemList.Find(i => i.HasTag("mechanic_equipment_button"));
-            doorButtonBeforeEquipmentOn.SpriteColor = inaccessibleButtonColor;
-            doorButtonBeforeWaterDrained.SpriteColor = inaccessibleButtonColor;
+            // Room 2
+            mechanic_equipmentObjectiveSensor = Item.ItemList.Find(i => i.HasTag("mechanic_equipmentObjectiveSensor"))?.GetComponent<MotionSensor>();
+            mechanic_doorButtonBeforeEquipmentOn = Item.ItemList.Find(i => i.HasTag("mechanic_doorButtonBeforeEquipmentOn"));
+            mechanic_doorButtonBeforeEquipmentOn.SpriteColor = inaccessibleButtonColor;
 
-            mechanic_brokenwall_1 = Structure.WallList.Find(i => i.SpecialTag == "mechanic_brokenwall_1");
-            mechanic_brokenwall_2 = Structure.WallList.Find(i => i.SpecialTag == "mechanic_brokenwall_2");
-            //mechanic_fire = new DummyFireSource(new Vector2(20f, 10f), Item.ItemList.Find(i => i.HasTag("mechanic_fire")).WorldPosition);
+            // Room 3
+            mechanic_brokenWall_1 = Structure.WallList.Find(i => i.SpecialTag == "mechanic_brokenWall_1");
+            mechanic_brokenWall_2 = Structure.WallList.Find(i => i.SpecialTag == "mechanic_brokenWall_2");
+            mechanic_doorButtonBeforeWaterDrained.SpriteColor = inaccessibleButtonColor;
 
-            mechanic_brokenwall_1.Indestructible = false;
-            for (int i = 0; i < mechanic_brokenwall_1.SectionCount; i++)
+            mechanic_brokenWall_1.Indestructible = false;
+            for (int i = 0; i < mechanic_brokenWall_1.SectionCount; i++)
             {
-                mechanic_brokenwall_1.AddDamage(i, 100);
+                mechanic_brokenWall_1.AddDamage(i, 100);
             }
 
-            mechanic_brokenhull_1 = mechanic_brokenwall_1.Sections[0].gap.FlowTargetHull;
+            mechanic_brokenhull_1 = mechanic_brokenWall_1.Sections[0].gap.FlowTargetHull;
 
-            mechanic_brokenwall_2.Indestructible = false;
-            for (int i = 0; i < mechanic_brokenwall_2.SectionCount; i++)
+            // Room 4
+            mechanic_deconstructor = Item.ItemList.Find(i => i.HasTag("deconstructor"))?.GetComponent<Deconstructor>();
+            mechanic_fabricator = Item.ItemList.Find(i => i.HasTag("fabricator"))?.GetComponent<Fabricator>();
+            mechanic_doorButtonBeforeFire.SpriteColor = inaccessibleButtonColor;
+
+            // Room 5
+            mechanic_fire = new DummyFireSource(new Vector2(10f, 5f), Item.ItemList.Find(i => i.HasTag("mechanic_fire")).WorldPosition);
+            mechanic_doorButtonBeforePressure.SpriteColor = inaccessibleButtonColor;
+
+            // Room 6
+            mechanic_brokenWall_2.Indestructible = false;
+            for (int i = 0; i < mechanic_brokenWall_2.SectionCount; i++)
             {
-                mechanic_brokenwall_2.AddDamage(i, 100);
+                mechanic_brokenWall_2.AddDamage(i, 100);
             }
 
-            mechanic_brokenhull_2 = mechanic_brokenwall_2.Sections[0].gap.FlowTargetHull;
+            mechanic_brokenhull_2 = mechanic_brokenWall_2.Sections[0].gap.FlowTargetHull;
+            mechanic_brokenPump = Item.ItemList.Find(i => i.HasTag("mechanic_brokenPump"))?.GetComponent<Pump>();
         }
 
         public override IEnumerable<object> UpdateState()
@@ -72,31 +105,94 @@ namespace Barotrauma.Tutorials
             // Room 1
             // Wake up, shake
             // RADIO: Concerned crewmember telling player to get moving and get to the sub, a Moloch 
+            // Open door objective
             // Remove first button highlight after interaction
 
             // Room 2
-            while (!equipmentVideoSensor.MotionDetected) yield return null;
-            TriggerTutorialSegment(1); // Equipment & inventory video
+            while (!mechanic_equipmentObjectiveSensor.MotionDetected) yield return null;
+            TriggerTutorialSegment(1); // Equipment & inventory objective
             // Highlight cabinet
             // Remove cabinet highlight after interact
             while (!Character.Controlled.HasEquippedItem("divingmask") || !Character.Controlled.HasEquippedItem("weldingtool")) yield return null; // Wait until equipped
             // RADIO: crewmember explaining that the player needs to repair the walls along the way
-            doorButtonBeforeEquipmentOn.SpriteColor = accessibleButtonColor; // Unlock door
+            mechanic_doorButtonBeforeEquipmentOn.SpriteColor = accessibleButtonColor; // Unlock door
             // Remove button highlight after interact
 
             // Room 3
-            while (!weldingVideoSensor.MotionDetected) yield return null;
-            TriggerTutorialSegment(2); // Welding video
+            while (!mechanic_weldingObjectiveSensor.MotionDetected) yield return null;
+            TriggerTutorialSegment(2); // Welding objective
             // Highlight damaged wall
-            while (WallHasDamagedSections(mechanic_brokenwall_1)) yield return null;
+            while (WallHasDamagedSections(mechanic_brokenWall_1)) yield return null;
             // Remove damaged wall highlight
-            TriggerTutorialSegment(3); // Pump video
+            TriggerTutorialSegment(3); // Pump objective
             // Highlight pump until draining
             while (mechanic_brokenhull_1.WaterPercentage > 0) yield return null;
-            doorButtonBeforeWaterDrained.SpriteColor = accessibleButtonColor;
+            mechanic_doorButtonBeforeWaterDrained.SpriteColor = accessibleButtonColor;
 
             // Room 4
 
+            while (!deconstructorInteractedWith && !fabricatorInteractedWith) // Wait until interacted with both
+            {
+                if (!deconstructorInteractedWith)
+                {
+                    if (IsSelectedItem(mechanic_deconstructor.Item))
+                    {
+                        deconstructorInteractedWith = true;
+                        TriggerTutorialSegment(4); // Deconstructor objective
+                    }
+                }
+
+                if (!fabricatorInteractedWith)
+                {
+                    if (IsSelectedItem(mechanic_fabricator.Item))
+                    {
+                        fabricatorInteractedWith = true;
+                        TriggerTutorialSegment(5); // Fabricator objective
+                    }
+                }
+
+                yield return null;
+            }
+
+            // RADIO: crewmember explains you must fabricate a fire extinguisher using sodium and aluminum from a deconstructed oxygen tank
+            while (!Character.Controlled.HasEquippedItem("extinguisher")) yield return null; // Wait until equipped
+            mechanic_doorButtonBeforeFire.SpriteColor = accessibleButtonColor;
+
+            // Room 5
+            while (!mechanic_doorToFire.IsOpen) yield return null;
+            TriggerTutorialSegment(6); // Using the extinguisher
+            while (mechanic_fire != null) yield return null; // Wait until extinguished
+            // RADIO: crewmember warns of high pressure in next room
+            while (!mechanic_divingSuitObjectiveSensor.MotionDetected) yield return null;
+            TriggerTutorialSegment(7); // Danges of pressure, equip diving suit objective
+            while (!Character.Controlled.HasEquippedItem("divingsuit")) yield return null;
+            mechanic_doorButtonBeforePressure.SpriteColor = accessibleButtonColor;
+
+            // Room 6
+            // Higlight damaged hull
+            while (WallHasDamagedSections(mechanic_brokenWall_2)) yield return null;
+            TriggerTutorialSegment(8); // Repairing machinery (pump)
+            // Highlight pump
+            while (!mechanic_brokenPump.Item.IsFullCondition && !mechanic_brokenPump.IsActive) yield return null;
+            // Disable highlight
+            while (mechanic_brokenhull_2.WaterPercentage > 0) yield return null;
+            // Open hatch
+
+            // Submarine
+            while (!mechanic_enteredSubmarineSensor.MotionDetected) yield return null;
+            // RADIO: crewmember prompts player to repair ballast pumps and engine
+            while (!mechanic_ballastPump_1.Item.IsFullCondition || !mechanic_ballastPump_2.Item.IsFullCondition || !mechanic_submarineEngine.Item.IsFullCondition)
+            {
+                // Remove highlights when each individual machine is repaired
+                yield return null;
+            }            
+
+            // END TUTORIAL
+        }
+
+        private bool IsSelectedItem(Item item)
+        {
+            return Character.Controlled?.SelectedConstruction == item;
         }
 
         private bool WallHasDamagedSections(Structure wall)
