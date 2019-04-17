3e9eb9ea75619cc62fd63e815e863d54c9e22db4
EdusFF <pitkanen.eetu@gmail.com>
2019-04-17 16:24:25 +0300

    Modified: Damage values inflicted to walls at the start of the mechanic tutorial

--- Barotrauma/BarotraumaClient/Source/GameSession/GameModes/Tutorials/MechanicTutorial.cs
+++ Barotrauma/BarotraumaClient/Source/GameSession/GameModes/Tutorials/MechanicTutorial.cs
@@ -138,7 +138,7 @@ namespace Barotrauma.Tutorials
             mechanic_brokenWall_1.SpriteColor = Color.White;
             for (int i = 0; i < mechanic_brokenWall_1.SectionCount; i++)
             {
-                mechanic_brokenWall_1.AddDamage(i, 100);
+                mechanic_brokenWall_1.AddDamage(i, 165);
             }
             mechanic_brokenhull_1 = mechanic_brokenWall_1.Sections[0].gap.FlowTargetHull;
 
@@ -194,7 +194,7 @@ namespace Barotrauma.Tutorials
             mechanic_brokenWall_2.SpriteColor = Color.White;
             for (int i = 0; i < mechanic_brokenWall_2.SectionCount; i++)
             {
-                mechanic_brokenWall_2.AddDamage(i, 100);
+                mechanic_brokenWall_2.AddDamage(i, 165);
             }
             mechanic_brokenhull_2 = mechanic_brokenWall_2.Sections[0].gap.FlowTargetHull;
             SetDoorAccess(tutorial_submarineDoor, tutorial_submarineDoorLight, false);
