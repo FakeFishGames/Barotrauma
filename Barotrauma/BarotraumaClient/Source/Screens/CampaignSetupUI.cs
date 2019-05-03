bd659383b4f52d211aa35bb2db61a8a67aa8a0a5
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-03 13:48:48 +0300

    (1aa050263) Cherry pick from human-ai: Fix bots not knowing how to use the integrated controllers, effectively failing to use the doors with integrated buttons.

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -72,6 +72,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
