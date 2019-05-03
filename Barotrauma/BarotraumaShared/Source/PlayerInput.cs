8c678e39771016e95d27f7274e364ae329f4ae1e
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-03 13:50:49 +0300

    (9f9b86b9b) Fixed tutorial level generation params being used in normal levels.

--- Barotrauma/BarotraumaShared/Source/PlayerInput.cs
+++ Barotrauma/BarotraumaShared/Source/PlayerInput.cs
@@ -208,6 +208,21 @@ namespace Barotrauma
         }
 #endif
 
+        public KeyOrMouse State
+        {
+            get { return binding; }
+        }
+
+        public void SetState()
+        {
+            hit = binding.IsHit();
+            if (hit) hitQueue = true;
+
+            held = binding.IsDown();
+            if (held) heldQueue = true;
+        }
+#endif
+
         public bool Hit
         {
             get
