0a931f31e64f2889b4a428e0a86ee20f128a1ec1
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-03 13:52:15 +0300

    (e9cc325b5) Cherry pick from human-ai: Fix not being able to open partially broken doors with integrated buttons.

--- Barotrauma/BarotraumaShared/Source/Items/Components/Door.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Door.cs
@@ -219,8 +219,6 @@ namespace Barotrauma.Items.Components
         private bool hasValidIdCard;
         public override bool HasRequiredItems(Character character, bool addMessage, string msg = null)
         {
-            if (item.Condition <= RepairThreshold) return true; //For repairing
-
             var idCard = character.Inventory.FindItemByIdentifier("idcard");
             hasValidIdCard = requiredItems.Any(ri => ri.Value.Any(r => r.MatchesItem(idCard)));
             Msg = requiredItems.None() || hasValidIdCard ? "ItemMsgOpen" : "ItemMsgForceOpenCrowbar";
@@ -229,7 +227,7 @@ namespace Barotrauma.Items.Components
             {
                 msg = msg ?? (HasIntegratedButtons ? accessDeniedTxt : cannotOpenText);
             }
-
+            if (item.Condition <= RepairThreshold) { return true; }
             //this is a bit pointless atm because if canBePicked is false it won't allow you to do Pick() anyway, however it's still good for future-proofing.
             return requiredItems.Any() ? base.HasRequiredItems(character, addMessage, msg) : canBePicked;
         }
@@ -244,7 +242,7 @@ namespace Barotrauma.Items.Components
 
         public override bool OnPicked(Character picker)
         {
-            if (item.Condition <= RepairThreshold) return true; //repairs
+            if (item.Condition <= RepairThreshold) { return true; }
             if (requiredItems.Any() && !hasValidIdCard)
             {
                 ForceOpen(ActionType.OnPicked);
@@ -262,23 +260,24 @@ namespace Barotrauma.Items.Components
 
         public override bool Select(Character character)
         {
-            //can only be selected if the item is broken
-            if (item.Condition <= RepairThreshold) return true; //repairs
-            bool hasRequiredItems = HasRequiredItems(character, false);
-            if (requiredItems.None() || hasRequiredItems && hasValidIdCard)
+            if (!isBroken)
             {
-                float originalPickingTime = PickingTime;
-                PickingTime = 0;
-                ForceOpen(ActionType.OnUse);
-                PickingTime = originalPickingTime;
-            }
-            else if (hasRequiredItems)
-            {
+                bool hasRequiredItems = HasRequiredItems(character, false);
+                if (requiredItems.None() || hasRequiredItems && hasValidIdCard)
+                {
+                    float originalPickingTime = PickingTime;
+                    PickingTime = 0;
+                    ForceOpen(ActionType.OnUse);
+                    PickingTime = originalPickingTime;
+                }
+                else if (hasRequiredItems)
+                {
 #if CLIENT
-                GUI.AddMessage(accessDeniedTxt, Color.Red);
+                    GUI.AddMessage(accessDeniedTxt, Color.Red);
 #endif
+                }
             }
-            return false;
+            return item.Condition <= RepairThreshold;
         }
 
         public override void Update(float deltaTime, Camera cam)
