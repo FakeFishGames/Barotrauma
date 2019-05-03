036e3423e68a2f18dae27d3fb0884a24bfe18902
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-03 13:55:40 +0300

    (289fc0ad0) Fixed some hull, door and waypoint issues in Dugong

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -120,6 +120,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
