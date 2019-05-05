c3083f3e03af1fe3fc84638e8edec84a1302478e
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-05 19:03:30 +0300

    Added google translated Russian & Chinese text files for testing

--- Barotrauma/BarotraumaClient/Source/GameSettings.cs
+++ Barotrauma/BarotraumaClient/Source/GameSettings.cs
@@ -132,7 +132,7 @@ namespace Barotrauma
             var languageDD = new GUIDropDown(new RectTransform(new Vector2(1.0f, 0.045f), generalLayoutGroup.RectTransform));
             foreach (string language in TextManager.AvailableLanguages)
             {
-                languageDD.AddItem(TextManager.Get("Language." + language), language);
+                languageDD.AddItem(TextManager.Get("Language." + language, returnNull: true) ?? Language, language);
             }
             languageDD.SelectItem(TextManager.Language);
             languageDD.OnSelected = (guiComponent, obj) =>
