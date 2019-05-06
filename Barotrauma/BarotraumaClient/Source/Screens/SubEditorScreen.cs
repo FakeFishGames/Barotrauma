e4adc47dadc8581379ea109eb70ab8570a21b176
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-06 11:22:02 +0300

    (1036993d3) Adjusted motion detector in autodoors

--- Barotrauma/BarotraumaClient/Source/Screens/SubEditorScreen.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/SubEditorScreen.cs
@@ -815,7 +815,7 @@ namespace Barotrauma
         {
             if (dummyCharacter != null) RemoveDummyCharacter();
 
-            dummyCharacter = Character.Create(Character.HumanConfigFile, Vector2.Zero, "", hasAi: false);
+            dummyCharacter = Character.Create(Character.HumanConfigFile, Vector2.Zero, "");
 
             //make space for the entity menu
             for (int i = 0; i < dummyCharacter.Inventory.SlotPositions.Length; i++)
