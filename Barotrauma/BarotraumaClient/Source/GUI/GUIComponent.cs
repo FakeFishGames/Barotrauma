917534b2b96c5f191c2401479f08e5d94c241c3b
Eetu Pitkänen <pitkanen.eetu@gmail.com>
2019-05-11 16:06:21 +0300

    Fixed: tooltip overflowing from container on 4k

--- Barotrauma/BarotraumaClient/Source/GUI/GUIComponent.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUIComponent.cs
@@ -471,13 +471,16 @@ namespace Barotrauma
 
         public static void DrawToolTip(SpriteBatch spriteBatch, string toolTip, Rectangle targetElement)
         {
-            int width = 400;
+            int width = (int)(400 * GUI.Scale);
+            int height = (int)(18 * GUI.Scale);
+            Point padding = new Point((int)(20 * GUI.Scale), (int)(7 * GUI.Scale));
+
             if (toolTipBlock == null || (string)toolTipBlock.userData != toolTip)
             {
-                toolTipBlock = new GUITextBlock(new RectTransform(new Point(width, 18), null), toolTip, font: GUI.SmallFont, wrap: true, style: "GUIToolTip");
+                toolTipBlock = new GUITextBlock(new RectTransform(new Point(width, height), null), toolTip, font: GUI.SmallFont, wrap: true, style: "GUIToolTip");
                 toolTipBlock.RectTransform.NonScaledSize = new Point(
-                    (int)(GUI.SmallFont.MeasureString(toolTipBlock.WrappedText).X + 20),
-                    toolTipBlock.WrappedText.Split('\n').Length * 18 + 7);
+                    (int)(GUI.SmallFont.MeasureString(toolTipBlock.WrappedText).X + padding.X),
+                    (int)(GUI.SmallFont.MeasureString(toolTipBlock.WrappedText).Y + padding.Y));
                 toolTipBlock.userData = toolTip;
             }
 
