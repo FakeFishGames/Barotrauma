e858408c26bfa5876071da13e6666dedab00696b
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-15 21:23:07 +0300

    Use localizable "signal_out_x" text in CustomInterface elements with an empty label (TODO: disable empty elements)

--- Barotrauma/BarotraumaClient/Source/Items/Components/Signal/CustomInterface.cs
+++ Barotrauma/BarotraumaClient/Source/Items/Components/Signal/CustomInterface.cs
@@ -119,11 +119,15 @@ namespace Barotrauma.Items.Components
             {
                 if (uiElements[i] is GUIButton button)
                 {
-                    button.Text = customInterfaceElementList[i].Label;
+                    button.Text = string.IsNullOrWhiteSpace(customInterfaceElementList[i].Label) ?
+                        TextManager.Get("connection.signaloutx").Replace("[num]", (i + 1).ToString()) :
+                        customInterfaceElementList[i].Label;
                 }
                 else if (uiElements[i] is GUITickBox tickBox)
                 {
-                    tickBox.Text = customInterfaceElementList[i].Label;
+                    tickBox.Text = string.IsNullOrWhiteSpace(customInterfaceElementList[i].Label) ?
+                        TextManager.Get("connection.signaloutx").Replace("[num]", (i + 1).ToString()) :
+                        customInterfaceElementList[i].Label;
                 }
             }
         }
