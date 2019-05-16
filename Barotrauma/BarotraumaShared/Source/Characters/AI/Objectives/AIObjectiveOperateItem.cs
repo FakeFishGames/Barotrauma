606e1f656048d3d748deebf1901c1d929518b91d
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:06:07 +0300

    (e761ae6fd) Implemented new diving suit locker

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -174,6 +174,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
