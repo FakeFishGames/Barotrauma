4f825ce309a65b3d3151bc66a9b50fc44c6f43e7
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:31:31 +0300

    (dabdac1a8) Fix bots waiting where they should not wait.

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveIdle.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveIdle.cs
@@ -73,21 +73,6 @@ namespace Barotrauma
             }
         }
 
-        public override void Update(float deltaTime)
-        {
-            if (objectiveManager.CurrentObjective == this)
-            {
-                if (randomTimer > 0)
-                {
-                    randomTimer -= deltaTime;
-                }
-                else
-                {
-                    SetRandom();
-                }
-            }
-        }
-
         public override bool IsCompleted() => false;
         public override bool CanBeCompleted => true;
 
@@ -109,13 +94,6 @@ namespace Barotrauma
             {
                 character.SelectedConstruction = null;
             }
-            if (!character.IsClimbing)
-            {
-                character.SelectedConstruction = null;
-            }
-
-            bool currentTargetIsInvalid = currentTarget == null || IsForbidden(currentTarget) || 
-                (PathSteering.CurrentPath != null && PathSteering.CurrentPath.Nodes.Any(n => HumanAIController.UnsafeHulls.Contains(n.CurrentHull)));
 
             bool currentTargetIsInvalid = currentTarget == null || IsForbidden(currentTarget) || 
                 (PathSteering.CurrentPath != null && PathSteering.CurrentPath.Nodes.Any(n => HumanAIController.UnsafeHulls.Contains(n.CurrentHull)));
