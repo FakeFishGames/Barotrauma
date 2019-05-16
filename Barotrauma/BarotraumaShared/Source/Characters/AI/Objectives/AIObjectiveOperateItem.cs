fda2d468779a4cf8954d97a3210c24071a8d593a
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:02:59 +0300

    (d243af062) hull sprite fix, part XXIV

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -130,6 +130,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
