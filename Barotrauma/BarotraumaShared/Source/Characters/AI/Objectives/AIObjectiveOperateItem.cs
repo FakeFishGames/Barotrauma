ff83e3fe386a0bba51ecd81961331f3c9b6c34c0
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:44:27 +0300

    (4ceb463a2) Fixed medic tutorial crashing on languages other than English due to RoomNames being used to find specific hulls (now uses cargo spawnpoints)

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -709,6 +709,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
