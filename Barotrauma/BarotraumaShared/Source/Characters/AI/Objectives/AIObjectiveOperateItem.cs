fbe0832b1e8679d94e0d3eb8c40bdd62fbc6a635
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:08:58 +0300

    (58e405f96) Berilia pass: reduced device power requirements, wired some unwired periscopes

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -214,6 +214,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
