08ade4a262472bee7a3b28bf45b09bb8acaab56e
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:45:02 +0300

    (12cfc8a35) Fixed doctor tutorial attempting to find the Doctor job based on the name instead of identifier

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -658,6 +658,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
