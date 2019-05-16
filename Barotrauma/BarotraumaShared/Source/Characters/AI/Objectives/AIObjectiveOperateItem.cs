3d354e32b2d8976ee32ffff891458cfcfa887d24
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:33:46 +0300

    (b3c12523f) Added a higher-res preview image to Orca

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -561,6 +561,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
