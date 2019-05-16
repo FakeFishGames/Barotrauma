1face40d928e5412575cee3bcfd76f5d5a757e0d
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:53:10 +0300

    (45ce701fa) Fixed accidental white borders on new environment assets

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -294,6 +294,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
