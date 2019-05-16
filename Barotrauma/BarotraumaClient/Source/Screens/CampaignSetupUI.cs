cbabefc2d06863f4ca8d5263826e60100639286c
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:38:35 +0300

    (f66183740) Add texts for rescue objective.

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -190,6 +190,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
