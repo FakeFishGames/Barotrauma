9ba7dee1d7ebfa15f55f4c70557cb95c2f920753
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:59:56 +0300

    (e858408c2) Use localizable "signal_out_x" text in CustomInterface elements with an empty label (TODO: disable empty elements)

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -917,6 +917,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
