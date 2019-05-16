d9ab57e2aee5dff284b065ed9c6a443a72ef1fbd
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:28:39 +0300

    (8a177f8bd) Fixed remora airlocks and refreshed drone. Added higher res preview pics to remora, typhon and bunyip

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -544,6 +544,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
