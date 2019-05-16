2215c2fba076443beb7727675c4e97c29147c12a
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:32:22 +0300

    (b3637fc6c) Merge branch 'dev' of https://github.com/Regalis11/Barotrauma-development into dev

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -541,6 +541,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
