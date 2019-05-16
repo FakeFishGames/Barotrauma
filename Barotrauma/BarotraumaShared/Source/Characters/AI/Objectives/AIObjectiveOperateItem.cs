5ff09db449faf333501e4ebce27d89a2a9e9b373
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 07:03:32 +0300

    (eff616485) Don't show mission info in the round summary if there's no localized "mission failed" text available and there's nothing else to show. TODO: add the text to localization so this isn't needed

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -965,6 +965,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
