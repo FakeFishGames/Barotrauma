36898958b228a1529cfda500b4e6221dd8daaddf
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:26:06 +0300

    (0a8612032) Graphics add, mostly structure stuff

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -453,6 +453,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
