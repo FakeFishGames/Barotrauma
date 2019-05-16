b105ef71eb72893a4215706aacf317f7d3e9b4a8
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:49:32 +0300

    (b4d4f6802) Re-enable initiative.

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveIdle.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveIdle.cs
@@ -52,11 +52,25 @@ namespace Barotrauma
 
         public override float GetPriority()
         {
-            return 1;
             float max = Math.Min(Math.Min(AIObjectiveManager.RunPriority, AIObjectiveManager.OrderPriority) - 1, 100);
             float initiative = character.GetSkillLevel("initiative");
             Priority = MathHelper.Lerp(1, max, MathUtils.InverseLerp(100, 0, initiative * Random));
             return Priority;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            if (objectiveManager.CurrentObjective == this)
+            {
+                if (randomTimer > 0)
+                {
+                    randomTimer -= deltaTime;
+                }
+                else
+                {
+                    SetRandom();
+                }
+            }
         }
 
         public override void Update(float deltaTime)
