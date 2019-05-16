f8a0d54449afe4968cffbdc5a573acb580572806
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:52:02 +0300

    (d54773ab0) Fix the project references.

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -286,6 +286,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
