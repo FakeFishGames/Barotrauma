bf359f58e9afbee3a3ef3bc422d11cf233df2190
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:11:15 +0300

    (099a1ac6b) Updated Berilia docking button signals

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -246,6 +246,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
