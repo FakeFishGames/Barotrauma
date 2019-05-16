f23e4c67db4fd65c4339d504a7c1a47d777d8b87
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:26:22 +0300

    (534c88165) Remora, remora drone & Typhon updated

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -457,6 +457,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
