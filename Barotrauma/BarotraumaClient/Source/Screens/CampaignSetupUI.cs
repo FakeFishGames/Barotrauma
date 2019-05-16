9f8723972410970bfe96a684da004d126a8f2012
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:32:38 +0300

    (f3d83a912) Disabled: Separate mac path to test if editing the actual application folder when using steam is OK

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -572,6 +572,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
