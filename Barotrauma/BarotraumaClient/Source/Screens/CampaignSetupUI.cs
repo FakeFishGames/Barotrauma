5e8800c1e17963e4b392cdbbf400308db598c4f1
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:37:45 +0300

    (54efef38f) Merge branch 'dev' of https://github.com/Regalis11/Barotrauma-development into dev

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -608,6 +608,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
