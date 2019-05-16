b0a8f0e5e137acb9c85056cb6f36b1f705e87d4a
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:20:37 +0300

    (c41c72d1b) new arrow decals

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -378,6 +378,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
