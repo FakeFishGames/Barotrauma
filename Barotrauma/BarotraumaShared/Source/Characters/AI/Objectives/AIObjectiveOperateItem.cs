1b4957d2e6c5e823416b626f1d517da4a5a538f1
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:41:13 +0300

    (15f884096) Remora HQ preview pic

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -665,6 +665,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
