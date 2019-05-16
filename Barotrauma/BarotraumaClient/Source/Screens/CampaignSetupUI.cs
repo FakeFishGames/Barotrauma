83cb848ecda0c88fbc46c414548bced4935959e9
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:38:01 +0300

    (2095f9490) Changed remora drone door button and component placement

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -610,6 +610,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
