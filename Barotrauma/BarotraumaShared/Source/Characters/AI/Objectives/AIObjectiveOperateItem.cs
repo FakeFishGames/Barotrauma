02c5a35331fbcf07fe9d7dbca625c581e2015440
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:16:06 +0300

    (fc70e8aa6) updated searchlight

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -314,6 +314,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
