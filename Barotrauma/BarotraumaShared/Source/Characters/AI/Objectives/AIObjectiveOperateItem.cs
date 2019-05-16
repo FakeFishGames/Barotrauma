a2f97bf303a429d3633bb5ca098b1a3b15238526
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:51:18 +0300

    (9cb944db7) Increase the priority modifier of extinguish fires for captain

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -801,6 +801,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
