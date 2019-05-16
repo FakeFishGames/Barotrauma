0a4a2aa35e344f3810918f64a430b17e4984758f
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:22:53 +0300

    (cdc3301a5) Fixed: Tutorial checkmark position

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -410,6 +410,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
