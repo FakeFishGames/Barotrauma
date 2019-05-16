a70f4f315e5a24a3eb1536ef79d7c39492db7dcb
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:01:19 +0300

    (6aad031a6) Implemented new plant LevelObjects

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -106,6 +106,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
