71cde8862fe117958fbcbdb1b3d89809ac2ac384
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:08:22 +0300

    (f35c3b912) new outpost

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -206,6 +206,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
