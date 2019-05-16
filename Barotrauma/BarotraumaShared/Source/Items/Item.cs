71685ef28cc7c174da3804a10aa63e6f3d340afd
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:52:21 +0300

    (3ebdb2066) Merge branch 'dev' of https://github.com/Regalis11/Barotrauma-development into dev

--- Barotrauma/BarotraumaShared/Source/Items/Item.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Item.cs
@@ -1736,6 +1736,10 @@ namespace Barotrauma
             {
                 ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
             }
+            if (!broken)
+            {
+                ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
+            }
             ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
 
             if (body == null || !body.Enabled || !inWater || ParentInventory != null || Removed) { return; }
