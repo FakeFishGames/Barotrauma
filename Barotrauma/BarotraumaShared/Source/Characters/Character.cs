3b5ae37a1a2c330eab18d837e879c1b1a10fe6de
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:44:44 +0300

    (a275d7aba) Use NotoSans instead of NotoSansCJK as an override for BebasNeue in Russian (NotoSansCJK is a chinese/japanese/korean font that has a ridiculous number of symbols)

--- Barotrauma/BarotraumaShared/Source/Characters/Character.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/Character.cs
@@ -1147,10 +1147,6 @@ namespace Barotrauma
             
             if (!(this is AICharacter) || Controlled == this || IsRemotePlayer)
             {
-                SmoothedCursorPosition = cursorPosition;
-            }
-            else
-            {
                 //apply some smoothing to the cursor positions of remote players when playing as a client
                 //to make aiming look a little less choppy
                 Vector2 smoothedCursorDiff = cursorPosition - SmoothedCursorPosition;
