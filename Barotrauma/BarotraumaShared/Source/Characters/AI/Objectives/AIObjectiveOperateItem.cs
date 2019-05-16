35cea7ae256a42230d04091a4b64afd851cc30a8
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:10:42 +0300

    (e25000a52) Added new bow and rear to be used with Typhon

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -238,6 +238,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
