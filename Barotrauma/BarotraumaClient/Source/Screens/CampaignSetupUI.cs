b7392b362e6897138411cf25c0c9206d472e098c
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:45:53 +0300

    (3c3673317) Tune the job priority modifiers.

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -242,6 +242,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
