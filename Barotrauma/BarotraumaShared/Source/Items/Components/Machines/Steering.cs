77f1d0ea8edda81bdd1e2c0e2f04f7df87440961
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:23:55 +0300

    (a843b19d4) Optimize distance checks.

--- Barotrauma/BarotraumaShared/Source/Items/Components/Machines/Steering.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Machines/Steering.cs
@@ -179,38 +179,24 @@ namespace Barotrauma.Items.Components
             }
         }
 
-        public Vector2? PosToMaintain
-        {
-            get { return posToMaintain; }
-            set { posToMaintain = value; }
-        }
-
-        struct ObstacleDebugInfo
-        {
-            public Vector2 Point1;
-            public Vector2 Point2;
-
-            public Vector2? Intersection;
-
-            public float Dot;
-
-            public Vector2 AvoidStrength;
-
-            public ObstacleDebugInfo(GraphEdge edge, Vector2? intersection, float dot, Vector2 avoidStrength)
-            {
-                Point1 = edge.Point1;
-                Point2 = edge.Point2;
-                Intersection = intersection;
-                Dot = dot;
-                AvoidStrength = avoidStrength;
-            }
-        }
-
         //edge point 1, edge point 2, avoid strength
         private List<ObstacleDebugInfo> debugDrawObstacles = new List<ObstacleDebugInfo>();
 
         public Steering(Item item, XElement element)
             : base(item, element)
+        {
+            sonar = item.GetComponent<Sonar>();
+        }
+
+        public override bool Select(Character character)
+        {
+            if (!CanBeSelected) return false;
+
+            user = character;
+            return true;
+        }
+
+        public override void OnItemLoaded()
         {
             sonar = item.GetComponent<Sonar>();
         }
