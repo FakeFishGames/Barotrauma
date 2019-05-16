a6b592a625713a6dd7c5ee4b27bc9362526c01b8
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:26:57 +0300

    (93733c170) Fix a null reference exception.

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveIdle.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveIdle.cs
@@ -73,6 +73,21 @@ namespace Barotrauma
             }
         }
 
+        public override void Update(float deltaTime)
+        {
+            if (objectiveManager.CurrentObjective == this)
+            {
+                if (randomTimer > 0)
+                {
+                    randomTimer -= deltaTime;
+                }
+                else
+                {
+                    SetRandom();
+                }
+            }
+        }
+
         public override bool IsCompleted() => false;
         public override bool CanBeCompleted => true;
 
@@ -301,8 +316,8 @@ namespace Barotrauma
         {
             if (hull == null) { return true; }
             string hullName = hull.RoomName?.ToLowerInvariant();
-            bool isForbidden = hullName.Contains("ballast") || hullName.Contains("airlock");
-            return isForbidden;
+            if (hullName == null) { return false; }
+            return hullName.Contains("ballast") || hullName.Contains("airlock");
         }
 
         public override bool IsDuplicate(AIObjective otherObjective)
