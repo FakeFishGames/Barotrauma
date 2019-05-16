14e894cbc85302ad08250f7f2a60130f2858e0fe
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:35:30 +0300

    (82dc10a74) Some margin.

--- Barotrauma/BarotraumaShared/Source/Items/Item.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Item.cs
@@ -1496,6 +1496,10 @@ namespace Barotrauma
             {
                 ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
             }
+            if (!broken)
+            {
+                ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
+            }
             ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
 
             if (body == null || !body.Enabled || !inWater || ParentInventory != null || Removed) { return; }
