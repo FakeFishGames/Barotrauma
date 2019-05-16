f02c7cdda9ef55cf85571c86e458530205dbdfdc
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:51:45 +0300

    (ee538829d) Added docking tutorial video, removed highlight from old docking button.

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -284,6 +284,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
--- Barotrauma/BarotraumaClient/Source/Screens/MainMenuScreen.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/MainMenuScreen.cs
@@ -789,6 +789,10 @@ namespace Barotrauma
 
             GUI.Draw(Cam, spriteBatch);
 
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, GameMain.ScissorTestEnable);
+
+            GUI.Draw(Cam, spriteBatch);
+
             GUI.Draw(Cam, spriteBatch);
             
 #if DEBUG
