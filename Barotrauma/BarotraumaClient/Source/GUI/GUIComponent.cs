37555ed1a4ea2cfc332dfedf291187f9cb8a2f5b
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:07:07 +0300

    (9fc7c8e98) Only reset when the objective is an order.

--- Barotrauma/BarotraumaClient/Source/GUI/GUIComponent.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUIComponent.cs
@@ -146,6 +146,8 @@ namespace Barotrauma
 
         public bool IgnoreLayoutGroups;
 
+        public bool IgnoreLayoutGroups;
+
         public virtual ScalableFont Font
         {
             get;
--- Barotrauma/BarotraumaClient/Source/GUI/GUIStyle.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUIStyle.cs
@@ -135,6 +135,12 @@ namespace Barotrauma
             return style;
         }
 
+        public GUIComponentStyle GetComponentStyle(string name)
+        {
+            componentStyles.TryGetValue(name.ToLowerInvariant(), out GUIComponentStyle style);
+            return style;
+        }
+
         public void Apply(GUIComponent targetComponent, string styleName = "", GUIComponent parent = null)
         {
             GUIComponentStyle componentStyle = null;  
