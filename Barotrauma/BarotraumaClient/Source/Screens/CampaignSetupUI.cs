5b3696f7e4e48bb93fdf63bbc7daec30f5642cc5
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:16:13 +0300

    (d16942d30) Simplify.

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -34,6 +34,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
