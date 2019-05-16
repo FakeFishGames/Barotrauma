113e17a4095e226ab71bc46a85c41c716070c7f2
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:40:21 +0300

    (4cbb13747) Humpback: added waypoints to bilge manually

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -653,6 +653,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
