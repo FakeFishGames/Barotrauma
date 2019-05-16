9dc33984a947750c856a33a615781944b70314bb
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:55:40 +0300

    (e0b208744) Removed wall edge textures from the code project and added them to the content project

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -138,6 +138,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
