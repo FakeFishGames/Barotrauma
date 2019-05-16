aad4b64014858315a877a2c4fb5bec85b95984e7
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:00:29 +0300

    (98bbe45fe) Added connection names to englishvanilla

--- Barotrauma/BarotraumaShared/Source/Items/Item.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Item.cs
@@ -1852,6 +1852,10 @@ namespace Barotrauma
             {
                 ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
             }
+            if (!broken)
+            {
+                ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
+            }
             ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
 
             if (body == null || !body.Enabled || !inWater || ParentInventory != null || Removed) { return; }
