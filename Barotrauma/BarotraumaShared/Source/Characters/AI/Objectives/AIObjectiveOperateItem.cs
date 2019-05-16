e50baabea08e9680ed9bd3cf15c3daf6c65a647b
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:30:22 +0300

    (36cf727ca) fixes and sub folder cleanup

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -513,6 +513,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
