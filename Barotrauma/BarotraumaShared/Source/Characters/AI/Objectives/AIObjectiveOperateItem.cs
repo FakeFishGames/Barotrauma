5e61e476c8a12e76f7c5669336584024ea89f2f6
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:40:04 +0300

    (4ff7c12d2) removed colliders from tail fins

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -649,6 +649,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
