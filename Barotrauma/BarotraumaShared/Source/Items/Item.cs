38b7d326d627eef6d39d7a9fb90f4ba8c475964e
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:44:10 +0300

    (774a92f5e) Merge branch 'dev' of https://github.com/Regalis11/Barotrauma-development into dev

--- Barotrauma/BarotraumaShared/Source/Items/Item.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Item.cs
@@ -1131,6 +1131,10 @@ namespace Barotrauma
             {
                 ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
             }
+            if (!broken)
+            {
+                ApplyStatusEffects(!waterProof && inWater ? ActionType.InWater : ActionType.NotInWater, deltaTime);
+            }
 
             if (body == null || !body.Enabled || !inWater || ParentInventory != null || Removed) { return; }
 
@@ -1199,6 +1203,27 @@ namespace Barotrauma
         public void UpdateTransform()
         {
             Submarine prevSub = Submarine;
+
+            FindHull();
+
+            if (Submarine == null && prevSub != null)
+            {
+                body.SetTransform(body.SimPosition + prevSub.SimPosition, body.Rotation);
+            }
+            else if (Submarine != null && prevSub == null)
+            {
+                body.SetTransform(body.SimPosition - Submarine.SimPosition, body.Rotation);
+            }
+            else if (Submarine != null && prevSub != null && Submarine != prevSub)
+            {
+                body.SetTransform(body.SimPosition + prevSub.SimPosition - Submarine.SimPosition, body.Rotation);
+            }
+
+            Vector2 displayPos = ConvertUnits.ToDisplayUnits(body.SimPosition);
+            rect.X = (int)(displayPos.X - rect.Width / 2.0f);
+            rect.Y = (int)(displayPos.Y + rect.Height / 2.0f);
+
+            if (body == null || !body.Enabled || !inWater || ParentInventory != null || Removed) { return; }
 
             FindHull();
 
