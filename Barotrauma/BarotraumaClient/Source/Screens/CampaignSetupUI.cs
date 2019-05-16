a09baa34849abb02b3059fb3c086f2da9c713d5a
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:24:04 +0300

    (e1f526341) Fixed turrets not rotating to the correct direction client-side when a bot is operating them

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -512,6 +512,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
