3318fd8e14b6b1c3fd331ae244d9443f19005a7d
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:45:18 +0300

    (fea9b6c97) Updated: Localization

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -721,6 +721,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
