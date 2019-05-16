71582afbbc26bea0a85059c9a8b648eac5ede9f5
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:35:12 +0300

    (66c7e4e6a) Reupdated orca preview image with correct BG

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -581,6 +581,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
