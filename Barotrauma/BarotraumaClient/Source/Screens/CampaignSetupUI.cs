31bb766d48adef551c812ba6145b35d923fcd94e
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:16:22 +0300

    (398a85bdb) Change the draw order of debug lines so that the indicator to the current way point is rendered on top of the other lines.

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -458,6 +458,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
