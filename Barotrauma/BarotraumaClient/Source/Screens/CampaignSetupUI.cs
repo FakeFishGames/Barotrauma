abd19f614f9f37befc199b3342af8436a55e8853
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:23:03 +0300

    (913d93c32) Setup new skills: initiative and courage. Not yet implemented.

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -80,6 +80,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
