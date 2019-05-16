f667ed8e1207547c8c9f3a98a9ddd6380fd04a37
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:34:03 +0300

    (3f4e69c0d) Only unequip items when idling. Fixes bots not being able to repair, because the tools were unequipped (if they were weapons).

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -96,6 +96,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
