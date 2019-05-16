b7e8407df5a4f6517989ffbc3aabe72e504d8d7c
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:42:59 +0300

    (b130881e0) Added: Overriding of the BebasNeue-Regular.otf in Russian due to missing characters

--- Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/CampaignSetupUI.cs
@@ -644,6 +644,8 @@ namespace Barotrauma
 
         private GUILayoutGroup subPreviewContainer;
 
+        private GUILayoutGroup subPreviewContainer;
+
         private GUIButton loadGameButton;
         
         public Action<Submarine, string, string> StartNewGame;
