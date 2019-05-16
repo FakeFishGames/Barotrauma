cdeb6d40b31fb22b67027095f3432c756cbb09dc
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 05:10:25 +0300

    (f3bfe1ac4) Fix damage priority calculation.

--- Barotrauma/BarotraumaClient/Source/GUI/GUIComponent.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUIComponent.cs
@@ -168,6 +168,8 @@ namespace Barotrauma
 
         public bool IgnoreLayoutGroups;
 
+        public bool IgnoreLayoutGroups;
+
         public virtual ScalableFont Font
         {
             get;
