8d155d8bdda369b494305a4960564096df6b6e40
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-16 06:36:18 +0300

    (34738e4a9) Waypoint fixes (nodes at the hatches not connecting outside, outside nodes going through exterior wall pieces and turrets). TODO: improve the waypoint generation so this kind of manual fixes aren't needed

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjectiveOperateItem.cs
@@ -597,6 +597,10 @@ namespace Barotrauma
                     {
                         isCompleted = true;
                     }
+                    if (component.AIOperate(deltaTime, character, this))
+                    {
+                        isCompleted = true;
+                    }
                 }
                 else
                 {
