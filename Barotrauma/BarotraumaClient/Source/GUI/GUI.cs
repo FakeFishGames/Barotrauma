a40a6150882277dcef39f92a33ec8517016d7065
EdusFF <pitkanen.eetu@gmail.com>
2019-05-17 16:54:26 +0300

    Added: Not drawing cursor when the window is not active

--- Barotrauma/BarotraumaClient/Source/GUI/GUI.cs
+++ Barotrauma/BarotraumaClient/Source/GUI/GUI.cs
@@ -396,8 +396,11 @@ namespace Barotrauma
             {
                 MouseOn.DrawToolTip(spriteBatch);
             }
-            
-            Cursor.Draw(spriteBatch, PlayerInput.LatestMousePosition, 0, Scale / 2f);            
+
+            if (GameMain.WindowActive)
+            {
+                Cursor.Draw(spriteBatch, PlayerInput.LatestMousePosition, 0, Scale / 2f);
+            }
         }
 
         public static void DrawBackgroundSprite(SpriteBatch spriteBatch, Sprite backgroundSprite, float blurAmount = 1.0f, float aberrationStrength = 1.0f)
