6abe73b88977f983d757c9dd7cea326481be5765
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-18 17:38:16 +0300

    (3f196aed7) Merge branch 'dev' of https://github.com/Regalis11/Barotrauma-development into dev

--- Barotrauma/BarotraumaClient/Source/Screens/ServerListScreen.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/ServerListScreen.cs
@@ -218,22 +218,6 @@ namespace Barotrauma
             return true;
         }
 
-        private bool RefreshJoinButtonState(GUIComponent component, object obj)
-        {
-            if (obj == null || waitingForRefresh) { return false; }
-
-            if (!string.IsNullOrWhiteSpace(clientNameBox.Text) && !string.IsNullOrWhiteSpace(ipBox.Text))
-            {
-                joinButton.Enabled = true;
-            }
-            else
-            {
-                joinButton.Enabled = false;
-            }
-
-            return true;
-        }
-
         private bool SelectServer(GUIComponent component, object obj)
         {
             if (obj == null || waitingForRefresh) { return false; }
