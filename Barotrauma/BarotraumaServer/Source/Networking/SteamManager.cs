85a4d4243a7c9f16fa216d3401504c05932575bb
Joonas Rikkonen <poe.regalis@gmail.com>
2019-05-22 16:14:19 +0300

    Fixed Steam servers never refreshing their info on the master server (game started, settings, etc), report the currently selected sub to Steam. Closes #1489

--- Barotrauma/BarotraumaServer/Source/Networking/SteamManager.cs
+++ Barotrauma/BarotraumaServer/Source/Networking/SteamManager.cs
@@ -48,6 +48,7 @@ namespace Barotrauma.Steam
             instance.server.ServerName = server.Name;
             instance.server.MaxPlayers = server.ServerSettings.MaxPlayers;
             instance.server.Passworded = server.ServerSettings.HasPassword;
+            instance.server.MapName = GameMain.NetLobbyScreen?.SelectedSub?.DisplayName ?? "";
             Instance.server.SetKey("message", GameMain.Server.ServerSettings.ServerMessageText);
             Instance.server.SetKey("version", GameMain.Version.ToString());
             Instance.server.SetKey("contentpackage", string.Join(",", GameMain.Config.SelectedContentPackages.Select(cp => cp.Name)));
