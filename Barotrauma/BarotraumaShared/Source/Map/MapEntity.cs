667564ff66cece0ab0dedc754e34a39093a010da
Joonas Rikkonen <poe.regalis@gmail.com>
2019-06-14 14:42:25 +0300

    Fixed item scale not being saved and item rect not being scaled according to the item's scale. Closes #1547

--- Barotrauma/BarotraumaShared/Source/Map/MapEntity.cs
+++ Barotrauma/BarotraumaShared/Source/Map/MapEntity.cs
@@ -555,7 +555,7 @@ namespace Barotrauma
             }
         }
         
-        [Serialize(1f, false), Editable(0.1f, 10f, DecimalCount = 3, ValueStep = 0.1f)]
+        [Serialize(1f, true), Editable(0.1f, 10f, DecimalCount = 3, ValueStep = 0.1f)]
         public virtual float Scale { get; set; } = 1;
         #endregion
     }
