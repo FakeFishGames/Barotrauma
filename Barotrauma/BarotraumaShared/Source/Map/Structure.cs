26835ee42a370f142ff99fde9ad6f97aafcf963f
Joonas Rikkonen <poe.regalis@gmail.com>
2019-07-27 21:20:21 +0300

    Don't attempt to set section damage during Structure loading if the index is out of bounds. Does not fix the underlying problem of the index somehow being out of bounds (see #1768), but allows the structure to still be loaded, preventing ID mismatch issues in multiplayer.

--- Barotrauma/BarotraumaShared/Source/Map/Structure.cs
+++ Barotrauma/BarotraumaShared/Source/Map/Structure.cs
@@ -1194,7 +1194,15 @@ namespace Barotrauma
                 {
                     case "section":
                         int index = subElement.GetAttributeInt("i", -1);
-                        if (index == -1) continue;
+                        if (index == -1) { continue; }
+
+                        if (index < 0 || index >= s.SectionCount)
+                        {
+                            string errorMsg = $"Error while loading structure \"{ s.Name}\". Section damage index out of bounds. Index: {index}, section count: {s.SectionCount}.";
+                            DebugConsole.ThrowError(errorMsg);
+                            GameAnalyticsManager.AddErrorEventOnce("Structure.Load:SectionIndexOutOfBounds", GameAnalyticsSDK.Net.EGAErrorSeverity.Error, errorMsg);
+                        }
+
                         s.Sections[index].damage = subElement.GetAttributeFloat("damage", 0.0f);
                         break;
                 }
