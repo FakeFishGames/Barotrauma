2dccdf529ca267bd59d5ac2771663a513ccdfa25
Joonas Rikkonen <poe.regalis@gmail.com>
2019-08-15 15:24:31 +0300

    Fixed server logging an "attempted to send a chat message to a null client" error when a client tries to send a PM to a non-existent client

--- Barotrauma/BarotraumaServer/Source/Networking/GameServer.cs
+++ Barotrauma/BarotraumaServer/Source/Networking/GameServer.cs
@@ -2340,7 +2340,7 @@ namespace Barotrauma.Networking
                                         var chatMsg = ChatMessage.Create(
                                             "", $"ServerMessage.PlayerNotFound~[player]={command}",
                                             ChatMessageType.Error, null);
-                                        SendDirectChatMessage(chatMsg, targetClient);
+                                        SendDirectChatMessage(chatMsg, senderClient);
                                     }
                                     else
                                     {
