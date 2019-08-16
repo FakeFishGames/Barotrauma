87c60a7a7df6426cf9ab1d936ac77a1fe59ca36d
itchyOwl <lauri.harkanen@gmail.com>
2019-08-16 14:01:15 +0300

    AI Target sight and sound range changes: - Set ai target sound and right range min values to 0 by default. Fixes some items (like sonar beacon) sending the noise or visuals even when it's powered off. Required a bit of refactoring. - The engine and reactor ranges are not hard coded anymore. Set the values lower than they were - Changed the noise calculation formulae for reactor, engine, and sonar. - Changed the logic how the noise is spread from the engine and the reactor to the hull. - Added the missing ai targets on the reactor, engine and sonar items, and implemented the logic to use them. - Removed the noise from the hull of the sonar, because we can target the sonar item itself.

--- Barotrauma/BarotraumaShared/Source/Items/Components/Machines/Engine.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Machines/Engine.cs
@@ -97,11 +97,17 @@ namespace Barotrauma.Items.Components
 
                 UpdatePropellerDamage(deltaTime);
 
+                if (item.AiTarget != null)
+                {
+                    var aiTarget = item.AiTarget;
+                    aiTarget.SoundRange = MathHelper.Lerp(aiTarget.MinSoundRange, aiTarget.MaxSoundRange, currForce.Length() / maxForce);
+                }
                 if (item.CurrentHull != null)
                 {
-                    item.CurrentHull.AiTarget.SoundRange = Math.Max(currForce.Length(), item.CurrentHull.AiTarget.SoundRange);
+                    var aiTarget = item.CurrentHull.AiTarget;
+                    float noise = MathHelper.Lerp(aiTarget.MinSoundRange, aiTarget.MaxSoundRange, currForce.Length() / maxForce);
+                    aiTarget.SoundRange = Math.Max(noise, aiTarget.SoundRange);
                 }
-
 #if CLIENT
                 for (int i = 0; i < 5; i++)
                 {
