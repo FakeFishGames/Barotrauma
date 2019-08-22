b530f51db7177dc472e9dcbee0fae62697e5f994
itchyOwl <lauri.harkanen@gmail.com>
2019-08-22 13:46:30 +0300

    Redo changes in SteamManager.cs

--- Barotrauma/BarotraumaClient/Source/Networking/SteamManager.cs
+++ Barotrauma/BarotraumaClient/Source/Networking/SteamManager.cs
@@ -923,9 +923,9 @@ namespace Barotrauma.Steam
             if (newPackage.CorePackage)
             {
                 //if enabling a core package, disable all other core packages
-                GameMain.Config.SelectedContentPackages.RemoveWhere(cp => cp.CorePackage);
+                GameMain.Config.SelectedContentPackages.RemoveAll(cp => cp.CorePackage);
             }
-            GameMain.Config.SelectedContentPackages.Add(newPackage);
+            GameMain.Config.SelectContentPackage(newPackage);
             GameMain.Config.SaveNewPlayerConfig();
 
             if (newPackage.Files.Any(f => f.Type == ContentType.Submarine))
@@ -1127,7 +1127,8 @@ namespace Barotrauma.Steam
                 }
 
                 ContentPackage.List.RemoveAll(cp => System.IO.Path.GetFullPath(cp.Path) == System.IO.Path.GetFullPath(installedContentPackagePath));
-                GameMain.Config.SelectedContentPackages.RemoveWhere(cp => !ContentPackage.List.Contains(cp));
+                GameMain.Config.SelectedContentPackages.RemoveAll(cp => !ContentPackage.List.Contains(cp));
+                ContentPackage.SortContentPackages();
                 GameMain.Config.SaveNewPlayerConfig();
             }
             catch (Exception e)
