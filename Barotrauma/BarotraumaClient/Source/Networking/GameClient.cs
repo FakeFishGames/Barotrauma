ff5b0abc4cce9986de6b926af3afd45bdd3e8e58
Joonas Rikkonen <poe.regalis@gmail.com>
2019-08-26 12:35:43 +0300

    Fixed character name box resetting in the server lobby when another client joins or disconnects

--- Barotrauma/BarotraumaClient/Source/Networking/GameClient.cs
+++ Barotrauma/BarotraumaClient/Source/Networking/GameClient.cs
@@ -1248,7 +1248,8 @@ namespace Barotrauma.Networking
                     {
                         existingClient.SetPermissions(permissions, permittedConsoleCommands);
                         name = tc.Name;
-                        if (GameMain.NetLobbyScreen.CharacterNameBox != null)
+                        if (GameMain.NetLobbyScreen.CharacterNameBox != null &&
+                            !GameMain.NetLobbyScreen.CharacterNameBox.Selected)
                         {
                             GameMain.NetLobbyScreen.CharacterNameBox.Text = name;
                         }
