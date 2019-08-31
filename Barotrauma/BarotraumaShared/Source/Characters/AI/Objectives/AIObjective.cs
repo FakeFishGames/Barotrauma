3273a58b4539996feafe2c46f7408790bfa5f57f
itchyOwl <lauri.harkanen@gmail.com>
2019-08-31 03:24:40 +0300

    Fix the initial CanBeCompleted condition.

--- Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjective.cs
+++ Barotrauma/BarotraumaShared/Source/Characters/AI/Objectives/AIObjective.cs
@@ -31,7 +31,7 @@ namespace Barotrauma
         /// <summary>
         /// Can the objective be completed. That is, does the objective have failing subobjectives or other conditions that prevent it from completing.
         /// </summary>
-        public virtual bool CanBeCompleted => !abandon && subObjectives.All(so => so.CanBeCompleted);
+        public virtual bool CanBeCompleted => !abandon && subObjectives.None(so => !so.CanBeCompleted);
 
         /// <summary>
         /// When true, the objective is never completed, unless CanBeCompleted returns false.
