FakeFishGames/Barotrauma

commit a4239a2be46d69cf64c0a139ddeceafba2b0aff4

Load some ItemComponent properties from sub/assembly xml even when overriding (items contained in an ItemContainer, wire nodes, wires connected to a connection panel, the attached-state of an attachable component)

Barotrauma/BarotraumaShared/Source/Items/Item.cs
@@ -2138,19 +2138,16 @@ namespace Barotrauma
 
             //if we're overriding a non-overridden item in a sub/assembly xml or vice versa, 
             //use the values from the prefab instead of loading them from the sub/assembly xml
-            if (thisIsOverride == prefab.IsOverride)
+            bool usePrefabValues = thisIsOverride == prefab.IsOverride;
+            List<ItemComponent> unloadedComponents = new List<ItemComponent>(item.components);
+            foreach (XElement subElement in element.Elements())
             {
-                List<ItemComponent> unloadedComponents = new List<ItemComponent>(item.components);
-                foreach (XElement subElement in element.Elements())
-                {
-                    ItemComponent component = unloadedComponents.Find(x => x.Name == subElement.Name.ToString());
-                    if (component == null) { continue; }
-
-                    component.Load(subElement);
-                    unloadedComponents.Remove(component);
-                }
-            }
-            else
+                ItemComponent component = unloadedComponents.Find(x => x.Name == subElement.Name.ToString());
+                if (component == null) { continue; }
+                component.Load(subElement, usePrefabValues);
+                unloadedComponents.Remove(component);
+            }            
+            if (!usePrefabValues)
             {
                 //use prefab scale when overriding a non-overridden item or vice versa
                 item.Scale = prefab.ConfigElement.GetAttributeFloat(item.scale, "scale", "Scale");

Barotrauma/BarotraumaShared/Source/Items/Components/Holdable/Holdable.cs
@@ -191,9 +191,16 @@ namespace Barotrauma.Items.Components
             }    
         }
 
-        public override void Load(XElement componentElement)
+        public override void Load(XElement componentElement, bool usePrefabValues)
         {
-            base.Load(componentElement);
+            base.Load(componentElement, usePrefabValues);
+
+            if (usePrefabValues)
+            {
+                //this needs to be loaded regardless
+                Attached = componentElement.GetAttributeBool("attached", attached);
+            }
+
             if (attachable)
             {
                 prevMsg = DisplayMsg;

Barotrauma/BarotraumaShared/Source/Items/Components/ItemComponent.cs
@@ -667,9 +667,9 @@ namespace Barotrauma.Items.Components
             }
         }
         
-        public virtual void Load(XElement componentElement)
+        public virtual void Load(XElement componentElement, bool usePrefabValues)
         {
-            if (componentElement == null) return;
+            if (componentElement == null || usePrefabValues) { return; }
             foreach (XAttribute attribute in componentElement.Attributes())
             {
                 if (!SerializableProperties.TryGetValue(attribute.Name.ToString().ToLowerInvariant(), out SerializableProperty property)) continue;

Barotrauma/BarotraumaShared/Source/Items/Components/ItemContainer.cs
@@ -282,20 +282,16 @@ namespace Barotrauma.Items.Components
             }               
         }        
 
-        public override void Load(XElement componentElement)
+        public override void Load(XElement componentElement, bool usePrefabValues)
         {
-            base.Load(componentElement);
+            base.Load(componentElement, usePrefabValues);
 
             string containedString = componentElement.GetAttributeString("contained", "");
-
             string[] itemIdStrings = containedString.Split(',');
-
             itemIds = new ushort[itemIdStrings.Length];
             for (int i = 0; i < itemIdStrings.Length; i++)
             {
-                ushort id = 0;
-                if (!ushort.TryParse(itemIdStrings[i], out id)) continue;
-
+                if (!ushort.TryParse(itemIdStrings[i], out ushort id)) { continue; }
                 itemIds[i] = id;
             }
         }

Barotrauma/BarotraumaShared/Source/Items/Components/Signal/ConnectionPanel.cs
@@ -171,9 +171,9 @@ namespace Barotrauma.Items.Components
             return true;
         }
 
-        public override void Load(XElement element)
+        public override void Load(XElement element, bool usePrefabValues)
         {
-            base.Load(element);
+            base.Load(element, usePrefabValues);
 
             List<Connection> loadedConnections = new List<Connection>();
 

Barotrauma/BarotraumaShared/Source/Items/Components/Signal/Wire.cs
@@ -674,9 +674,9 @@ namespace Barotrauma.Items.Components
             UpdateSections();
         }
 
-        public override void Load(XElement componentElement)
+        public override void Load(XElement componentElement, bool usePrefabValues)
         {
-            base.Load(componentElement);
+            base.Load(componentElement, usePrefabValues);
 
             string nodeString = componentElement.GetAttributeString("nodes", "");
             if (nodeString == "") return;

commit 78d84b68c626610f777ef1ea2a190642b1acd0de

Derp: fixed prefab values overriding item properties when the item is NOT overridden (instead of the other way around). Broken in a4239a2

Barotrauma/BarotraumaShared/Source/Items/Item.cs
@@ -2138,7 +2138,7 @@ namespace Barotrauma
 
             //if we're overriding a non-overridden item in a sub/assembly xml or vice versa, 
             //use the values from the prefab instead of loading them from the sub/assembly xml
-            bool usePrefabValues = thisIsOverride == prefab.IsOverride;
+            bool usePrefabValues = thisIsOverride != prefab.IsOverride;
             List<ItemComponent> unloadedComponents = new List<ItemComponent>(item.components);
             foreach (XElement subElement in element.Elements())
             {
@@ -2146,8 +2146,8 @@ namespace Barotrauma
                 if (component == null) { continue; }
                 component.Load(subElement, usePrefabValues);
                 unloadedComponents.Remove(component);
-            }            
-            if (!usePrefabValues)
+            }
+            if (usePrefabValues)
             {
                 //use prefab scale when overriding a non-overridden item or vice versa
                 item.Scale = prefab.ConfigElement.GetAttributeFloat(item.scale, "scale", "Scale");