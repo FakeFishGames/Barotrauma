77a976184942197a8da6985c00d3d657d3f1a9a7
Joonas Rikkonen <poe.regalis@gmail.com>
2019-10-17 22:43:03 +0300

    Improved the look of the file transfer indicator

--- Barotrauma/BarotraumaClient/Source/Screens/NetLobbyScreen.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/NetLobbyScreen.cs
@@ -464,8 +464,8 @@ namespace Barotrauma
             {
                 Stretch = true
             };
-            FileTransferProgressBar = new GUIProgressBar(new RectTransform(new Vector2(0.6f, 1.0f), fileTransferBottom.RectTransform), 0.0f, Color.LightGreen);
-            FileTransferProgressText = new GUITextBlock(new RectTransform(new Vector2(1.0f, 0.5f), FileTransferProgressBar.RectTransform), "", 
+            FileTransferProgressBar = new GUIProgressBar(new RectTransform(new Vector2(0.6f, 1.0f), fileTransferBottom.RectTransform), 0.0f, Color.DarkGreen);
+            FileTransferProgressText = new GUITextBlock(new RectTransform(Vector2.One, FileTransferProgressBar.RectTransform), "", 
                 font: GUI.SmallFont, textAlignment: Alignment.CenterLeft);
             new GUIButton(new RectTransform(new Vector2(0.4f, 1.0f), fileTransferBottom.RectTransform), TextManager.Get("cancel"))
             {
