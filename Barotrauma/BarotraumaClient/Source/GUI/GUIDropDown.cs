FakeFishGames/Barotrauma

commit 8b21777e7a2e65bb84f53ef13de8a207aafeee0c

Fixed dropdowns not opening in the character editor

Barotrauma/BarotraumaClient/Source/GUI/GUIDropDown.cs
@@ -183,7 +183,10 @@ namespace Barotrauma
             parentHierarchy.Clear();
             parentHierarchy = new List<RectTransform>() { RectTransform.Parent };
             RectTransform parent = parentHierarchy.Last();
-            while (parent?.Parent != null && !(parent.Parent is GUICanvas))
+            while (parent?.Parent?.GUIComponent != null &&
+                parent.Parent.GUIComponent != Screen.Selected?.Frame &&
+                parent.Parent.GUIComponent.Style != null &&
+                !(parent.Parent is GUICanvas))
             {
                 parent = parent.Parent;
                 parentHierarchy.Add(parent);

commit 888c53a868d6df143aad27bd67c1696f22bafcab

Fixed shuttle dropdown being drawn below the submarine preview in server lobby (got broken again by 8b21777)

Barotrauma/BarotraumaClient/Source/GUI/GUIDropDown.cs
@@ -181,17 +181,33 @@ namespace Barotrauma
         private RectTransform FindHighestParent()
         {
             parentHierarchy.Clear();
+
+            //collect entire parent hierarchy to a list
             parentHierarchy = new List<RectTransform>() { RectTransform.Parent };
             RectTransform parent = parentHierarchy.Last();
-            while (parent?.Parent?.GUIComponent != null &&
-                parent.Parent.GUIComponent != Screen.Selected?.Frame &&
-                parent.Parent.GUIComponent.Style != null &&
-                !(parent.Parent is GUICanvas))
+            while (parent?.Parent != null)
             {
+                parentHierarchy.Add(parent.Parent);
                 parent = parent.Parent;
-                parentHierarchy.Add(parent);
             }
-            return parent;
+
+            //find the highest parent that has a guicomponent with a style 
+            //(and so should be rendered and not just some empty parent/root element used for constructing a layout)
+            for (int i = parentHierarchy.Count - 1; i > 0; i--)
+            {
+                if (parentHierarchy[i] is GUICanvas ||
+                    parentHierarchy[i].GUIComponent == null ||
+                    parentHierarchy[i].GUIComponent.Style == null ||
+                    parentHierarchy[i].GUIComponent == Screen.Selected?.Frame)
+                {
+                    parentHierarchy.RemoveAt(i);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return parentHierarchy.Last();
         }
                 
         public void AddItem(string text, object userData = null, string toolTip = "")