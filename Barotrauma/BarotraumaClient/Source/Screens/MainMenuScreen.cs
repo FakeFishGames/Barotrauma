a32b4d0cb68ca139cb796007ef919eb1ad5cf67b
juanjp600 <juanjprot@gmail.com>
2019-10-24 14:53:59 -0300

    Launch server through CMD on Windows to work around alt-tab locking in fullscreen

--- Barotrauma/BarotraumaClient/Source/Screens/MainMenuScreen.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/MainMenuScreen.cs
@@ -749,6 +749,10 @@ namespace Barotrauma
                 string filename = exeName;
 #if LINUX || OSX
                 filename = "./" + Path.GetFileNameWithoutExtension(exeName);
+#elif WINDOWS
+                DateTime startTime = DateTime.Now;
+                filename = "cmd.exe";
+                arguments = "/C start /b " + exeName + " " + arguments;
 #endif
                 var processInfo = new ProcessStartInfo
                 {
@@ -762,6 +766,10 @@ namespace Barotrauma
                 };
                 GameMain.ServerChildProcess = Process.Start(processInfo);
                 Thread.Sleep(1000); //wait until the server is ready before connecting
+
+#if WINDOWS
+                GameMain.ServerChildProcess = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(exeName)).First(p => p.StartTime >= startTime);
+#endif
 
                 GameMain.Client = new GameClient(name, System.Net.IPAddress.Loopback.ToString(), Steam.SteamManager.GetSteamID(), name, ownerKey, true);
             }
