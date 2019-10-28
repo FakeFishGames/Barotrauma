3d77c3bd9475adfb0006f873c0cd2623bfdc33fc
Regalis <poe.regalis@gmail.com>
2019-10-28 12:25:20 +0100

    Send hull updates every 5 seconds even if the status of the hull hasn't changed

--- Barotrauma/BarotraumaShared/Source/Networking/NetConfig.cs
+++ Barotrauma/BarotraumaShared/Source/Networking/NetConfig.cs
@@ -44,6 +44,7 @@ namespace Barotrauma.Networking
         public const float ItemConditionUpdateInterval = 0.15f;
         public const float LevelObjectUpdateInterval = 0.5f;
         public const float HullUpdateInterval = 0.5f;
+        public const float SparseHullUpdateInterval = 5.0f;
         public const float HullUpdateDistance = 20000.0f;
 
         public const int MaxEventPacketsPerUpdate = 4;
