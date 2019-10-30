0dcb54a658041e6338f9e8fe41735136883de934
Regalis <poe.regalis@gmail.com>
2019-10-30 10:52:26 +0100

    Fixed and re-enabled job indicators

--- Barotrauma/BarotraumaClient/Source/Characters/CharacterInfo.cs
+++ Barotrauma/BarotraumaClient/Source/Characters/CharacterInfo.cs
@@ -215,8 +215,6 @@ namespace Barotrauma
 
         private void DrawAttachmentSprite(SpriteBatch spriteBatch, WearableSprite attachment, Sprite head, Vector2 drawPos, float scale, float depthStep, SpriteEffects spriteEffects = SpriteEffects.None)
         {
-            // Disable job indicators for now TODO: undo and fix them
-            if (attachment.Type == WearableType.JobIndicator) { return; }
             if (attachment.InheritSourceRect)
             {
                 if (attachment.SheetIndex.HasValue)
