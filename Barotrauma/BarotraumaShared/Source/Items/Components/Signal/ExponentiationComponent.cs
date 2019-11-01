d638f27b140e52b3479c4b002b34a1950d0f09c6
Kasperi Ekqvist <kasperi.ekqvist@gmail.com>
2019-11-01 15:48:31 +0200

    Create ExponentiationComponent

---
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Signal/ExponentiationComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Xml.Linq;
+
+namespace Barotrauma.Items.Components
+{
+    class ExponentiationComponent : AdderComponent
+    {
+        public ExponentiationComponent(Item item, XElement element)
+            : base(item, element)
+        {
+            IsActive = true;
+        }
+
+        public override void Update(float deltaTime, Camera cam)
+        {
+            bool sendOutput = true;
+            for (int i = 0; i < timeSinceReceived.Length; i++)
+            {
+                if (timeSinceReceived[i] > timeFrame) sendOutput = false;
+                timeSinceReceived[i] += deltaTime;
+            }
+            if (sendOutput)
+            {
+                item.SendSignal(0, MathUtils.Pow(receivedSignal[0], receivedSignal[1]).ToString("G", CultureInfo.InvariantCulture), "signal_out", null);
+            }
+        }
+    }
+}
