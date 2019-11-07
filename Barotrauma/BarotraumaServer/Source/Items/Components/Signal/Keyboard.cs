c3340a5bd50e3033bd66631fd9927a1d95cf66b4
Kasperi Ekqvist <kasperi.ekqvist@gmail.com>
2019-11-07 15:19:53 +0200

    Create server logic for Keyboard component

---
+++ Barotrauma/BarotraumaServer/Source/Items/Components/Signal/Keyboard.cs
@@ -0,0 +1,26 @@
+﻿using Barotrauma.Networking;
+
+namespace Barotrauma.Items.Components
+{
+    partial class Keyboard : ItemComponent, IClientSerializable, IServerSerializable
+    {
+        public void ServerRead(ClientNetObject type, IReadMessage msg, Client c)
+        {
+            string newOutputValue = msg.ReadString();
+
+            if (item.CanClientAccess(c))
+            {
+                GameServer.Log(c.Character.LogName + " set the output value of " + item.Name + " to " + newOutputValue,
+                    ServerLog.MessageType.ItemInteraction);
+                OutputValue = newOutputValue;
+            }
+
+            item.CreateServerEvent(this);
+        }
+
+        public void ServerWrite(IWriteMessage msg, Client c, object[] extraData = null)
+        {
+            msg.Write(OutputValue);
+        }
+    }
+}
