c9f6dc704dbc4a5fb8f76a0f6bcd1999f11d0975
Kasperi Ekqvist <kasperi.ekqvist@gmail.com>
2019-11-07 15:10:13 +0200

    Improve Keyboard component

--- Barotrauma/BarotraumaShared/Source/Items/Components/Signal/Keyboard.cs
+++ Barotrauma/BarotraumaShared/Source/Items/Components/Signal/Keyboard.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 
 namespace Barotrauma.Items.Components
 {
     partial class Keyboard : ItemComponent
     {
-        private List<string> history;
-        private int maxHistoryCount = 10;
+        private string OutputValue { get; set; }
 
         public Keyboard(Item item, XElement element)
             : base(item, element)
         {
             IsActive = true;
-            history = new List<string>();
-            for (int i = 0; i < maxHistoryCount; i++) { history.Add(null); }
             InitProjSpecific(element);
         }
 
@@ -21,7 +17,7 @@ namespace Barotrauma.Items.Components
 
         public override void Update(float deltaTime, Camera cam)
         {
-            item.SendSignal(0, history[0], "signal_out", null);
+            item.SendSignal(0, OutputValue, "signal_out", null);
         }
     }
 }
