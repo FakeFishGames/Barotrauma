FakeFishGames/Barotrauma

commit ced6c1c8e27ba49d5fdc068694fdcad446f37d70

Fixed bullets not emitting sparks when hitting walls. Happened because the OnImpact StatusEffect targeted a character, and projectiles didn't support targeting the entity that was hit unless it was a limb/character. Also required some changes to the way StatusEffects are synced: now the server can notify clients about a StatusEffect that targets something else than a character, and include the position where the StatusEffect should be executed.

Barotrauma/BarotraumaClient/Source/Items/Item.cs
@@ -919,18 +919,25 @@ namespace Barotrauma
                         byte componentIndex = msg.ReadByte();
                         ushort targetID = msg.ReadUInt16();
                         byte targetLimbID = msg.ReadByte();
+                        Vector2? worldPosition = null;
+                        bool hasPosition = msg.ReadBoolean();
+                        if (hasPosition)
+                        {
+                            worldPosition = new Vector2(msg.ReadSingle(), msg.ReadSingle());
+                        }
 
                         ItemComponent targetComponent = componentIndex < components.Count ? components[componentIndex] : null;
-                        Character target = FindEntityByID(targetID) as Character;
-                        Limb targetLimb = target != null && targetLimbID < target.AnimController.Limbs.Length ? target.AnimController.Limbs[targetLimbID] : null;
-                        
+                        Entity target = FindEntityByID(targetID);
+                        Limb targetLimb = target is Character targetCharacter && targetLimbID < targetCharacter.AnimController.Limbs.Length ? 
+                            targetCharacter.AnimController.Limbs[targetLimbID] : null;
+
                         if (targetComponent == null)
                         {
-                            ApplyStatusEffects(actionType, 1.0f, target, targetLimb, true);
+                            ApplyStatusEffects(actionType, 1.0f, target, targetLimb, true, worldPosition: worldPosition);
                         }
                         else
                         {
-                            targetComponent.ApplyStatusEffects(actionType, 1.0f, target, targetLimb);
+                            targetComponent.ApplyStatusEffects(actionType, 1.0f, target, targetLimb, worldPosition: worldPosition);
                         }                        
                     }
                     break;

Barotrauma/BarotraumaServer/Source/Items/Item.cs
@@ -102,6 +102,8 @@ namespace Barotrauma
                         ItemComponent targetComponent = extraData.Length > 2 ? (ItemComponent)extraData[2] : null;
                         ushort targetID = extraData.Length > 3 ? (ushort)extraData[3] : (ushort)0;
                         Limb targetLimb = extraData.Length > 4 ? (Limb)extraData[4] : null;
+                        Vector2? worldPosition = null;
+                        if (extraData.Length > 5) { worldPosition = (Vector2)extraData[5]; }
 
                         Character targetCharacter = FindEntityByID(targetID) as Character;
                         byte targetLimbIndex = targetLimb != null && targetCharacter != null ? (byte)Array.IndexOf(targetCharacter.AnimController.Limbs, targetLimb) : (byte)255;
@@ -110,6 +112,12 @@ namespace Barotrauma
                         msg.Write((byte)(targetComponent == null ? 255 : components.IndexOf(targetComponent)));
                         msg.Write(targetID);
                         msg.Write(targetLimbIndex);
+                        msg.Write(worldPosition.HasValue);
+                        if (worldPosition.HasValue)
+                        {
+                            msg.Write(worldPosition.Value.X);
+                            msg.Write(worldPosition.Value.Y);
+                        }
                     }
                     break;
                 case NetEntityEvent.Type.ChangeProperty:

Barotrauma/BarotraumaShared/Source/Items/Components/ItemComponent.cs
@@ -669,7 +669,7 @@ namespace Barotrauma.Items.Components
             }
         }
         
-        public void ApplyStatusEffects(ActionType type, float deltaTime, Character character = null, Limb targetLimb = null, Character user = null)
+        public void ApplyStatusEffects(ActionType type, float deltaTime, Entity target = null, Limb targetLimb = null, Character user = null, Vector2? worldPosition = null)
         {
             if (statusEffectLists == null) return;
 
@@ -680,7 +680,7 @@ namespace Barotrauma.Items.Components
             {
                 if (broken && effect.type != ActionType.OnBroken) { continue; }
                 if (user != null) { effect.SetUser(user); }
-                item.ApplyStatusEffect(effect, type, deltaTime, character, targetLimb, false, false);
+                item.ApplyStatusEffect(effect, type, deltaTime, target, targetLimb, false, false, worldPosition);
             }
         }
         

Barotrauma/BarotraumaShared/Source/Items/Components/Projectile.cs
@@ -494,19 +494,26 @@ namespace Barotrauma.Items.Components
                             }
                         }
                     }
+#if SERVER
+                    if (GameMain.NetworkMember.IsServer)
+                    {
+                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnUse, this, targetLimb.character.ID, targetLimb, item.WorldPosition });
+                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnImpact, this, targetLimb.character.ID, targetLimb, item.WorldPosition });
+                    }
+#endif
                 }
                 else
                 {
-                    ApplyStatusEffects(ActionType.OnUse, 1.0f,  user: user);
-                    ApplyStatusEffects(ActionType.OnImpact, 1.0f,  user: user);
-                }
+                    ApplyStatusEffects(ActionType.OnUse, 1.0f, target: target.Body.UserData as Entity, user: user);
+                    ApplyStatusEffects(ActionType.OnImpact, 1.0f, target: target.Body.UserData as Entity, user: user);
 #if SERVER
-                if (GameMain.NetworkMember.IsServer)
-                {
-                    GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnUse });
-                    GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnImpact });
-                }
+                    if (GameMain.NetworkMember.IsServer)
+                    {
+                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnUse, this, (target.Body.UserData as Entity)?.ID ?? 0, null, item.WorldPosition });
+                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnImpact, this, (target.Body.UserData as Entity)?.ID ?? 0, null, item.WorldPosition });
+                    }
 #endif
+                }
             }
 
             item.body.FarseerBody.OnCollision -= OnProjectileCollision;

Barotrauma/BarotraumaShared/Source/Items/Item.cs
@@ -1060,18 +1060,18 @@ namespace Barotrauma
             return true;
         }
 
-        public void ApplyStatusEffects(ActionType type, float deltaTime, Character character = null, Limb limb = null, bool isNetworkEvent = false)
+        public void ApplyStatusEffects(ActionType type, float deltaTime, Entity target = null, Limb limb = null, bool isNetworkEvent = false, Vector2? worldPosition = null)
         {
             if (!hasStatusEffectsOfType[(int)type]) { return; }
             foreach (StatusEffect effect in statusEffectLists[type])
             {
-                ApplyStatusEffect(effect, type, deltaTime, character, limb, isNetworkEvent, false);
+                ApplyStatusEffect(effect, type, deltaTime, target, limb, isNetworkEvent, false, worldPosition);
             }
         }
         
         readonly List<ISerializableEntity> targets = new List<ISerializableEntity>();
 
-        public void ApplyStatusEffect(StatusEffect effect, ActionType type, float deltaTime, Character character = null, Limb limb = null, bool isNetworkEvent = false, bool checkCondition = true)
+        public void ApplyStatusEffect(StatusEffect effect, ActionType type, float deltaTime, Entity target = null, Limb limb = null, bool isNetworkEvent = false, bool checkCondition = true, Vector2? worldPosition = null)
         {
             if (!isNetworkEvent && checkCondition)
             {
@@ -1110,6 +1110,13 @@ namespace Barotrauma
                 if (targets.Count > 0) { hasTargets = true; }
             }
 
+            if (effect.HasTargetType(StatusEffect.TargetType.UseTarget) && target is ISerializableEntity serializableTarget &&
+                (!(target is Character) || !effect.HasTargetType(StatusEffect.TargetType.Character)))
+            {
+                hasTargets = true;
+                targets.Add(serializableTarget);
+            }
+
             if (!hasTargets) { return; }
 
             if (effect.HasTargetType(StatusEffect.TargetType.Hull) && CurrentHull != null)
@@ -1125,30 +1132,32 @@ namespace Barotrauma
                 }
             }
 
-            if (effect.HasTargetType(StatusEffect.TargetType.Character))
+            if (target is Character character)
             {
-                if (type == ActionType.OnContained && ParentInventory is CharacterInventory characterInventory)
+                if (effect.HasTargetType(StatusEffect.TargetType.Character))
                 {
-                    targets.Add(characterInventory.Owner as ISerializableEntity);
+                    if (type == ActionType.OnContained && ParentInventory is CharacterInventory characterInventory)
+                    {
+                        targets.Add(characterInventory.Owner as ISerializableEntity);
+                    }
+                    else
+                    {
+                        targets.Add(character);
+                    }
                 }
-                else
+                if (effect.HasTargetType(StatusEffect.TargetType.AllLimbs))
                 {
-                    targets.Add(character);
+                    targets.AddRange(character.AnimController.Limbs.ToList());
                 }
             }
-
             if (effect.HasTargetType(StatusEffect.TargetType.Limb))
             {
                 targets.Add(limb);
             }
-            if (effect.HasTargetType(StatusEffect.TargetType.AllLimbs))
-            {
-                targets.AddRange(character.AnimController.Limbs.ToList());
-            }
             
             if (Container != null && effect.HasTargetType(StatusEffect.TargetType.Parent)) targets.Add(Container);
             
-            effect.Apply(type, deltaTime, this, targets);            
+            effect.Apply(type, deltaTime, this, targets, worldPosition);            
         }
 
 

Barotrauma/BarotraumaShared/Content/Items/Weapons/weapons.xml
@@ -148,7 +148,7 @@
         <Affliction identifier="gunshotwound" strength="40" />
         <Affliction identifier="internaldamage" strength="30" />
       </Attack>
-      <StatusEffect type="OnImpact" target="Character">
+      <StatusEffect type="OnImpact" target="UseTarget">
         <Conditional entitytype="eq Structure"/>
         <ParticleEmitter particle="spark" copyentityangle="true" anglemin="-10" anglemax="10" particleamount="5" velocitymin="-10" velocitymax="-200" scalemin="0.5" scalemax="1" />
       </StatusEffect>
@@ -191,7 +191,7 @@
         <Affliction identifier="gunshotwound" strength="15" />
         <Affliction identifier="internaldamage" strength="10" />
       </Attack>
-      <StatusEffect type="OnImpact" target="Character">
+      <StatusEffect type="OnImpact" target="UseTarget">
         <Conditional entitytype="eq Structure"/>
         <ParticleEmitter particle="spark" copyentityangle="true" anglemin="-10" anglemax="10" particleamount="5" velocitymin="-10" velocitymax="-200" scalemin="0.5" scalemax="1" />
       </StatusEffect>
@@ -703,7 +703,7 @@
         <Affliction identifier="gunshotwound" strength="6" />
         <Affliction identifier="internaldamage" strength="5" />
       </Attack>
-      <StatusEffect type="OnImpact" target="Character">
+      <StatusEffect type="OnImpact" target="UseTarget">
         <Conditional entitytype="eq Structure"/>
         <ParticleEmitter particle="spark" copyentityangle="true" anglemin="-10" anglemax="10" particleamount="5" velocitymin="-10" velocitymax="-200" scalemin="0.5" scalemax="1" />
       </StatusEffect>
@@ -766,7 +766,7 @@
         <Sound file="Content/Items/Weapons/TaserHit2.ogg" type="OnUse" range="3000" />
         <Remove />
       </StatusEffect>
-      <StatusEffect type="OnImpact" target="Character">
+      <StatusEffect type="OnImpact" target="UseTarget">
         <Conditional entitytype="eq Structure"/>
         <ParticleEmitter particle="spark" copyentityangle="true" anglemin="-10" anglemax="10" particleamount="5" velocitymin="-10" velocitymax="-200" scalemin="0.5" scalemax="1" />
       </StatusEffect>

commit a10bcc2b793f7f7b71125e5f278911338765f40b

Fixed user being considered the UseTarget of a StatusEffect (caused by ced6c1c), causing plasma cutters and welding tools to always do burn damage to the user and probably other issues as well.

Barotrauma/BarotraumaClient/Source/Items/Item.cs
@@ -916,28 +916,30 @@ namespace Barotrauma
                 case NetEntityEvent.Type.ApplyStatusEffect:
                     {
                         ActionType actionType = (ActionType)msg.ReadRangedInteger(0, Enum.GetValues(typeof(ActionType)).Length - 1);
-                        byte componentIndex = msg.ReadByte();
-                        ushort targetID = msg.ReadUInt16();
-                        byte targetLimbID = msg.ReadByte();
-                        Vector2? worldPosition = null;
-                        bool hasPosition = msg.ReadBoolean();
+                        byte componentIndex         = msg.ReadByte();
+                        ushort targetCharacterID    = msg.ReadUInt16();
+                        byte targetLimbID           = msg.ReadByte();
+                        ushort useTargetID          = msg.ReadUInt16();
+                        Vector2? worldPosition      = null;
+                        bool hasPosition            = msg.ReadBoolean();
                         if (hasPosition)
                         {
                             worldPosition = new Vector2(msg.ReadSingle(), msg.ReadSingle());
                         }
 
                         ItemComponent targetComponent = componentIndex < components.Count ? components[componentIndex] : null;
-                        Entity target = FindEntityByID(targetID);
-                        Limb targetLimb = target is Character targetCharacter && targetLimbID < targetCharacter.AnimController.Limbs.Length ? 
+                        Character targetCharacter = FindEntityByID(targetCharacterID) as Character;
+                        Limb targetLimb = targetCharacter != null && targetLimbID < targetCharacter.AnimController.Limbs.Length ? 
                             targetCharacter.AnimController.Limbs[targetLimbID] : null;
+                        Entity useTarget = FindEntityByID(useTargetID);
 
                         if (targetComponent == null)
                         {
-                            ApplyStatusEffects(actionType, 1.0f, target, targetLimb, true, worldPosition: worldPosition);
+                            ApplyStatusEffects(actionType, 1.0f, targetCharacter, targetLimb, useTarget, true, worldPosition: worldPosition);
                         }
                         else
                         {
-                            targetComponent.ApplyStatusEffects(actionType, 1.0f, target, targetLimb, worldPosition: worldPosition);
+                            targetComponent.ApplyStatusEffects(actionType, 1.0f, targetCharacter, targetLimb, useTarget, worldPosition: worldPosition);
                         }                        
                     }
                     break;

Barotrauma/BarotraumaServer/Source/Items/Item.cs
@@ -100,18 +100,20 @@ namespace Barotrauma
                     {
                         ActionType actionType = (ActionType)extraData[1];
                         ItemComponent targetComponent = extraData.Length > 2 ? (ItemComponent)extraData[2] : null;
-                        ushort targetID = extraData.Length > 3 ? (ushort)extraData[3] : (ushort)0;
+                        ushort characterID = extraData.Length > 3 ? (ushort)extraData[3] : (ushort)0;
                         Limb targetLimb = extraData.Length > 4 ? (Limb)extraData[4] : null;
+                        ushort useTargetID = extraData.Length > 5 ? (ushort)extraData[5] : (ushort)0;
                         Vector2? worldPosition = null;
-                        if (extraData.Length > 5) { worldPosition = (Vector2)extraData[5]; }
+                        if (extraData.Length > 6) { worldPosition = (Vector2)extraData[6]; }
 
-                        Character targetCharacter = FindEntityByID(targetID) as Character;
+                        Character targetCharacter = FindEntityByID(characterID) as Character;
                         byte targetLimbIndex = targetLimb != null && targetCharacter != null ? (byte)Array.IndexOf(targetCharacter.AnimController.Limbs, targetLimb) : (byte)255;
 
                         msg.WriteRangedInteger((int)actionType, 0, Enum.GetValues(typeof(ActionType)).Length - 1);
                         msg.Write((byte)(targetComponent == null ? 255 : components.IndexOf(targetComponent)));
-                        msg.Write(targetID);
+                        msg.Write(characterID);
                         msg.Write(targetLimbIndex);
+                        msg.Write(useTargetID);
                         msg.Write(worldPosition.HasValue);
                         if (worldPosition.HasValue)
                         {

Barotrauma/BarotraumaShared/Source/Items/Components/ItemComponent.cs
@@ -669,7 +669,7 @@ namespace Barotrauma.Items.Components
             }
         }
         
-        public void ApplyStatusEffects(ActionType type, float deltaTime, Entity target = null, Limb targetLimb = null, Character user = null, Vector2? worldPosition = null)
+        public void ApplyStatusEffects(ActionType type, float deltaTime, Character character = null, Limb targetLimb = null, Entity useTarget = null, Character user = null, Vector2? worldPosition = null)
         {
             if (statusEffectLists == null) return;
 
@@ -680,7 +680,7 @@ namespace Barotrauma.Items.Components
             {
                 if (broken && effect.type != ActionType.OnBroken) { continue; }
                 if (user != null) { effect.SetUser(user); }
-                item.ApplyStatusEffect(effect, type, deltaTime, target, targetLimb, false, false, worldPosition);
+                item.ApplyStatusEffect(effect, type, deltaTime, character, targetLimb, useTarget, false, false, worldPosition);
             }
         }
         

Barotrauma/BarotraumaShared/Source/Items/Components/Power/PowerTransfer.cs
@@ -314,7 +314,7 @@ namespace Barotrauma.Items.Components
 
                     foreach (StatusEffect effect in recipient.Effects)
                     {
-                        recipient.Item.ApplyStatusEffect(effect, ActionType.OnUse, 1.0f, null, null, false, false);
+                        recipient.Item.ApplyStatusEffect(effect, ActionType.OnUse, 1.0f);
                     }
                 }
             }

Barotrauma/BarotraumaShared/Source/Items/Components/Projectile.cs
@@ -497,20 +497,20 @@ namespace Barotrauma.Items.Components
 #if SERVER
                     if (GameMain.NetworkMember.IsServer)
                     {
-                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnUse, this, targetLimb.character.ID, targetLimb, item.WorldPosition });
-                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnImpact, this, targetLimb.character.ID, targetLimb, item.WorldPosition });
+                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnUse, this, targetLimb.character.ID, targetLimb, 0, item.WorldPosition });
+                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnImpact, this, targetLimb.character.ID, targetLimb, 0, item.WorldPosition });
                     }
 #endif
                 }
                 else
                 {
-                    ApplyStatusEffects(ActionType.OnUse, 1.0f, target: target.Body.UserData as Entity, user: user);
-                    ApplyStatusEffects(ActionType.OnImpact, 1.0f, target: target.Body.UserData as Entity, user: user);
+                    ApplyStatusEffects(ActionType.OnUse, 1.0f, useTarget: target.Body.UserData as Entity, user: user);
+                    ApplyStatusEffects(ActionType.OnImpact, 1.0f, useTarget: target.Body.UserData as Entity, user: user);
 #if SERVER
                     if (GameMain.NetworkMember.IsServer)
                     {
-                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnUse, this, (target.Body.UserData as Entity)?.ID ?? 0, null, item.WorldPosition });
-                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnImpact, this, (target.Body.UserData as Entity)?.ID ?? 0, null, item.WorldPosition });
+                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnUse, this, (ushort)0, null, (target.Body.UserData as Entity)?.ID ?? 0, item.WorldPosition });
+                        GameMain.Server?.CreateEntityEvent(item, new object[] { NetEntityEvent.Type.ApplyStatusEffect, ActionType.OnImpact, this, (ushort)0, null, (target.Body.UserData as Entity)?.ID ?? 0, item.WorldPosition });
                     }
 #endif
                 }

Barotrauma/BarotraumaShared/Source/Items/Components/Signal/Connection.cs
@@ -258,7 +258,7 @@ namespace Barotrauma.Items.Components
 
                 foreach (StatusEffect effect in recipient.Effects)
                 {
-                    recipient.Item.ApplyStatusEffect(effect, ActionType.OnUse, (float)Timing.Step, null, null, false, false);
+                    recipient.Item.ApplyStatusEffect(effect, ActionType.OnUse, (float)Timing.Step);
                 }
             }
         }

Barotrauma/BarotraumaShared/Source/Items/Components/Signal/CustomInterface.cs
@@ -172,7 +172,7 @@ namespace Barotrauma.Items.Components
 
                 foreach (StatusEffect effect in ciElement.StatusEffects)
                 {
-                    item.ApplyStatusEffect(effect, ciElement.State ? ActionType.OnUse : ActionType.OnSecondaryUse, 1.0f, null, null, true, false);
+                    item.ApplyStatusEffect(effect, ciElement.State ? ActionType.OnUse : ActionType.OnSecondaryUse, 1.0f, null, null, null, true, false);
                 }
             }
         }

Barotrauma/BarotraumaShared/Source/Items/Item.cs
@@ -1060,18 +1060,18 @@ namespace Barotrauma
             return true;
         }
 
-        public void ApplyStatusEffects(ActionType type, float deltaTime, Entity target = null, Limb limb = null, bool isNetworkEvent = false, Vector2? worldPosition = null)
+        public void ApplyStatusEffects(ActionType type, float deltaTime, Character character = null, Limb limb = null, Entity useTarget = null, bool isNetworkEvent = false, Vector2? worldPosition = null)
         {
             if (!hasStatusEffectsOfType[(int)type]) { return; }
             foreach (StatusEffect effect in statusEffectLists[type])
             {
-                ApplyStatusEffect(effect, type, deltaTime, target, limb, isNetworkEvent, false, worldPosition);
+                ApplyStatusEffect(effect, type, deltaTime, character, limb, useTarget, isNetworkEvent, false, worldPosition);
             }
         }
         
         readonly List<ISerializableEntity> targets = new List<ISerializableEntity>();
 
-        public void ApplyStatusEffect(StatusEffect effect, ActionType type, float deltaTime, Entity target = null, Limb limb = null, bool isNetworkEvent = false, bool checkCondition = true, Vector2? worldPosition = null)
+        public void ApplyStatusEffect(StatusEffect effect, ActionType type, float deltaTime, Character character = null, Limb limb = null, Entity useTarget = null, bool isNetworkEvent = false, bool checkCondition = true, Vector2? worldPosition = null)
         {
             if (!isNetworkEvent && checkCondition)
             {
@@ -1110,8 +1110,7 @@ namespace Barotrauma
                 if (targets.Count > 0) { hasTargets = true; }
             }
 
-            if (effect.HasTargetType(StatusEffect.TargetType.UseTarget) && target is ISerializableEntity serializableTarget &&
-                (!(target is Character) || !effect.HasTargetType(StatusEffect.TargetType.Character)))
+            if (effect.HasTargetType(StatusEffect.TargetType.UseTarget) && useTarget is ISerializableEntity serializableTarget)
             {
                 hasTargets = true;
                 targets.Add(serializableTarget);
@@ -1132,7 +1131,7 @@ namespace Barotrauma
                 }
             }
 
-            if (target is Character character)
+            if (character != null)
             {
                 if (effect.HasTargetType(StatusEffect.TargetType.Character))
                 {
@@ -1577,7 +1576,7 @@ namespace Barotrauma
                 foreach (StatusEffect effect in connection.Effects)
                 {
                     if (condition <= 0.0f && effect.type != ActionType.OnBroken) { continue; }
-                    if (signal != "0" && !string.IsNullOrEmpty(signal)) { ApplyStatusEffect(effect, ActionType.OnUse, (float)Timing.Step, null, null, false, false); }
+                    if (signal != "0" && !string.IsNullOrEmpty(signal)) { ApplyStatusEffect(effect, ActionType.OnUse, (float)Timing.Step); }
                 }
                 connection.SendSignal(stepsTaken, signal, source ?? this, sender, power, signalStrength);
             }