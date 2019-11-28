8db7c22c8ab8b34c44fadf14a1d15a1cd38461e5
Joonas Rikkonen <poe.regalis@gmail.com>
2019-11-28 21:01:00 +0200

    Items can be spawned inside autospawned items (e.g. oxygen tanks spawn inside automatically spawned diving masks), list the spawned items in the debug console

--- Barotrauma/BarotraumaShared/Source/GameSession/AutoItemPlacer.cs
+++ Barotrauma/BarotraumaShared/Source/GameSession/AutoItemPlacer.cs
@@ -8,6 +8,8 @@ namespace Barotrauma
 {
     static class AutoItemPlacer
     {
+        private static readonly List<Item> spawnedItems = new List<Item>();
+
         public static void Place(IEnumerable<Submarine> subs)
         {
             if (GameMain.NetworkMember != null && GameMain.NetworkMember.IsClient)
@@ -23,64 +25,95 @@ namespace Barotrauma
                 containers.AddRange(item.GetComponents<ItemContainer>());
             }
 
-            Dictionary<ItemContainer, int> placedItemCount = new Dictionary<ItemContainer, int>();
-            Dictionary<ItemContainer, PreferredContainer> validContainers = new Dictionary<ItemContainer, PreferredContainer>();
-            foreach (ItemPrefab itemPrefab in MapEntityPrefab.List.Where(mp => mp is ItemPrefab))
+            List<ItemPrefab> prefabsWithContainer = new List<ItemPrefab>();
+            List<ItemPrefab> prefabsWithoutContainer = new List<ItemPrefab>();
+            foreach (MapEntityPrefab prefab in MapEntityPrefab.List)
             {
-                //gather containers the item can spawn in
-                validContainers.Clear();
-                foreach (PreferredContainer preferredContainer in itemPrefab.PreferredContainers)
-                {
-                    if (preferredContainer.SpawnProbability <= 0.0f && preferredContainer.MaxAmount <= 0) { continue; }
-                    foreach (ItemContainer container in containers)
-                    {
-                        if (!preferredContainer.Identifiers.Any(id => container.Item.Prefab.Identifier == id || container.Item.HasTag(id))) { continue; }
+                if (!(prefab is ItemPrefab ip)) { continue; }
 
-                        if (validContainers.ContainsKey(container))
-                        {
-                            //the container has already been marked as valid due to some other PreferredContainer
-                            // -> override if this PreferredContainer has a higher chance of spawning more of the item
-                            // (for example, an item with a 5% chance to spawn in a cabinet and 50% chance to spawn in a supplycabinet
-                            //  has a 50% chance to spawn in an item with both tags cabinet and supplycabinet)
-                            if (GetSpawnPriority(preferredContainer) > GetSpawnPriority(validContainers[container]))
-                            {
-                                validContainers[container] = preferredContainer;
-                            }
-                        }
-                        else
-                        {
-                            validContainers.Add(container, preferredContainer);
-                        }
-                    }
+                if (ip.ConfigElement.Elements().Any(e => e.Name.ToString().ToLower() == typeof(ItemContainer).Name.ToString().ToLower()))
+                {
+                    prefabsWithContainer.Add(ip);
                 }
-
-                foreach (KeyValuePair<ItemContainer, PreferredContainer> validContainer in validContainers)
+                else
                 {
-                    if (Rand.Range(0.0f, 1.0f) > validContainer.Value.SpawnProbability) { continue; }
-                    int amount = Rand.Range(validContainer.Value.MinAmount, validContainer.Value.MaxAmount + 1);
-                    for (int i = 0; i < amount; i++)
-                    {
-                        int placedCount = placedItemCount.ContainsKey(validContainer.Key) ? placedItemCount[validContainer.Key] : 0;
-                        if (validContainer.Key.Inventory.Items.Count(it => it == null) - placedCount <= 0)
-                        {
-                            containers.Remove(validContainer.Key);
-                            break;
-                        }
-                        if (GameMain.NetworkMember != null && GameMain.NetworkMember.IsServer)
-                        {
-                            Entity.Spawner.AddToSpawnQueue(itemPrefab, validContainer.Key.Inventory);
-                            placedItemCount[validContainer.Key]++;
-                        }
-                        else
-                        {
-                            var item = new Item(itemPrefab, validContainer.Key.Item.Position, validContainer.Key.Item.Submarine);
-                            validContainer.Key.Inventory.TryPutItem(item, null);
-                        }
-                    }
+                    prefabsWithoutContainer.Add(ip);
                 }
             }
 
+            spawnedItems.Clear();
+            Dictionary<ItemContainer, PreferredContainer> validContainers = new Dictionary<ItemContainer, PreferredContainer>();
+            //spawn items that have an ItemContainer component first so we can fill them up with items if needed (oxygen tanks inside the spawned diving masks, etc)
+            foreach (ItemPrefab itemPrefab in prefabsWithContainer.OrderBy(sp => Rand.Int(int.MaxValue)).Concat(prefabsWithoutContainer.OrderBy(sp => Rand.Int(int.MaxValue))))
+            {
+                GetValidContainers(itemPrefab, containers, ref validContainers);
+                foreach (KeyValuePair<ItemContainer, PreferredContainer> validContainer in validContainers)
+                {
+                    SpawnItem(itemPrefab, containers, validContainer);
+                }
+            }
+
+            DebugConsole.NewMessage("Automatically placed items: ");
+            foreach (string itemName in spawnedItems.Select(it => it.Name).Distinct())
+            {
+                DebugConsole.NewMessage(" - " + itemName + " x" + spawnedItems.Count(it => it.Name == itemName));
+            }
         }
+
+        private static void GetValidContainers(ItemPrefab itemPrefab, IEnumerable<ItemContainer> allContainers, ref Dictionary<ItemContainer, PreferredContainer> validContainers)
+        {
+            //gather containers the item can spawn in
+            validContainers.Clear();
+            foreach (PreferredContainer preferredContainer in itemPrefab.PreferredContainers)
+            {
+                if (preferredContainer.SpawnProbability <= 0.0f && preferredContainer.MaxAmount <= 0) { continue; }
+                foreach (ItemContainer container in allContainers)
+                {
+                    if (!preferredContainer.Identifiers.Any(id => container.Item.Prefab.Identifier == id || container.Item.HasTag(id))) { continue; }
+
+                    if (validContainers.ContainsKey(container))
+                    {
+                        //the container has already been marked as valid due to some other PreferredContainer
+                        // -> override if this PreferredContainer has a higher chance of spawning more of the item
+                        // (for example, an item with a 5% chance to spawn in a cabinet and 50% chance to spawn in a supplycabinet
+                        //  has a 50% chance to spawn in an item with both tags cabinet and supplycabinet)
+                        if (GetSpawnPriority(preferredContainer) > GetSpawnPriority(validContainers[container]))
+                        {
+                            validContainers[container] = preferredContainer;
+                        }
+                    }
+                    else
+                    {
+                        validContainers.Add(container, preferredContainer);
+                    }
+                }
+            }
+        }
+
+        private static void SpawnItem(ItemPrefab itemPrefab, List<ItemContainer> containers, KeyValuePair<ItemContainer, PreferredContainer> validContainer)
+        {
+            if (Rand.Range(0.0f, 1.0f) > validContainer.Value.SpawnProbability) { return; }
+            int amount = Rand.Range(validContainer.Value.MinAmount, validContainer.Value.MaxAmount + 1);
+            for (int i = 0; i < amount; i++)
+            {
+                if (!validContainer.Key.Inventory.Items.Any(it => it == null))
+                {
+                    containers.Remove(validContainer.Key);
+                    break;
+                }
+
+                var item = new Item(itemPrefab, validContainer.Key.Item.Position, validContainer.Key.Item.Submarine);
+                spawnedItems.Add(item);
+#if SERVER
+                Entity.Spawner.CreateNetworkEvent(item, remove: false);
+#endif
+                validContainer.Key.Inventory.TryPutItem(item, null);
+                containers.AddRange(item.GetComponents<ItemContainer>());
+                
+            }
+        }
+
+
         private static float GetSpawnPriority(PreferredContainer container)
         {
             return Math.Max(container.SpawnProbability, 0.1f) * (container.MinAmount + container.MaxAmount) / 2.0f;
