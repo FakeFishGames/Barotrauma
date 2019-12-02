FakeFishGames/Barotrauma

commit b587f2721026bd40d71a12dae6eec4f0a67ba803

Sync the user field of ConnectionPanel and the CurrentFixer of Repairable, don't play the rewire sound when repairing an item with a screwdriver.

Barotrauma/BarotraumaClient/Source/Items/Components/Signal/ConnectionPanel.cs
@@ -40,7 +40,8 @@ namespace Barotrauma.Items.Components
                     }
                 }
             }
-            if (user != null && user.SelectedConstruction == item && HasRequiredItems(user, addMessage: false))
+            if (user != null && user.SelectedConstruction == item && HasRequiredItems(user, addMessage: false) && 
+                item.GetComponent<Repairable>()?.CurrentFixer != user)
             {
                 if (rewireSoundChannel == null || !rewireSoundChannel.IsPlaying)
                 {
@@ -129,6 +130,17 @@ namespace Barotrauma.Items.Components
         {
             List<Wire> prevWires = Connections.SelectMany(c => c.Wires.Where(w => w != null)).ToList();
             List<Wire> newWires = new List<Wire>();
+
+            ushort userID = msg.ReadUInt16();
+
+            if (userID == 0)
+            {
+                user = null;
+            }
+            else
+            {
+                user = Entity.FindEntityByID(userID) as Character;
+            }
 
             foreach (Connection connection in Connections)
             {

Barotrauma/BarotraumaServer/Source/Items/Components/Signal/ConnectionPanel.cs
@@ -32,7 +32,6 @@ namespace Barotrauma.Items.Components
                 }
             }
 
-
             List<Wire> clientSideDisconnectedWires = new List<Wire>();
             ushort disconnectedWireCount = msg.ReadUInt16();
             for (int i = 0; i < disconnectedWireCount; i++)
@@ -179,6 +178,7 @@ namespace Barotrauma.Items.Components
 
         public void ServerWrite(IWriteMessage msg, Client c, object[] extraData = null)
         {
+            msg.Write(user == null ? (ushort)0 : user.ID);
             ClientWrite(msg, extraData);
         }
     }

Barotrauma/BarotraumaShared/Source/Items/Components/Signal/ConnectionPanel.cs
@@ -126,6 +126,9 @@ namespace Barotrauma.Items.Components
 
             if (user == null || user.SelectedConstruction != item)
             {
+#if SERVER
+                if (user != null) { item.CreateServerEvent(this); }
+#endif
                 user = null;
                 return;
             }
@@ -133,6 +136,11 @@ namespace Barotrauma.Items.Components
             if (!user.Enabled || !HasRequiredItems(user, addMessage: false)) { return; }
 
             user.AnimController.UpdateUseItem(true, item.WorldPosition + new Vector2(0.0f, 100.0f) * (((float)Timing.TotalTime / 10.0f) % 0.1f));
+        }
+
+        public override void UpdateBroken(float deltaTime, Camera cam)
+        {
+            Update(deltaTime, cam);
         }
 
         partial void UpdateProjSpecific(float deltaTime);
@@ -147,13 +155,16 @@ namespace Barotrauma.Items.Components
             }
 
             user = picker;
+#if SERVER
+            if (user != null) { item.CreateServerEvent(this); }
+#endif
             IsActive = true;
             return true;
         }
         
         public override bool Use(float deltaTime, Character character = null)
         {
-            if (character == null || character != user) return false;
+            if (character == null || character != user) { return false; }
 
             var powered = item.GetComponent<Powered>();
             if (powered != null)

Barotrauma/BarotraumaClient/Source/Items/Components/Repairable.cs
@@ -142,7 +142,7 @@ namespace Barotrauma.Items.Components
             {
                 if (repairSoundChannel == null || !repairSoundChannel.IsPlaying)
                 {
-                repairSoundChannel = SoundPlayer.PlaySound("repair", item.WorldPosition, hullGuess: item.CurrentHull);
+                    repairSoundChannel = SoundPlayer.PlaySound("repair", item.WorldPosition, hullGuess: item.CurrentHull);
                 }
             }
             else
@@ -221,8 +221,17 @@ namespace Barotrauma.Items.Components
             deteriorationTimer = msg.ReadSingle();
             deteriorateAlwaysResetTimer = msg.ReadSingle();
             DeteriorateAlways = msg.ReadBoolean();
-            CurrentFixer = msg.ReadBoolean() ? Character.Controlled : null;
+            ushort currentFixerID = msg.ReadUInt16();
             currentFixerAction = (FixActions)msg.ReadRangedInteger(0, 2);
+
+            if (currentFixerID == 0)
+            {
+                CurrentFixer = null;
+            }
+            else
+            {
+                CurrentFixer = Entity.FindEntityByID(currentFixerID) as Character;
+            }
         }
 
         public void ClientWrite(IWriteMessage msg, object[] extraData = null)

Barotrauma/BarotraumaServer/Source/Items/Components/Repairable.cs
@@ -38,7 +38,7 @@ namespace Barotrauma.Items.Components
             msg.Write(deteriorationTimer);
             msg.Write(deteriorateAlwaysResetTimer);
             msg.Write(DeteriorateAlways);
-            msg.Write(CurrentFixer == c.Character);
+            msg.Write(CurrentFixer == null ? (ushort)0 : CurrentFixer.ID);
             msg.WriteRangedInteger((int)currentFixerAction, 0, 2);
         }
     }

commit 3eb8bae188f22e8ea415a7e53814f5052d80b368

Fixed clients reading an incorrect number of bits from ConnectionPanel messages when midround syncing, causing them to read the rest of the message incorrectly and disconnect with an "invalid object header" error. Was caused by b587f272

Barotrauma/BarotraumaClient/Source/Items/Components/Signal/ConnectionPanel.cs
@@ -104,6 +104,7 @@ namespace Barotrauma.Items.Components
                 //delay reading the state until midround syncing is done
                 //because some of the wires connected to the panel may not exist yet
                 long msgStartPos = msg.BitPosition;
+                msg.ReadUInt16(); //user ID
                 foreach (Connection connection in Connections)
                 {
                     for (int i = 0; i < Connection.MaxLinked; i++)