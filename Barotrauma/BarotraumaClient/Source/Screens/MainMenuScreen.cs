bb608b7cd76de9bce8637c15e3ead9e19dee98e3
Regalis <poe.regalis@gmail.com>
2019-12-18 17:54:16 +0100

    Karma can be enabled and karma settings preset selected in the "host server" menu, fixed NetPropertyData.PropEquals throwin an exception if a string value is null

--- Barotrauma/BarotraumaClient/Source/Screens/MainMenuScreen.cs
+++ Barotrauma/BarotraumaClient/Source/Screens/MainMenuScreen.cs
@@ -29,7 +29,8 @@ namespace Barotrauma
         private readonly CampaignSetupUI campaignSetupUI;
 
         private GUITextBox serverNameBox, /*portBox, queryPortBox,*/ passwordBox, maxPlayersBox;
-        private GUITickBox isPublicBox, wrongPasswordBanBox;
+        private GUITickBox isPublicBox, wrongPasswordBanBox, karmaEnabledBox;
+        private GUIDropDown karmaPresetDD;
         private readonly GUIButton joinServerButton, hostServerButton, steamWorkshopButton;
         private readonly GameMain game;
 
@@ -830,6 +831,8 @@ namespace Barotrauma
                                    " -public " + isPublicBox.Selected.ToString() +
                                    " -playstyle " + ((PlayStyle)playstyleBanner.UserData).ToString()  +
                                    " -banafterwrongpassword " + wrongPasswordBanBox.Selected.ToString() +
+                                   " -karmaenabled " + karmaEnabledBox.Selected.ToString() +
+                                   " -karmapreset " + (karmaPresetDD.SelectedData?.ToString() ?? "default") +
                                    " -maxplayers " + maxPlayersBox.Text;
 
                 if (!string.IsNullOrWhiteSpace(passwordBox.Text))
@@ -1192,14 +1195,42 @@ namespace Barotrauma
                 Censor = true
             };
 
-            var tickboxArea = new GUILayoutGroup(new RectTransform(new Vector2(1.0f, tickBoxSize.Y), parent.RectTransform), isHorizontal: true);
+            // tickbox upper ---------------
 
-            isPublicBox = new GUITickBox(new RectTransform(new Vector2(0.5f, 1.0f), tickboxArea.RectTransform), TextManager.Get("PublicServer"))
+            var tickboxAreaUpper = new GUILayoutGroup(new RectTransform(new Vector2(1.0f, tickBoxSize.Y), parent.RectTransform), isHorizontal: true);
+
+            isPublicBox = new GUITickBox(new RectTransform(new Vector2(0.5f, 1.0f), tickboxAreaUpper.RectTransform), TextManager.Get("PublicServer"))
             {
                 ToolTip = TextManager.Get("PublicServerToolTip")
             };
 
-            wrongPasswordBanBox = new GUITickBox(new RectTransform(new Vector2(0.5f, 1.0f), tickboxArea.RectTransform), TextManager.Get("ServerSettingsBanAfterWrongPassword"));
+            wrongPasswordBanBox = new GUITickBox(new RectTransform(new Vector2(0.5f, 1.0f), tickboxAreaUpper.RectTransform), TextManager.Get("ServerSettingsBanAfterWrongPassword"));
+
+            // tickbox lower ---------------
+
+            var tickboxAreaLower = new GUILayoutGroup(new RectTransform(new Vector2(1.0f, tickBoxSize.Y), parent.RectTransform), isHorizontal: true);
+
+            karmaEnabledBox = new GUITickBox(new RectTransform(new Vector2(0.4f, 1.0f), tickboxAreaLower.RectTransform), TextManager.Get("ServerSettingsUseKarma"))
+            {
+                ToolTip = TextManager.Get("karmaexplanation"),
+                OnSelected = (tb) =>
+                {
+                    karmaPresetDD.Enabled = karmaPresetDD.ButtonEnabled = tb.Selected;
+                    return true;
+                }                
+            };
+            karmaPresetDD = new GUIDropDown(new RectTransform(new Vector2(0.6f, 1.0f), tickboxAreaLower.RectTransform))
+            {
+                ButtonEnabled = false,
+                Enabled = false
+            };
+            var tempKarmaManager = new KarmaManager();
+            foreach (string karmaPreset in tempKarmaManager.Presets.Keys)
+            {
+                karmaPresetDD.AddItem(TextManager.Get("KarmaPreset." + karmaPreset), karmaPreset);
+                if (karmaPreset == "default") { karmaPresetDD.SelectItem(karmaPreset); }
+            }
+            if (karmaPresetDD.SelectedIndex == -1) { karmaPresetDD.Select(0); }
 
             new GUIButton(new RectTransform(new Vector2(0.4f, 0.1f), menuTabs[(int)Tab.HostServer].RectTransform, Anchor.BottomRight)
             {
