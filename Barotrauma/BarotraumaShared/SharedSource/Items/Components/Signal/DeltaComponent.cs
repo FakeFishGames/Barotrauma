5b969ee1b70ae487adc897ae94ad27636120af50
Béla Becker <bela@becker.rocks>
2021-08-24 19:14:06 +0200

    Delta Component
    A component which sends a signal only when its input changes

---
+++ Barotrauma/BarotraumaShared/SharedSource/Items/Components/Signal/DeltaComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace Barotrauma.Items.Components
+{
+    class DeltaComponent : ItemComponent
+    {
+        private int prevValueHash;
+
+        public DeltaComponent(Item item, XElement element)
+            : base (item, element)
+        {
+        }
+
+        public override void ReceiveSignal(Signal signal, Connection connection)
+        {
+            if (connection.Name != "signal_in") { return; }
+
+            int valueHash = signal.value.GetHashCode();
+
+            if (valueHash == prevValueHash) {return;}
+            prevValueHash = valueHash;
+            item.SendSignal(signal, "signal_out");
+        }
+    }
+}
