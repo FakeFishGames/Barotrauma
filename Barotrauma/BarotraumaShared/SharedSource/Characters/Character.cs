067d174eabadb159e7838bcca57641351ce291ea
Eman <emanueleguarneri03@gmail.com>
2021-11-10 18:16:06 +0100

    Fixed merge conflicts

--- Barotrauma/BarotraumaShared/SharedSource/Characters/Character.cs
+++ Barotrauma/BarotraumaShared/SharedSource/Characters/Character.cs
@@ -266,6 +266,9 @@ namespace Barotrauma
 
         public readonly CharacterParams Params;
         public string SpeciesName => Params.SpeciesName;
+        public string MusicType => Params.MusicType;
+        public float MusicWeight => Params.MusicWeight;
+        public float MusicRangeMultiplier => Params.MusicRangeMultiplier;
         public string Group => Params.Group;
         public bool IsHumanoid => Params.Humanoid;
         public bool IsHusk => Params.Husk;
