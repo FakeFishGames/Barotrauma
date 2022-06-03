5a10b444ee027eb251e6169047e0705f2b4d0a83
Markus Isberg <3e849f2e5c@pm.me>
2022-06-04 00:34:02 +0900

    Build 0.18.6.0

--- Barotrauma/BarotraumaShared/SharedSource/Items/Components/Door.cs
+++ Barotrauma/BarotraumaShared/SharedSource/Items/Components/Door.cs
@@ -270,11 +270,6 @@ namespace Barotrauma.Items.Components
             {
                 Body?.SetTransform(Body.SimPosition + ConvertUnits.ToSimUnits(amount), 0.0f);
             }
-            if (linkedGap != null)
-            {
-                RefreshLinkedGap();
-                linkedGap.Rect = item.Rect;
-            }
 #if CLIENT
             UpdateConvexHulls();
 #endif
