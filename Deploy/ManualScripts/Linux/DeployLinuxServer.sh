8c9715c0f16a05b85891258f02919ba689d12bc4
Aaron <aaron.golder@gmx.de>
2022-10-15 22:33:26 +0200

    Fix: Linux deploy behoben

--- Deploy/ManualScripts/Linux/DeployLinuxServer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ../../../Barotrauma/BarotraumaServer
-dotnet publish LinuxServer.csproj -c Release --self-contained -r linux-x64 \/p:Platform="x64" \/p:RollForward=Disable \/p:RuntimeFrameworkVersion=3.1.16
