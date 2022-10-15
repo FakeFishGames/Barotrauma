018cb5eefa3faef53ecae8b3afcb1ce9910f7683
Aaron <aaron.golder@gmx.de>
2022-10-15 23:01:59 +0200

    Add: Despawnen deaktiviert

--- Barotrauma/BarotraumaShared/SharedSource/Characters/Character.cs
+++ Barotrauma/BarotraumaShared/SharedSource/Characters/Character.cs
@@ -943,7 +943,7 @@ namespace Barotrauma
             }
         }
 
-        public bool EnableDespawn { get; set; } = true;
+        public bool EnableDespawn { get; set; } = false;
 
         public CauseOfDeath CauseOfDeath
         {
