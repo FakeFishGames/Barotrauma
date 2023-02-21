614470f1d800c974a4009a01b9b7caeb809e9d74
mygamingaccount <68995233+mygamingaccount@users.noreply.github.com>
2023-02-21 10:24:25 +0100

    GreaterComponent support for multiple signals within a timeframe

--- Barotrauma/BarotraumaShared/SharedSource/Items/Components/Signal/GreaterComponent.cs
+++ Barotrauma/BarotraumaShared/SharedSource/Items/Components/Signal/GreaterComponent.cs
@@ -5,7 +5,8 @@ namespace Barotrauma.Items.Components
 {
     class GreaterComponent : EqualsComponent
     {
-        private float val1, val2;
+        private float val1 = float.NegativeInfinity;
+        private float val2 = float.PositiveInfinity;
 
         public GreaterComponent(Item item, ContentXElement element)
             : base(item, element)
@@ -25,6 +26,8 @@ namespace Barotrauma.Items.Components
             if (sendOutput)
             {
                 string signalOut = val1 > val2 ? output : falseOutput;
+                val1 = float.NegativeInfinity;
+                val2 = float.PositiveInfinity;
                 if (string.IsNullOrEmpty(signalOut)) return;
 
                 item.SendSignal(signalOut, "signal_out");
@@ -33,15 +36,18 @@ namespace Barotrauma.Items.Components
 
         public override void ReceiveSignal(Signal signal, Connection connection)
         {           
-            //base.ReceiveSignal(signal, connection);
             switch (connection.Name)
             {
                 case "signal_in1":
-                    float.TryParse(signal.value, NumberStyles.Float, CultureInfo.InvariantCulture, out val1);
+                    float signal1 = float.NegativeInfinity;
+                    float.TryParse(signal.value, NumberStyles.Float, CultureInfo.InvariantCulture, out signal1);
+                    val1 = signal1 > val1 ? signal1 : val1;
                     timeSinceReceived[0] = 0.0f;
                     break;
                 case "signal_in2":
-                    float.TryParse(signal.value, NumberStyles.Float, CultureInfo.InvariantCulture, out val2);
+                    float signal2 = float.PositiveInfinity;
+                    float.TryParse(signal.value, NumberStyles.Float, CultureInfo.InvariantCulture, out signal2);
+                    val2 = signal2 < val2 ? signal2 : val2;
                     timeSinceReceived[1] = 0.0f;
                     break;
                 case "set_output":
