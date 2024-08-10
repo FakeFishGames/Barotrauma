c69ae30dd6c23b750b9a693329e7a0b161fc67b9
Jade-Harleyy <67431770+Jade-Harleyy@users.noreply.github.com>
2024-08-09 23:12:11 -0500

    Add files.

---
+++ Barotrauma/BarotraumaClient/ClientSource/Screens/SetupUI/ISinglePlayerSetupUI.cs
@@ -0,0 +1,14 @@
+﻿using Barotrauma.Extensions;
+using Barotrauma.IO;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Barotrauma
+{
+    public interface ISinglePlayerSetupUI
+    {
+
+    }
+}
---
+++ Barotrauma/BarotraumaClient/ClientSource/Screens/SetupUI/SetupUI.cs
@@ -0,0 +1,15 @@
+﻿using Barotrauma.Extensions;
+using Barotrauma.IO;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Barotrauma
+{
+    abstract class SetupUI
+    {
+
+    }
+}
