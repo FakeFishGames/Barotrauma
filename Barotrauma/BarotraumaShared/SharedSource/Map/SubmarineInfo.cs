91c07e55bb98f4bf127b1442fc43de8e2feaea10
Vamist <dm@vamist.dev>
2025-05-03 14:14:13 +0100

    [fix] Use filepath instead of XDocument for md5hash (submarine)
    Prior to this commit, it would load an XML file (ignoring white spaces),
    write XDocument into a string (Loading the game would peak at 150MB just
    for this function alone). It would then try to strip whitespaces again
    (which allocates another 50-70mb as it constructs a string builder with
    half the current string size) and finally hash it.
    
    Instead, read the FilePath (which could be cached by the OS already),
    strip whitespaces and hash.
    
    With this change, this function no longer allocates enough to appear in
    DPA.

--- Barotrauma/BarotraumaShared/SharedSource/Map/SubmarineInfo.cs
+++ Barotrauma/BarotraumaShared/SharedSource/Map/SubmarineInfo.cs
@@ -160,8 +160,7 @@ namespace Barotrauma
                 {
                     if (hashTask == null)
                     {
-                        XDocument doc = OpenFile(FilePath);
-                        StartHashDocTask(doc);
+                        StartHashDocTask();
                     }
                     hashTask.Wait();
                     hashTask = null;
@@ -381,7 +380,7 @@ namespace Barotrauma
             }
             if (hash == null)
             {
-                StartHashDocTask(doc);
+                StartHashDocTask();
             }
             SubmarineElement = doc.Root;
         }
@@ -528,14 +527,14 @@ namespace Barotrauma
             return false;
         }
 
-        public void StartHashDocTask(XDocument doc)
+        public void StartHashDocTask()
         {
             if (hash != null) { return; }
             if (hashTask != null) { return; }
 
             hashTask = new Task(() =>
             {
-                hash = Md5Hash.CalculateForString(doc.ToString(), Md5Hash.StringHashOptions.IgnoreWhitespace);
+                hash = Md5Hash.CalculateForFile(FilePath, Md5Hash.StringHashOptions.IgnoreWhitespace);
             });
             hashTask.Start();
         }
