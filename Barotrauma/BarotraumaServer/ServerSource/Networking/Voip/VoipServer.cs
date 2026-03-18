876a6ac7c6843f20e33452c79e88c17e25aa49e6
BramvanZijp <56019239+BramvanZijp@users.noreply.github.com>
2026-03-18 08:03:32 +0100

    Add comment to voipserver to clarify isRadio's purpose.

--- Barotrauma/BarotraumaServer/ServerSource/Networking/Voip/VoipServer.cs
+++ Barotrauma/BarotraumaServer/ServerSource/Networking/Voip/VoipServer.cs
@@ -93,7 +93,7 @@ namespace Barotrauma.Networking
             //sender can't speak
             if (sender.Character != null && sender.Character.SpeechImpediment >= 100.0f) { return false; }
 
-            //check if the message can be sent via radio
+            //check if the message can be sent via radio, and if so, sets isRadio to true so the client knows to apply the appropriate audio effects.
             WifiComponent recipientRadio = null;
             if (!sender.VoipQueue.ForceLocal &&
                 ChatMessage.CanUseRadio(sender.Character, out WifiComponent senderRadio) &&
